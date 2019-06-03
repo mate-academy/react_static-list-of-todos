b4bf6b419f05b639f2d7f41d6b97e2e542854fdf
Dmitry Batsenko <batsenkod@gmail.com>
2019-06-03 16:36:22 +0300

    ghpages

---
+++ build/precache-manifest.941c6d320c4aeb5b858ff93383e8b46e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bbed5ab3cd8b8f710e964e5d01429b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "063307e76e09f8b47848",
+    "url": "/static/css/main.c244cbac.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "063307e76e09f8b47848",
+    "url": "/static/js/main.0dabd05f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
