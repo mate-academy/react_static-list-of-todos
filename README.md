0c80418f57702085538064c647a50b4b089ce93a
Vitalii <ilovepinkpony1@gmail.com>
2019-06-04 16:48:54 +0300

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-[Preview](https://ilovepinkpony1.github.io/react_static-list-of-todos/build/index.html);
--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import TodoList from './components/TodoList.js';
-
-function App() {
-  return <TodoList />;
-}
-
-export default App;
