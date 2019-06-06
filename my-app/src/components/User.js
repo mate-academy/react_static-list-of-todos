9d1b95fd0f46308f8d6aeac9da7fea796df070ab
OlhaMazurenko <mail.olgamazurenko@gmail.com>
2019-06-06 13:37:03 +0300

    'Solution'

--- my-app/src/components/User.js
+++ my-app/src/components/User.js
@@ -2,21 +2,23 @@ import React from 'react';
 import TodoList from './TodoList';
 import {todos} from '../todos';
 
-function User(props) {
-   return (
-    <div>
-      <h2 className = {props.id}>{props.name} ({props.phone})</h2>
-    <table>
-      <thead>
-       <tr>
-         <th> TODO </th>
-         <th> STATUS </th>
-       </tr>
-      </thead>
-      <TodoList todo = {todos}/>
-    </table>
-  </div>
-  );
+class User extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2 className = {this.props.id}>{this.props.name} ({this.props.phone})</h2>
+      <table>
+        <thead>
+         <tr>
+           <th> TODO </th>
+           <th> STATUS </th>
+         </tr>
+        </thead>
+        <TodoList todo = {todos}/>
+      </table>
+    </div>
+    );
+  } 
 }
 
 export default User;
--- my-app/src/components/UserList.js
+++ my-app/src/components/UserList.js
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
 import User from './User';
 import './UserList.css';
 
-function UserList(props) {
+class UserList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
   const usersList = [];
-  for (const eachUser of props.users) {
-    usersList.push(
-    <User name = {eachUser.name} phone = {eachUser.phone} key={eachUser.id}/>
+    for (const eachUser of this.props.users) {
+      usersList.push(
+      <User name = {eachUser.name} phone = {eachUser.phone} key={eachUser.id}/>
     );
- }
+  }
   return (
     <div>
      {usersList}
     </div>
-   );
+    );
+  }
 }
 
 export default UserList;
