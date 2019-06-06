30450c4bb32426edb35174bb4b7be4a99ed0db37
vitalii <dumavb777@gmail.com>
2019-06-06 14:32:55 +0300

    sol

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
-import TodoList from './components/TodoList';
 
 function App() {
   return (
-    <div className="App">
-      <TodoList />
-    </div>
+      <div className="App">
+      </div>
   );
 }
 
