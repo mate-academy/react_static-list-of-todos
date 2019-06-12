625ff7c3733bc3ac94e72cd11b5561f2724ce109
Batsenko <48134243+Batsenko@users.noreply.github.com>
2019-06-12 09:33:40 +0300

    Update TodoList.js

--- src/TodoList.js
+++ src/TodoList.js
@@ -3,17 +3,27 @@ import TodoItem from "./TodoItem.js"
 import User from "./User.js"
 
 function TodoList(props) {
-  return (
-  <>
+  return (  
+    <table>
+      <tr>
+        <th>Id</th>
+        <th>Title</th>
+        <th>Completed</th>
+	      <th>Username</th>
+	      <th>Email</th>
+	      <th>Name</th>
+      </tr>
+      
     {props.todoList.map((value, index) => {
       return (
-      <tr>
-       <TodoItem todo={value} />
-       <User user={props.userList[props.userList.findIndex((user) => user.id === value.userId)]}/>
-      </tr>
+        <tr>
+         <TodoItem todo={value} />
+         <User user={props.userList[props.userList.findIndex((user) => user.id === value.userId)]}/>
+        </tr>
       );
     })}
-  </>
+  
+    </table>
   );
 }
 
