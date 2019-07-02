20e66e3d1cfdd4535c9bb4363914d0fa6f523171
mgrinko <glukym@gmail.com>
2019-07-02 09:18:53 +0300

    Boilerplate was added

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+import todos from './api/todos';
+import users from './api/users';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Static list of todos</h1>
+      <p>
+        <span>Todos: </span>
+        {todos.length}
+      </p>
+
+      <p>
+        <span>Users: </span>
+        {users.length}
+      </p>
+    </div>
+  );
+}
+
+export default App;
