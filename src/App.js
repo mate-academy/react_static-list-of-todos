9f8fc5744fa3f0f32275c4812df4a7bc43d1157f
mgrinko <glukym@gmail.com>
2019-07-03 10:36:18 +0300

    Todo selection was implemented

--- src/App.js
+++ src/App.js
@@ -1,45 +1,52 @@
 import React from 'react';
 
-import './App.css';
-
-import todos from './api/todos';
-import users from './api/users';
-
-import TodoList from './components/TodoList';
-
-function getUser(userId) {
-  return users.find(user => user.id === userId);
-}
-
-const todosWithUser = todos.map(todo => ({
-  ...todo,
-  user: getUser(todo.userId),
-}));
-
 class App extends React.Component {
   state = {
-    count: 0,
-    todos: todosWithUser,
-  };
+    selectedTodoId: null,
 
-  handleClick = () => {
-    this.setState(prevState => ({
-      count: prevState.count + 1,
-    }));
+    todosFromServer: [
+      { id: 1, title: '111111'},
+      { id: 2, title: '222222'},
+      { id: 3, title: '333333'},
+    ],
   };
 
   render() {
     return (
       <div className="App">
-        <h1>
-          Static list of todos
-          {this.state.count}
-        </h1>
+        <h1>Todos {this.state.selectedTodoId}</h1>
 
-        <TodoList todos={this.state.todos} />
+        <TodoList
+          items={this.state.todosFromServer}
+          onTodoSelected={(todoId) => {
+            this.setState({
+              selectedTodoId: todoId
+            });
+          }}
+        />
       </div>
     );
   }
+}
+
+function TodoList(props) {
+  console.log('render');
+
+  return (
+    <ul>
+      {props.items.map(todo => (
+        <li
+          key={todo.id}
+          onClick={() => {
+            props.onTodoSelected(todo.id);
+          }}
+        >
+          {todo.title}
+          <input type="text" />
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default App;
