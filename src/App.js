0ba7cf43b29c0591155481a3da0ff5c4b88e6842
mgrinko <glukym@gmail.com>
2019-07-05 09:30:48 +0300

    Load button was implemented

--- src/App.js
+++ src/App.js
@@ -1,66 +1,45 @@
 import React from 'react';
 
+const todosFromServer = [
+  { id: 1, title: '111111' },
+  { id: 2, title: '222222' },
+  { id: 3, title: '333333' },
+];
+
 class App extends React.Component {
   state = {
-    selectedTodoId: null,
+    todos: [],
+    isLoaded: false,
+    isLoading: false,
+  };
 
-    todosFromServer: [
-      { id: 1, title: '111111'},
-      { id: 2, title: '222222'},
-      { id: 3, title: '333333'},
-    ],
+  handleClick = () => {
+    this.setState({
+      isLoading: true,
+    });
+
+    setTimeout(() => {
+      this.setState({
+        todos: todosFromServer,
+        isLoaded: true,
+        isLoading: false,
+      });
+    }, 2000);
   };
 
   render() {
     return (
-      <div className="App">
-        <h1>Todos {this.state.selectedTodoId}</h1>
-
-        <ul>
-          {this.state.todosFromServer.map(todo => (
-            <li
-              key={todo.id}
-              onClick={() => {
-                this.setState({
-                  selectedTodoId: todo.id
-                });
-              }}
-            >
-              {todo.title}
-              <input type="text" />
-            </li>
-          ))}
-        </ul>
-
-        <TodoList
-          items={this.state.todosFromServer}
-          onTodoSelected={(todoId) => {
-            this.setState({
-              selectedTodoId: todoId,
-            });
-          }}
-        />
-      </div>
+      <main>
+        { this.state.isLoaded ? (
+          <h1>Todos {this.state.todos.length}</h1>
+        ) : (
+          <button onClick={this.handleClick}>
+            {this.state.isLoading ? 'Loading...' : 'Load' }
+          </button>
+        )}
+      </main>
     );
   }
-}
-
-function TodoList(props) {
-  return (
-    <ul>
-      {props.items.map(todo => (
-        <li
-          key={todo.id}
-          onClick={() => {
-            props.onTodoSelected(todo.id);
-          }}
-        >
-          {todo.title}
-          <input type="text" />
-        </li>
-      ))}
-    </ul>
-  );
 }
 
 export default App;
