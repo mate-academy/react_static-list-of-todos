1c2970d6c453524b918b93f0f4304ebd29a30a2a
igaryok <igor.sobko@gmail.com>
2019-09-09 23:36:50 +0300

    solution

---
+++ src/components/User/User.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './User.css';
+import PropTypes from 'prop-types';
+
+function User({ name, email }) {
+  return (
+    <>
+      <span className="li-user">User</span>
+      &nbsp;:&nbsp;
+      {name}
+      (
+      {email}
+      )
+    </>
+  );
+}
+
+export default User;
+
+User.propTypes = {
+  name: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+};
