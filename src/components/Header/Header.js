91cea1ffccf9dd5087c557c4f1cc1d46af2ee1ac
Alex Andrutskii <alex.andrutskii@gmail.com>
2019-09-09 13:28:55 +0300

    Changes according to comments

---
+++ src/components/Header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Header.css';
+
+const Header = ({ usersCount, todosCount }) => {
+  return (
+    <div className="container mb-5">
+      <div className="row">
+        <header className="col text-center">
+          <h1>Static list of todos</h1>
+          <p>
+            <span>Todos: </span>
+            {todosCount}
+          </p>
+          <p>
+            <span>Users: </span>
+            {usersCount}
+          </p>
+        </header>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
