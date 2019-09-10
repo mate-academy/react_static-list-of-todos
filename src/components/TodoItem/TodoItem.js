ec29bdbcf346c18fb2538cb0f62105624f88e712
Dmytro <voronok16@gmail.com>
2019-09-10 16:51:35 +0300

    solution

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -3,10 +3,10 @@ import User from '../User/User';
 import './TodoItem.css';
 
 function TodoItem(props) {
-  const {todo, user} = props;
+  const { todo, user } = props;
 
   return (
-    <div className={todo.completed ? "todo-item__complete" : "todo-item__not-complete"}>
+    <div className={todo.completed ? 'todo-item__complete' : 'todo-item__not-complete'}>
       <h2>{todo.title}</h2>
       <p>{todo.completed ? 'Completed' : 'Not completed'}</p>
       <User user={user} />
