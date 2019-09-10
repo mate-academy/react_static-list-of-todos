664f7cf86a02e567c0ee9a265fbea04da8027625
ivan_safiulin <ivansafiulin@myrambler.ru>
2019-09-10 14:12:07 +0300

    Changed sulution

--- src/component/todoItem/todoItem.js
+++ src/component/todoItem/todoItem.js
@@ -4,12 +4,12 @@ import User from '../User/User';
 
 function TodoItem({ todo }) {
   return (
-    <div className={`card ${todo.completed ? 'background-color__green'
-      : 'background-color__yellow'}`}>
+    <div className={`card ${todo.completed ? 'bg-green'
+      : 'bg-yellow'}`}>
       <h3 className="card__title">{todo.title}</h3>
       <div className="card__checked">
         <i className="icon far fa-check-square" />
-        Complited Task:
+        Completed Task:
         <i className={`icon fas ${todo.completed ? 'checked fa-check'
           : 'unchecked fa-times'}`} />
       </div>
