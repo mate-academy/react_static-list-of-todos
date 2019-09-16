412ef49f78d5ffc8f3f6ae468a8a694005968a6e
ArtemMarkevych1 <aw.markevich@gmail.com>
2019-09-16 17:07:56 +0300

    getting info from api

--- src/App.js
+++ src/App.js
@@ -5,26 +5,27 @@ import apiTodos from './api/api-todos';
 import apiUsers from './api/api-users';
 
 import TodoList from './Components/TodoList/TodoList';
-import Header from './Components/Header/Header';
-
-function getTodosWithUsers(todos, usersList) {
-  return todos.map(item => (
-    {
-      ...item,
-      user: usersList.find(user => user.id === item.userId),
-    }
-  ));
-}
 
 const preparedTodos = getTodosWithUsers(apiTodos, apiUsers);
 
 function App() {
   return (
-    <>
-      <Header />
+    <div className="App">
+      <h1>Static list of todos</h1>
+      <p>
+        <span>Todos: </span>
+        {preparedTodos.length}
+      </p>
       <TodoList todos={preparedTodos} />
-    </>
+    </div>
   );
+}
+
+function getTodosWithUsers(todos, usersList) {
+  return todos.map(item => ({
+    ...item,
+    user: usersList.find(user => user.id === item.userId),
+  }));
 }
 
 export default App;
