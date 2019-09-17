cd7d21a6b37ff84118dfab6b04c58af2f8fc99b2
Deman250 <deman.250052@gmail.com>
2019-09-17 06:46:08 +0300

    add new line before rules

--- src/App.js
+++ src/App.js
@@ -9,8 +9,7 @@ function getTodosWithUsers(todoes, user) {
   return todoes.map(todo => ({
     ...todo,
     user: user.find(item => item.id === todo.userId),
-  }
-  ));
+  }));
 }
 
 const todosWithUsers = getTodosWithUsers(todos, users);
@@ -18,6 +17,7 @@ const todosWithUsers = getTodosWithUsers(todos, users);
 function App() {
   return (
     <div>
+      <h1 className="app">list of todos</h1>
       <TodoList todos={todosWithUsers} />
     </div>
   );
