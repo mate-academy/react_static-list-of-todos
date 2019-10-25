86fbfb60f16f909f99cf1b203f265b805346974d
Volodymyr_Roiuk <heppivova@gmail.com>
2019-10-25 18:12:48 +0300

    'Solution'

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import React from 'react';
 import './App.css';
-
-import todos from './api/todos';
-import users from './api/users';
+import TodoTable from './components/todoTable/TodoTable';
 
 function App() {
   return (
     <div className="App">
-      <h1>Static list of todos</h1>
-      <p>
-        <span>Todos: </span>
-        {todos.length}
-      </p>
-
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
+      <TodoTable />
     </div>
   );
 }
---
+++ src/components/TodoItem/TodoItem.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Icon, Table } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import User from '../User/User';
+
+const TodoItem = ({ title, user, isCompleted }) => (
+  <tr>
+    <td>{title}</td>
+    <User name={user.name} />
+    {isCompleted ? (
+      <Table.Cell positive>
+        <Icon name="checkmark" />
+        Approved
+      </Table.Cell>
+    ) : (
+      <Table.Cell negative>
+        <Icon name="close" />
+        In procces
+      </Table.Cell>
+    )
+    }
+  </tr>
+);
+
+TodoItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  isCompleted: PropTypes.bool.isRequired,
+  user: PropTypes.object.isRequired,
+};
+
+export default TodoItem;
