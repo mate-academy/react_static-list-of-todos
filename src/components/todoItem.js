5c2e56d463af9e17387fa898e0cad5b573ff28cd
MMashivskij <mmashivskij@gmail.com>
2019-10-29 23:40:53 +0200

    Solutions

---
+++ src/components/todoItem.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import User from './user';
+
+function TodoItem({ todo: { user, title, completed } }) {
+  return (
+    <tr className={completed ? 'positive' : 'negative'}>
+      <td>{title}</td>
+      <td><User name={user} /></td>
+      <td>{completed ? 'Completed' : 'Failed'}</td>
+    </tr>
+  );
+}
+
+export default TodoItem;
---
+++ src/components/user.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function User({ name }) {
+  return (
+    <>
+      {name.name}
+    </>
+  );
+}
+
+export default User;
