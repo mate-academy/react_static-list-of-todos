db284d7c76d66fa1a29f83074f931d98a036b6f3
YuraBobeshko <gorobech55@gmail.com>
2019-10-30 11:14:43 +0200

    sol

--- src/getTodosWithUsers.js
+++ src/getTodosWithUsers.js
@@ -5,7 +5,7 @@ function getTodosWithUsers(todos, users) {
   for (let user of users) {
     for (let todo of todos) {
       if (user.id === todo.userId) {
-        todo.user = Object.assign({}, user);
+        todo.user = user;
       }
     }
   }
