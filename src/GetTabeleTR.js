834bdbe8c28b0dc94e66ff9e0d1ad69fe5b42f5d
YuraBobeshko <gorobech55@gmail.com>
2019-10-31 15:38:51 +0200

    sol

--- src/GetTabeleTR.js
+++ src/GetTabeleTR.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
 
-function GetTabeleTR (elem) {
-  return elem.props.map(user =>{
+function GetTabeleTR (users) {
+  return users.props.map(user =>{
     return(
     <tr className={user.completed ? "redBG" : "greenBG"} key={user.id}>
       <td>{user.title}</td>
