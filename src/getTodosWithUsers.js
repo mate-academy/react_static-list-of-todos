7294f86a5e1806d3fa2fd9036d616069e5bcdf0e
YuraBobeshko <gorobech55@gmail.com>
2019-11-03 17:11:45 +0200

    sol

--- src/getTodosWithUsers.js
+++ src/getTodosWithUsers.js
@@ -3,8 +3,8 @@ import './App.css';
 function getTodosWithUsers(todos, users) {
 
   return todos.map((todo) => {
-    const currentUser = users.find(user => user.id === todo.userId);
-    let userMutated = todo;
+    const currentUser = Object.assign({}, users.find(user => user.id === todo.userId));
+    let userMutated = Object.assign({}, todo);
     userMutated.user = currentUser;
 
     return userMutated;
