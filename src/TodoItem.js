cb67b6f7c8635b7e62496cef9e65f2962a08434e
Anna Gerdii <annagerdiy@gmail.com>
2019-12-03 19:35:40 +0200

    fix

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -7,12 +7,12 @@ const TodoItem = ({ todo: { user, id, title, completed } }) => (
     <dt>
       {`${id}. ${title}`}
     </dt>
-    <dd style={{ marginBottom: '20px' }}>
+    <dd className="itemDescription">
       <span style={{ color: completed ? 'green' : 'red' }}>
         {completed ? 'completed' : 'not completed'}
       </span>
       <br />
-      <span style={{ color: 'blue' }}>
+      <span className="userName">
         <User user={user} />
       </span>
     </dd>
