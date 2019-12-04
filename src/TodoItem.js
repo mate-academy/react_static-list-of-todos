ca69448a4662d62cb104139862dad928a0506d3e
vadim-os <osinsky_vadim@yahoo.com>
2019-12-04 18:31:10 +0200

    solution

---
+++ src/TodoItem.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import User from './User';
+
+const TodoItem = ({ todo }) => (
+  <>
+    <tr>
+      <td>{todo.id}</td>
+      <td>{todo.title}</td>
+      <User user={todo.user} />
+      <td>{todo.completed ? 'Done' : 'In progress'}</td>
+    </tr>
+  </>
+);
+
+TodoItem.propTypes = {
+  todo: PropTypes.shape({
+    id: PropTypes.number,
+    title: PropTypes.string,
+    completed: PropTypes.bool,
+  }).isRequired,
+};
+
+export default TodoItem;
