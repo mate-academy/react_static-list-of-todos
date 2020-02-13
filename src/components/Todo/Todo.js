990e837f675c41e1ca77765fef928a1a8d26ea43
Yaroslav Magrelo <slava.magrelo@gmail.com>
2020-02-13 16:49:32 +0200

    add solution

---
+++ src/components/Todo/Todo.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Todo.css';
+import PropTypes from 'prop-types';
+
+export const Todo = ({ title, completed }) => (
+  <>
+    <p className="title">
+      <strong>Title</strong>
+      { `: ${title}` }
+    </p>
+    {completed
+      ? (
+        <p className="status">
+          <strong>Status:</strong>
+          <span className="status--true"> True</span>
+        </p>
+      )
+      : (
+        <p className="status">
+          <strong>Status:</strong>
+          <span className="status--false"> False</span>
+        </p>
+      )}
+  </>
+);
+
+Todo.propTypes = {
+  title: PropTypes.string.isRequired,
+  completed: PropTypes.string.isRequired,
+};
