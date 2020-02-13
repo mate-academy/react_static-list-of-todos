574fae5cfdde025094c0ccd4ae67b5b8694725a9
Stanislav Mochyr <delenjer@gmail.com>
2020-02-13 14:36:23 +0200

    create a separate folder

--- src/components/todoitem/TodoItems.js
+++ src/components/todoitem/TodoItems.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import User from './User';
+import User from '../user/User';
 
 const TodoItem = ({ todo }) => (
   <tr>
--- src/components/todolist/TodoList.js
+++ src/components/todolist/TodoList.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import TodoItem from './TodoItems';
+import TodoItem from '../todoitem/TodoItems';
 
 const TodoList = ({ todos }) => (
 
