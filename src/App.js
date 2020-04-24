f93dbf76a946034abc0ab9c9e99fd0b449f8afa4
Aleksandr-Tyagun <alexander.tiagun@gmail.com>
2020-04-24 10:12:54 +0300

    minor fixes

--- src/App.js
+++ src/App.js
@@ -5,22 +5,22 @@ import TodoList from './components/TodoList';
 import todos from './api/todos';
 import users from './api/users';
 
+const preparedTodos = [];
+
+todos.forEach((todo) => {
+  const user = users.find(person => person.id === todo.userId);
+
+  if (user !== undefined) {
+    preparedTodos.push({
+      id: todo.id,
+      title: todo.title,
+      completed: todo.completed,
+      user,
+    });
+  }
+});
+
 function App() {
-  const preparedTodos = [];
-
-  todos.forEach((todo) => {
-    const user = users.find(person => person.id === todo.userId);
-
-    if (user !== undefined) {
-      preparedTodos.push({
-        id: todo.id,
-        title: todo.title,
-        completed: todo.completed,
-        user,
-      });
-    }
-  });
-
   return (
     <div className="app">
       <h1 className="app__title">Static list of todos</h1>
