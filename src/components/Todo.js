73a9d437a8bdcb94e926163d44054365ba9fd709
Andreas_Just <andreas.wq@gmail.com>
2020-04-25 04:45:09 +0300

    Solution

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,25 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Icon, Table } from 'semantic-ui-react';
+import { Table } from 'semantic-ui-react';
 import User from './User';
+import TodoCellCompleted from './TodoCellCompleted';
 
 const TodoItem = ({ id, user, completed, title }) => (
   <Table.Row warning>
     <Table.Cell>{id}</Table.Cell>
     <User {...user} />
-    <TodoCell compl={completed} />
+    <TodoCellCompleted compl={completed} />
     <Table.Cell textAlign="left">{title}</Table.Cell>
   </Table.Row>
-);
-
-const TodoCell = ({ compl }) => (
-  <Table.Cell negative={!compl} positive={compl}>
-    <Icon
-      color={compl ? 'green' : 'red'}
-      name={compl ? 'checkmark' : 'close'}
-    />
-    {compl ? 'Complebitur' : 'In processus'}
-  </Table.Cell>
 );
 
 TodoItem.propTypes = {
@@ -33,9 +24,6 @@ TodoItem.propTypes = {
   ).isRequired,
   title: PropTypes.string.isRequired,
   completed: PropTypes.bool.isRequired,
-};
-TodoCell.propTypes = {
-  compl: PropTypes.bool.isRequired,
 };
 
 export default TodoItem;
