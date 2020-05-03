1400eb20385b88fc331df844af77f3dc004f5512
sanyanemesh <sanya.nemesh@gmail.comgit config --global user.name sanyanemeshgit config --global user.email sanya.nemesh@gmail.com>
2020-05-03 19:02:42 +0300

    Fix

--- src/components/User.js
+++ src/components/User.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function User({ user }) {
-  return (
-    <p>
-      <strong>User: </strong>
-      <i className="user__name">{user.name}</i>
-    </p>
-  );
-}
+const User = ({ user }) => (
+  <p>
+    <strong>User: </strong>
+    <i className="user__name">{user.name}</i>
+  </p>
+);
 
 User.propTypes = {
   user: PropTypes.shape({
