2cb705e9deb4415636a04b1b91c9c18db2ada918
Yakiv Syvukha <yakov.sivukha@gmail.com>
2020-06-26 19:24:02 +0300

    move data preparation to PreparedTodos

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
 import React from 'react';
 import './App.css';
 
-import todos from './api/todos';
-import users from './api/users';
 import { TodoList } from './components/TodoList/TodoList';
-
-const usersMap = {};
-const usersLength = users.length;
-
-for (let i = 0; i < usersLength; i += 1) {
-  usersMap[users[i].id] = users[i];
-}
-
-const preparedTodos = todos.map(todo => ({
-  user: usersMap[todo.userId],
-  ...todo,
-}));
+import { preparedTodos } from './components/PreparedTodos';
 
 function App() {
   return (
     <div className="App">
       <TodoList todolist={preparedTodos} />
-
-      {/* <p>
-        <span>Users: </span>
-        {users.length}
-      </p> */}
     </div>
   );
 }
