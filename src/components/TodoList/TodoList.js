12cc9b3f9c969ae26063c4809ccfaa35cf349c53
Yakiv Syvukha <yakov.sivukha@gmail.com>
2020-06-27 08:36:49 +0300

    TodoList shape

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Todo } from '../Todo/Todo';
 import './TodoList.css';
+import { TodoListShape } from './TodoListShape';
 
 export function TodoList(props) {
   const { todolist } = props;
@@ -13,24 +13,11 @@ export function TodoList(props) {
       {todolist.length}
       <ul className="tasks">
         {todolist.map(todo => (
-          <Todo todo={todo} />
+          <Todo key={todo.id} todo={todo} />
         ))}
       </ul>
     </>
   );
 }
 
-TodoList.propTypes = {
-  todolist: PropTypes.arrayOf(
-    PropTypes.shape({
-      user: PropTypes.shape({
-        name: PropTypes.number.isRequired,
-        id: PropTypes.number.isRequired,
-      }).isRequired,
-      userId: PropTypes.number.isRequired,
-      id: PropTypes.number.isRequired,
-      completed: PropTypes.bool.isRequired,
-      title: PropTypes.string.isRequired,
-    }).isRequired,
-  ).isRequired,
-};
+TodoList.propTypes = TodoListShape;
