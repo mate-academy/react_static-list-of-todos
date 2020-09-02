126d6142f83a526b39e6b8eaf6edcf170b81a972
Vlad Savustianenko <vladsavustianenko@gmail.com>
2020-09-02 18:30:59 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import React from 'react';
 import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import todos from './api/todos';
-import users from './api/users';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className="App">
-      <h1>Static list of todos</h1>
-      <p>
-        <span>Todos: </span>
-        {todos.length}
-      </p>
-
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
+      <TodoList />
     </div>
   );
 }
