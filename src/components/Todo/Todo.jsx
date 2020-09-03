9a9dafc1f760829a61d49cbd62ef818db0e11b04
Eduard Nazarenko <ferrarif40lemans@gmail.com>
2020-09-03 14:31:45 +0300

    changed

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,34 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import './Todo.scss';
 import { User } from '../User/User';
 
-export const Todo = ({ title, user, completed }) => {
-  const stage = completed ? `done` : 'pending';
-
-  return (
-    <ul className="todo">
+export const Todo = ({ title, user, completed }) => (
+  <ul className="todo">
+    <li className="todo__username">
       <User name={user.name} />
+    </li>
 
-      <li className="todo__title">
-        <span className="todo__prefix">Title: </span>
-        {title}
-      </li>
+    <li className="todo__title">
+      <span className="todo__prefix">Title: </span>
+      {title}
+    </li>
 
-      <li className={`todo__stage todo__stage--${stage}`}>
-        <span className="todo__prefix">Stage: </span>
-        {stage}
-      </li>
-    </ul>
-  );
-};
-
-Todo.defaultProps = {
-  user: {},
-};
+    <li className={
+      classNames('todo__stage', { 'todo__stage--done': completed })
+    }
+    >
+      <span className="todo__prefix">Stage: </span>
+      {completed ? `done` : 'pending'}
+    </li>
+  </ul>
+);
 
 Todo.propTypes = {
   title: PropTypes.string.isRequired,
   completed: PropTypes.bool.isRequired,
-  user: PropTypes.oneOfType([PropTypes.string]),
+  user: PropTypes.objectOf(PropTypes.string).isRequired,
 };
--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -3,10 +3,10 @@ import PropTypes from 'prop-types';
 import './User.scss';
 
 export const User = ({ name }) => (
-  <li className="todo__username">
+  <>
     <span className="todo__prefix">User name: </span>
     {name}
-  </li>
+  </>
 );
 
 User.propTypes = {
