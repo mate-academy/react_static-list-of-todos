9d2304e224a7c3f9a1486e9beaf878ffe7ccfbaa
Serhii Antonyshyn <serginio4422@gmail.com>
2020-09-18 13:20:12 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import React from 'react';
 import './App.scss';
+import PropTypes from 'prop-types';
+
+import { TodoList } from './components/TodoList';
 
 import todos from './api/todos';
 import users from './api/users';
 
+const preparedTodos = todos.map(todo => ({
+  ...todo,
+  user: users.find(user => user.id === todo.userId),
+}));
+
+preparedTodos.propTypes = {
+  user: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    }),
+  ),
+  todo: PropTypes.arrayOf(
+    PropTypes.shape({
+      userId: PropTypes.number.isRequired,
+    }),
+  ),
+};
+
+preparedTodos.defaultProps = {
+  user: [],
+  userId: [],
+};
+
 function App() {
   return (
     <div className="App">
-      <h1>Static list of todos</h1>
+      <h1>Static list of Todos</h1>
+      <TodoList todos={preparedTodos} />
       <p>
         <span>Todos: </span>
         {todos.length}
