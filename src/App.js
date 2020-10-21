c57a3c83417ad77f0818110078a1963a41ff2653
Alina <alina90565@gmail.com>
2020-10-21 12:56:37 +0300

    delete App.css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { preparedTodos } from './api/preparedTodos';
 import { TodoList } from './components/TodoList';
-import './App.scss';
 
 function App() {
   return (
