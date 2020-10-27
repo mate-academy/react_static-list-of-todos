31ebfa33c11a67c152157b86efe60981d6073ab4
OleksandrMykoliuk <69759907+OleksandrMykoliuk@users.noreply.github.com>
2020-10-27 11:26:26 +0200

    Solution fixed shapes

--- src/components/Todo/TodoShape.js
+++ src/components/Todo/TodoShape.js
@@ -1,18 +1,9 @@
 import PropTypes from 'prop-types';
 import { UserShape } from '../User/UserShape';
 
-export const TodoShape = {
+export const TodoShape = PropTypes.shape({
+  id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
-  user: PropTypes.objectOf(
-    PropTypes.shape = {
-      userId: PropTypes.number.isRequired,
-      id: PropTypes.number.isRequired,
-      title: PropTypes.string.isRequired,
-      completed: PropTypes.bool.isRequired,
-      user: PropTypes.shape = {
-        UserShape,
-      }.isRequired,
-    },
-  ).isRequired,
   completed: PropTypes.bool.isRequired,
-};
+  user: UserShape.isRequired,
+});
