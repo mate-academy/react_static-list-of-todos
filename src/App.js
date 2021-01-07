0b75ce237974cd0aa4226167350d82b9ba6345b0
Olga Rubalska <olga.anikeieva@gmail.com>
2021-01-07 23:33:44 +0200

    Solution

--- src/App.js
+++ src/App.js
@@ -3,20 +3,32 @@ import './App.scss';
 
 import todos from './api/todos';
 import users from './api/users';
+import { TodoList } from './components/TodoList';
+
+// eslint-disable-next-line max-len
+const preparedTodos = todos.map(todo => (
+  {
+    ...todo, user: users.find(user => user.id === todo.userId),
+  }));
 
 function App() {
   return (
     <div className="App">
-      <h1>Static list of todos</h1>
-      <p>
-        <span>Todos: </span>
-        {todos.length}
-      </p>
+      <div className="header"><h1>Static list of todos:</h1></div>
+      <div className="wrap-list">
+        <TodoList todos={preparedTodos} />
+      </div>
+      <div className="footer">
+        <p>
+          <span>Todos: </span>
+          {todos.length}
+        </p>
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
+        <p>
+          <span>Users: </span>
+          {users.length}
+        </p>
+      </div>
     </div>
   );
 }
