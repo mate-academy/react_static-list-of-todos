8064c967910fee4d22d7eeb0bf16adc9bab8ac29
RioDron <riopositivniy@gmail.com>
2021-01-12 15:44:28 +0200

    add all propsType

--- src/TodoList/TodoList.jsx
+++ src/TodoList/TodoList.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { TodoItem } from '../TodoItem';
 
-export const TodoList = props => (
-  props.preparedTodos.map(
+export const TodoList = ({ preparedTodos }) => (
+  preparedTodos.map(
     item => (<TodoItem key={item.id} {...item} />),
   )
 );
+
+TodoList.propTypes = {
+  preparedTodos: PropTypes.arrayOf(
+    PropTypes.shape({
+      item: PropTypes.string.isRequired,
+      id: PropTypes.string.isRequired,
+      userId: PropTypes.string.isRequired,
+      completed: PropTypes.string.isRequired,
+      user: PropTypes.array.isRequired,
+    }).isRequired,
+  ),
+};
