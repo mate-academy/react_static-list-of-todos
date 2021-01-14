7be3b1c097616e581436e0fa6ad8c800f49034d2
Dima Bond <neverpooh@gmail.com>
2021-01-14 11:44:06 +0200

    simplified

--- src/App.js
+++ src/App.js
@@ -11,13 +11,20 @@ const preparedTodos = todos
     user: users.find(user => user.id === todo.userId),
   }));
 
+const doneTasks = todos.filter(todo => todo.completed).length;
+
 function App() {
   return (
     <div className="App">
       <div className="header">
         <h1>TODO list</h1>
         <div className="header__info">
-          <span className="header__todo" />
+          <span>
+            {`Tasks accomplished: ${doneTasks}`}
+          </span>
+          <span>
+            {`Tasks to do: ${todos.length - doneTasks}`}
+          </span>
           <span className="header__done" />
           <span>
             People involved:
@@ -30,34 +37,5 @@ function App() {
     </div>
   );
 }
-
-function counter() {
-  const headerInfoTodo = document.querySelector('.header__todo');
-  const headerInfoDone = document.querySelector('.header__done');
-  const doneItems = document.querySelectorAll('.done').length;
-  const todoItems = todos.length - doneItems;
-
-  headerInfoTodo.innerText = `Todos left: ${todoItems}`;
-  headerInfoDone.innerText = `Todos accomplished: ${doneItems}`;
-}
-
-function checker() {
-  const container = document.querySelector('ul');
-
-  counter();
-  container.addEventListener('click', (e) => {
-    const item = e.target.closest('.todo__item');
-
-    if (!item || !container.contains(item)) {
-      return;
-    }
-
-    e.target.closest('.card').classList.toggle('done');
-    item.querySelector('.todotext').classList.toggle('donetext');
-    counter();
-  });
-}
-
-setTimeout(checker, 100);
 
 export default App;
