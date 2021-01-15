2eb659f208ae26660c9f554f7d0a5f7b1d503cbc
Marina <59792949+Layrovell@users.noreply.github.com>
2021-01-15 11:39:04 +0200

    Update Todo.jsx

--- src/component/Todo/Todo.jsx
+++ src/component/Todo/Todo.jsx
@@ -22,12 +22,4 @@ export const Todo = ({ title, completed, user }) => {
   );
 };
 
-Todo.propTypes = {
-  title: PropTypes.string.isRequired,
-  completed: PropTypes.bool.isRequired,
-  user: PropTypes.shape({
-    name: PropTypes.string.isRequired,
-  }).isRequired,
-};
-
 Todo.propTypes = TodoType;
