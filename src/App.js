ac0867d4b8b450faf9d16ffad3b2b5df034a91c7
AndrewKreshchenko <mail2andyk@gmail.com>
2021-01-20 12:51:53 +0200

    Solution

--- src/App.js
+++ src/App.js
@@ -3,21 +3,27 @@ import './App.scss';
 
 import todos from './api/todos';
 import users from './api/users';
+import { TodoList } from './components/TodoList';
+
+const preparedTodos = todos.map(todo => ({
+  ...todo,
+  user: users.find(user => todo.userId === user.id).name,
+}));
 
 function App() {
   return (
-    <div className="App">
-      <h1>Static list of todos</h1>
-      <p>
+    <>
+      <p className="items-summary">
         <span>Todos: </span>
         {todos.length}
       </p>
 
-      <p>
+      <p className="items-summary">
         <span>Users: </span>
         {users.length}
       </p>
-    </div>
+      <TodoList todos={preparedTodos} />
+    </>
   );
 }
 
---
+++ src/components/Todo/Todo.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { User } from '../User';
+
+export const Todo = ({
+  title,
+  user,
+  completed,
+}) => (
+  <>
+    <h2>{title}</h2>
+    <User name={user} />
+    <p>
+      Task is
+      {completed || 'not '}
+      completed.
+    </p>
+    {completed ? (
+      <i className="todo-list__item-completed" />
+    ) : ''
+    }
+  </>
+);
+
+Todo.propTypes = {
+  title: PropTypes.string.isRequired,
+  user: PropTypes.string.isRequired,
+  completed: PropTypes.bool.isRequired,
+};
