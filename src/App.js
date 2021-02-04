67a266d29ad793ef3f625afff9f20dc9790f3059
Nazarii <nazarrishchuk@gmail.com>
2021-02-04 13:26:28 +0200

    Solution

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import './App.scss';
 
 import todos from './api/todos';
 import users from './api/users';
+import { TodoList } from './components/TodoList';
 
 function App() {
+  const preparedTodos = [...todos].map(todo => ({
+    ...todo,
+    user: users.find(el => el.id === todo.userId),
+  }));
+
   return (
     <div className="App">
       <h1>Static list of todos</h1>
@@ -17,6 +23,7 @@ function App() {
         <span>Users: </span>
         {users.length}
       </p>
+      <TodoList todos={preparedTodos} />
     </div>
   );
 }
