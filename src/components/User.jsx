2ebef91d5bf2ee199d2076b77dec65b16bdc5d6b
chri911 <delightsome@bigmir.net>
2021-03-10 14:32:23 +0200

    remove fragment

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -2,9 +2,7 @@ import React from 'react';
 import { UserType } from '../types';
 
 export const User = ({ user }) => (
-  <>
     <p className="user">{user.name}</p>
-  </>
 );
 
 User.propTypes = {
