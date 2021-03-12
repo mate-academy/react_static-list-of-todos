e5e892e51d6143cf5611eeb0fc6ef4cda79b1a60
Olexandr Bieliaiev <Shurasava17@gmail.com>
2021-03-12 18:32:23 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.scss';
+import TodoList from './components/TodoList/TodoList';
 
 import todos from './api/todos';
 import users from './api/users';
 
 function App() {
+  const preparedTodos = todos.map((todo) => {
+    const a = {
+      ...todo,
+      user: users[todo.userId - 1],
+    };
+
+    return a;
+  });
+
   return (
     <div className="App">
       <h1>Static list of todos</h1>
@@ -17,6 +27,9 @@ function App() {
         <span>Users: </span>
         {users.length}
       </p>
+      <TodoList
+        preparedTodos={preparedTodos}
+      />
     </div>
   );
 }
