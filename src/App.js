6a9918001d546d18083e459974e2a0dd394ff965
Vladyslav Semyrekov <semyrekov.vladyslav@gmail.com>
2021-03-16 20:27:42 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -4,19 +4,28 @@ import './App.scss';
 import todos from './api/todos';
 import users from './api/users';
 
+import { TodoList } from './components/TodoList/TodoList';
+
+const preparedTodos = todos.map(todo => ({
+  ...todo,
+  user: users.find(user => user.id === todo.userId),
+}));
+
 function App() {
   return (
     <div className="App">
-      <h1>Static list of todos</h1>
+      <h1 className="App__header">Static list of todos</h1>
       <p>
-        <span>Todos: </span>
+        <span className="App__info">Todos: </span>
         {todos.length}
       </p>
 
       <p>
-        <span>Users: </span>
+        <span className="App__info">Users: </span>
         {users.length}
       </p>
+
+      <TodoList todos={preparedTodos} />
     </div>
   );
 }
---
+++ src/components/Todo/Todo.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { User } from '../User/User';
+
+export const Todo = ({
+  title,
+  completed,
+  user,
+}) => (
+  <>
+    <User user={user} />
+    {' '}
+    <span className="title">{title}</span>
+    <span>
+      {completed
+        ? <span role="img" aria-label="good">&#128077;</span>
+        : <span role="img" aria-label="bad">&#128078;</span>}
+    </span>
+  </>
+);
+
+Todo.propTypes = {
+  title: PropTypes.string.isRequired,
+  completed: PropTypes.bool.isRequired,
+  user: PropTypes.shape({ name: PropTypes.string.isRequired }).isRequired,
+};
