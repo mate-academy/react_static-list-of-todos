3dc66bb346822ea954d57cc24737a8304ad912b2
Anna <garsinaanna32778@gmail.com>
2021-03-17 16:44:23 +0200

    created static todo list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import React from 'react';
-import './App.scss';
 
 import todos from './api/todos';
 import users from './api/users';
 
+import { TodoList } from './components/TodoList/TodoList';
+import './App.scss';
+
+const preparedTodos = todos.map(todo => ({
+  ...todo,
+  user: users.find(user => user.id === todo.userId),
+}));
+
 function App() {
   return (
-    <div className="App">
-      <h1>Static list of todos</h1>
-      <p>
-        <span>Todos: </span>
-        {todos.length}
-      </p>
+    <>
+      <div className="App">
+        <h1 className="app__header">Static list of todos</h1>
+        <p className="app__todos">
+          <span>Todos:</span>
+          {todos.length}
+        </p>
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
-    </div>
+        <p className="app__users">
+          <span>Users: </span>
+          {users.length}
+        </p>
+      </div>
+      <TodoList todos={preparedTodos} />
+    </>
   );
 }
 
