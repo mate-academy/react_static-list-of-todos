3046910aa7fc3c223740089b82d7fda36f92419c
Smihotur Roman <smikhotur@gmail.com>
2021-03-29 16:07:29 +0300

    corrected code

--- src/component/Todo/Todo.jsx
+++ src/component/Todo/Todo.jsx
@@ -3,16 +3,21 @@ import PropTypes from 'prop-types';
 import './Todo.scss';
 import { User } from '../User/User';
 
-export const Todo = ({ user, title, completed }) => (
+export const Todo = ({
+  user,
+  title,
+  completed,
+}) => (
   <todo>
     <User name={user} />
 
     <p className="todo__line">
       {title}
       {' - '}
-      {completed
+      {(completed)
         ? <span style={{ color: 'green' }}>completed</span>
-        : <span style={{ color: 'red' }}>not completed</span>}
+        : <span style={{ color: 'red' }}>not completed</span>
+      }
     </p>
   </todo>
 );
