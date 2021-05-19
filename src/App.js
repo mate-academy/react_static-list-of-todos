f445755c19d246bf9dedbbf64dead8617dee5da6
Olga Tsarova <alita.jet@gmail.com>
2021-05-19 16:32:11 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import './App.scss';
+import { TodoList } from './components/TodoList/TodoList';
 
 import todos from './api/todos';
 import users from './api/users';
 
+const preparedTodos = todos.map(el => ({
+  ...el,
+  user: users.find(user => user.id === el.userId),
+}));
+
 function App() {
   return (
-    <div className="App">
-      <h1>Static list of todos</h1>
-      <p>
-        <span>Todos: </span>
-        {todos.length}
-      </p>
-
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
+    <div className="app">
+      <TodoList todosArr={preparedTodos} />
     </div>
   );
 }
