7aeae7c492659a6276a45133b480284f422bc95d
Bulaev Denis <bulaevdenis7@gmail.com>
2021-05-20 21:00:55 +0300

    types was replaced

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { User } from '../User/User';
+import { TypeUser } from '../../types';
 
 export const Todo = ({ title, completed, user }) => (
   <>
@@ -18,28 +19,9 @@ export const Todo = ({ title, completed, user }) => (
   </>
 );
 
-const TypeGeo = PropTypes.shape({
-  lat: PropTypes.string.isRequired,
-  lng: PropTypes.string.isRequired,
-});
-
-const TypeAddress = PropTypes.shape({
-  street: PropTypes.string.isRequired,
-  suite: PropTypes.string.isRequired,
-  city: PropTypes.string.isRequired,
-  zipcode: PropTypes.string.isRequired,
-  geo: TypeGeo,
-});
-
 Todo.propTypes = {
   title: PropTypes.string.isRequired,
   completed: PropTypes.bool.isRequired,
 
-  user: PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    name: PropTypes.string.isRequired,
-    username: PropTypes.string.isRequired,
-    email: PropTypes.string.isRequired,
-    address: TypeAddress,
-  }).isRequired,
+  user: TypeUser.isRequired,
 };
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Todo } from '../Todo/Todo';
+import { TypeTodos } from '../../types';
 
 export const TodoList = ({ todos }) => (
   <ul>
@@ -9,35 +10,6 @@ export const TodoList = ({ todos }) => (
     ))}
   </ul>
 );
-
-const TypeGeo = PropTypes.shape({
-  lat: PropTypes.string.isRequired,
-  lng: PropTypes.string.isRequired,
-});
-
-const TypeAddress = PropTypes.shape({
-  street: PropTypes.string.isRequired,
-  suite: PropTypes.string.isRequired,
-  city: PropTypes.string.isRequired,
-  zipcode: PropTypes.string.isRequired,
-  geo: TypeGeo,
-});
-
-const TypeUser = PropTypes.shape({
-  id: PropTypes.number.isRequired,
-  name: PropTypes.string.isRequired,
-  username: PropTypes.string.isRequired,
-  email: PropTypes.string.isRequired,
-  address: TypeAddress,
-});
-
-const TypeTodos = PropTypes.shape({
-  userId: PropTypes.number.isRequired,
-  id: PropTypes.number.isRequired,
-  title: PropTypes.string.isRequired,
-  completed: PropTypes.bool.isRequired,
-  user: TypeUser,
-});
 
 TodoList.propTypes = {
   todos: PropTypes.arrayOf(
