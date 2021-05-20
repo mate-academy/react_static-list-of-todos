27a93383637215d81d76c673941664ddbe4ab8e8
Vasyl Habalevych <vasylhab@gmail.com>
2021-05-20 10:32:16 +0200

    React static-list-of-todos

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import './App.scss';
-
+import { TodoList } from './components/TodoList/TodoList';
 import todos from './api/todos';
 import users from './api/users';
+
+const preparedTodos = todos.map((task) => {
+  const user = users.find(person => person.id === task.userId);
+
+  return {
+    ...task, user,
+  };
+});
 
 function App() {
   return (
     <div className="App">
       <h1>Static list of todos</h1>
-      <p>
-        <span>Todos: </span>
-        {todos.length}
-      </p>
-
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
+      <TodoList todos={preparedTodos} />
     </div>
   );
 }
