d4cfd9ceb0a0422f1dc466002917b6544d1a5f16
Yurii Lisovenko <yriyliso@gmail.com>
2021-05-20 12:10:11 +0300

    created todos-list

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import './App.scss';
+import TodoList from './components/TodoList';
 
 import todos from './api/todos';
 import users from './api/users';
+
+const preparedTodos = todos.map(todo => ({
+  ...todo,
+  user: users.find(user => user.id === todo.userId),
+}));
 
 function App() {
   return (
     <div className="App">
       <h1>Static list of todos</h1>
       <p>
-        <span>Todos: </span>
+        <span>TodoList: </span>
         {todos.length}
       </p>
 
@@ -17,6 +23,13 @@ function App() {
         <span>Users: </span>
         {users.length}
       </p>
+
+      <span>Todos: </span>
+
+      {preparedTodos.map(el => (
+        <TodoList {...el} />
+      ))}
+
     </div>
   );
 }
