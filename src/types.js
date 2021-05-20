378cd35b7bd18d1675b1b1c0634393b44e7d13e3
Bulaev Denis <bulaevdenis7@gmail.com>
2021-05-20 21:02:02 +0300

    types.js added

---
+++ src/types.js
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+
+const TypeGeo = PropTypes.shape({
+  lat: PropTypes.string.isRequired,
+  lng: PropTypes.string.isRequired,
+});
+
+const TypeAddress = PropTypes.shape({
+  street: PropTypes.string.isRequired,
+  suite: PropTypes.string.isRequired,
+  city: PropTypes.string.isRequired,
+  zipcode: PropTypes.string.isRequired,
+  geo: TypeGeo,
+});
+
+export const TypeUser = PropTypes.shape({
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  address: TypeAddress,
+});
+
+export const TypeTodos = PropTypes.shape({
+  userId: PropTypes.number.isRequired,
+  id: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  completed: PropTypes.bool.isRequired,
+  user: TypeUser,
+});
