8bfd55c25a36174c166c7a0e21bb40dc32967ece
Eva <eva@MacBook-Air-Eva.local>
2021-05-20 09:11:57 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import './App.scss';
 
 import todos from './api/todos';
 import users from './api/users';
+import { TodoList } from './components/TodoList/TodoList';
+
+const preparedTodos = todos.map((todo) => {
+  const user = users.find(person => person.id === todo.userId);
+
+  const copy = { ...todo };
+
+  copy.user = user;
+
+  return copy;
+});
 
 function App() {
   return (
     <div className="App">
       <h1>Static list of todos</h1>
-      <p>
-        <span>Todos: </span>
-        {todos.length}
-      </p>
-
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
+      <TodoList todosArr={preparedTodos} />
     </div>
   );
 }
