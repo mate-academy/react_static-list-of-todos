dbd4f21f285d5ec734a49738e4f7aea6fd30597b
Eva <eva@MacBook-Air-Eva.local>
2021-05-23 15:52:41 +0300

    requested changes

--- src/App.js
+++ src/App.js
@@ -5,21 +5,16 @@ import todos from './api/todos';
 import users from './api/users';
 import { TodoList } from './components/TodoList/TodoList';
 
-const preparedTodos = todos.map((todo) => {
-  const user = users.find(person => person.id === todo.userId);
-
-  const copy = { ...todo };
-
-  copy.user = user;
-
-  return copy;
-});
+const preparedTodos = todos.map(todo => ({
+  ...todo,
+  user: users.find(user => user.id === todo.userId),
+}));
 
 function App() {
   return (
     <div className="App">
       <h1>Static list of todos</h1>
-      <TodoList todosArr={preparedTodos} />
+      <TodoList todos={preparedTodos} />
     </div>
   );
 }
--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -2,26 +2,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { User } from '../User/User';
 
-export const Todo = ({ title, completed, user }) => {
-  const status = (completed)
-    ? 'done'
-    : 'not completed yet';
-
-  return (
-    <>
-      <h2>
-        {title}
-      </h2>
-      <User {...user} />
-      <p>
-        {`Status: ${status}`}
-      </p>
-    </>
-  );
-};
+export const Todo = ({
+  title,
+  completed,
+  user,
+}) => (
+  <>
+    <h2 className="title">
+      {title}
+    </h2>
+    <p className="status">
+      {completed ? (
+        <span className="status__true">True</span>
+      ) : (
+        <span className="status__false">False</span>
+      )}
+    </p>
+    <User {...user} />
+  </>
+);
 
 Todo.propTypes = {
   title: PropTypes.string.isRequired,
   completed: PropTypes.bool.isRequired,
-  user: PropTypes.shape({}).isRequired,
+  user: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+  }).isRequired,
 };
