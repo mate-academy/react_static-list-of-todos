f6d165a4153f67c23cf2de91de57c61088eae359
Tolik <tolikdolinskiy@gmail.com>
2021-07-03 16:24:51 +0300

    some text

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { TodoList } from './components/TodoList/TodoList';
 import './App.scss';
 
 import todos from './api/todos';
 import users from './api/users';
+
+const preparedTodos = [...todos.map((todo) => {
+  todo.user = users.find(user => user.id === todo.userId);
+
+  return todo;
+})];
 
 function App() {
   return (
@@ -12,11 +19,11 @@ function App() {
         <span>Todos: </span>
         {todos.length}
       </p>
-
       <p>
         <span>Users: </span>
         {users.length}
       </p>
+      <TodoList listTodo={preparedTodos} />
     </div>
   );
 }
