62e3fce21f03de152d9a7512e9911643282cd28d
Oleksii Blezniuk <oleksii.blezniuk@gmail.com>
2021-07-18 17:51:07 +0300

    task is completed

--- src/App.js
+++ src/App.js
@@ -2,21 +2,13 @@ import React from 'react';
 import './App.scss';
 
 import todos from './api/todos';
-import users from './api/users';
+import TodoList from './components/TodoList/TodoList';
 
 function App() {
   return (
     <div className="App">
-      <h1>Static list of todos</h1>
-      <p>
-        <span>Todos: </span>
-        {todos.length}
-      </p>
-
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
+      <h1 className="title">Static list of todos</h1>
+      <TodoList preparedTodos={todos} />
     </div>
   );
 }
