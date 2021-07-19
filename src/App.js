858a84ff13dad967193ef3651f23450f649f4f81
Sasha Kozlovskyy <alex33507@gmail.com>
2021-07-19 15:21:08 +0300

    React_todos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import './App.scss';
-
+import { TodosList } from './components/TodosList/TodosList';
 import todos from './api/todos';
 import users from './api/users';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Static list of todos</h1>
-      <p>
-        <span>Todos: </span>
-        {todos.length}
-      </p>
+const preparedTodos = todos.map((item) => {
+  const todo = { ...item };
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
+  todo.user = users.find(person => person.id === todo.userId);
+
+  return todo;
+});
+
+const App = () => (
+  <div className="App">
+    <h1>Static list of todos</h1>
+    <div>
+      <TodosList className="list" todos={preparedTodos} />
     </div>
-  );
-}
+  </div>
+);
 
 export default App;
