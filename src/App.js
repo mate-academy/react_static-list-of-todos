ef2dac08db98f7dd1a7bc49831a45ab5b9c42e25
vlepekha <vlepekha@student.ucode.world>
2021-07-19 16:21:58 +0300

    addressed comments

--- src/App.js
+++ src/App.js
@@ -9,14 +9,14 @@ const preparedTodos = todos.map(todo => ({
   ...todo,
   user: users.find(person => (
     person.id === todo.userId
-  )).name,
+  )),
 }));
 
 function App() {
   return (
     <div className="App">
       <h1>Static list of todos</h1>
-      <TodoList entries={preparedTodos} />
+      <TodoList todos={preparedTodos} />
     </div>
   );
 }
--- src/components/todoList/TodoList.js
+++ src/components/todoList/TodoList.js
@@ -3,18 +3,22 @@ import './TodoList.scss';
 import PropTypes from 'prop-types';
 import { Todo } from '../todo';
 
-export const TodoList = ({ entries }) => (
+export const TodoList = ({ todos }) => (
   <ul className="list">
-    {entries.map(entry => (
-      <li className="card" key={entry.id}>
-        <Todo {...entry} />
+    {todos.map(todo => (
+      <li className="card" key={todo.id}>
+        <Todo
+          title={todo.title}
+          completed={todo.completed}
+          user={todo.user.name}
+        />
       </li>
     ))}
   </ul>
 );
 
 TodoList.propTypes = {
-  entries: PropTypes.arrayOf(
+  todos: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.number.isRequired,
     }),
