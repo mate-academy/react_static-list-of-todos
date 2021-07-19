f59c8ca598f8e438a63a30e4adf5667534a41ac0
Sergiy Fartushniak <fartux97@gmail.com>
2021-07-19 23:37:56 +0300

    list todo first try

--- src/App.js
+++ src/App.js
@@ -3,20 +3,21 @@ import './App.scss';
 
 import todos from './api/todos';
 import users from './api/users';
+import { TodoList } from './components/TodoList/TodoList';
+
+const preparedTodos = todos.map((todo) => {
+  const todoCopy = { ...todo };
+
+  todoCopy.user = users.find(user => user.id === todo.userId);
+
+  return todoCopy;
+});
 
 function App() {
   return (
     <div className="App">
-      <h1>Static list of todos</h1>
-      <p>
-        <span>Todos: </span>
-        {todos.length}
-      </p>
-
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
+      <h1 className="display-4">Static list of todos</h1>
+      <TodoList todos={preparedTodos} />
     </div>
   );
 }
---
+++ src/components/TodoList/TodoList.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Todo } from '../Todo';
+
+export const TodoList = ({ todos }) => (
+  <ul className="list-group">
+    {todos.map(todo => (
+      <li className="list-group-item list-inline" key={todo.id}>
+        <Todo {...todo} />
+      </li>
+    ))}
+  </ul>
+);
+
+TodoList.propTypes = {
+  todos: PropTypes.arrayOf(PropTypes.shape({
+    userId: PropTypes.number.isRequired,
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    completed: PropTypes.bool.isRequired,
+  })).isRequired,
+};
