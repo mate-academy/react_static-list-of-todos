1bb0dc2c7c25c1240cde4a7129b52d425a1764fb
pavlo1983 <39806950+pavlo1983@users.noreply.github.com>
2021-07-23 13:34:14 +0300

    Update src/components/Todo/Todo.jsx
    Co-authored-by: ArtemTopolskyi <57574816+ArtemTopolskyi@users.noreply.github.com>

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { User } from '../User';
 import { TodoShape } from '../../shapes';
 
-export const Todo = ({ todoGroup }) => (
+export const Todo = ({ todo }) => (
   <>
     <div className="item">
       <h3>{todoGroup.title}</h3>
