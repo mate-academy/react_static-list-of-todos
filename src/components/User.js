cc5aa200cfdad86670bb17202c1da30320c93b63
Kostrica <romiorw@ukr.net>
2021-08-04 19:02:33 +0300

    I deletet fragment in component User

--- src/components/User.js
+++ src/components/User.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
 const User = ({ name }) => (
-  <>
     <p className="name">
       user name
       {' - '}
       <strong>{name}</strong>
     </p>
-  </>
 );
 
 export default User;
