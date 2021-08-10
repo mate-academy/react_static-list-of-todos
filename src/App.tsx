67f2833775e4e45459610e3d28ddaa6d7d4471d2
mgrinko <glukym@gmail.com>
2021-08-10 10:09:32 +0300

    add typescript

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+
+// import todos from './api/todos';
+// import users from './api/users';
+
+const App: React.FC = () => (
+  <div className="App">
+    <h1>Static list of todos</h1>
+  </div>
+);
+
+export default App;
