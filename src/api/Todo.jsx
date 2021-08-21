22b1eb6b5ee45938b51093d6af707cd14222455b
Андрій Наливайко <andrijnalivajko@MacBook-Pro-Andrij.local>
2021-08-22 00:18:18 +0300

    Fixed

--- src/api/Todo.jsx
+++ src/api/Todo.jsx
@@ -14,15 +14,13 @@ function Todo({ user }) {
         {user.title}
       </span>
 
-      <User people={user.person} />
+      <User person={user.person} />
     </>
   );
 }
 
 Todo.propTypes = {
   user: PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    userId: PropTypes.number.isRequired,
     completed: PropTypes.bool.isRequired,
     title: PropTypes.string.isRequired,
     person: PropTypes.shape({
@@ -31,26 +29,6 @@ Todo.propTypes = {
       email: PropTypes.string.isRequired,
       phone: PropTypes.string.isRequired,
       website: PropTypes.string.isRequired,
-
-      address: PropTypes.shape({
-        street: PropTypes.string.isRequired,
-        suite: PropTypes.string.isRequired,
-        city: PropTypes.string.isRequired,
-        zipcode: PropTypes.string.isRequired,
-
-        geo: PropTypes.shape({
-          lat: PropTypes.string.isRequired,
-          lng: PropTypes.string.isRequired,
-        }),
-
-        company: PropTypes.shape({
-          name: PropTypes.string.isRequired,
-          catchPhrase: PropTypes.string.isRequired,
-          bs: PropTypes.string.isRequired,
-        }),
-
-      }).isRequired,
-
     }).isRequired,
   }).isRequired,
 };
