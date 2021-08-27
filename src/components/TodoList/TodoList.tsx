5c5aa0c99ce096946ae2a85759a4c13b3f043e8b
lera-yaskevych <redrum.back@gmail.com>
2021-08-27 19:42:18 +0300

    change in todolist

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -13,7 +13,12 @@ const TodoList: React.FC<Props> = ({ tasksToDo }) => (
   <div className="task-table">
     <ul className="task-table__list">
       {tasksToDo.map((toDo: PreparedToDos) => (
-        <li key={toDo.id} className={toDo.completed ? 'task-table__item task-table__item--true' : 'task-table__item task-table__item--false'}>
+        <li
+          key={toDo.id}
+          className={toDo.completed
+            ? 'task-table__item task-table__item--true'
+            : 'task-table__item task-table__item--false'}
+        >
           <div className="task-table__user-info">
             <UserInfo user={toDo.user} />
           </div>
