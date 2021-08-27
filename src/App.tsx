d00eb8a38ebca61417079938772ffda6558e4ab0
Moroz Dmytro <dmytro101100@gmail.com>
2021-08-27 21:04:56 +0300

    The task has been done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.scss';
 
-// import todos from './api/todos';
-// import users from './api/users';
+import todos from './api/todos';
+import users from './api/users';
+
+import { TodoList } from './TodoList';
+
+const preparedTodos = todos.map((todo) => {
+  const user = users.find(person => person.id === todo.userId) || null;
+  const preparedTodo = { ...todo, user };
+
+  return preparedTodo;
+});
 
 const App: React.FC = () => (
   <div className="App">
-    <h1>Static list of todos</h1>
+    <h1> List of Todos</h1>
+    <TodoList toDos={preparedTodos} />
   </div>
 );
 
