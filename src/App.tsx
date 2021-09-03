8df527bc1b4fb4ebe6d4f2da9ec50c25c5be21d7
Andriy Nikorych <andrei,nikorich@gmail.com>
2021-09-03 14:32:32 +0300

    solution Todos without CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import './App.scss';
 
-// import todos from './api/todos';
-// import users from './api/users';
+import todos from './api/todos';
+import users from './api/users';
+import { TodoList } from './components/Todo/TodoList';
+
+const preparedTodos = todos.map(todo => ({
+  ...todo,
+  user: users.find(user => user.id === todo.userId) || null,
+}));
 
 const App: React.FC = () => (
   <div className="App">
-    <h1>Static list of todos</h1>
+    <div className="list__name">Name</div>
+    <div className="list__userName">UserName</div>
+    <div className="list__email">Email</div>
+    <div className="list__title">Title</div>
+    <div className="list__Status">Status</div>
+
+    <TodoList todos={preparedTodos} />
   </div>
 );
 
