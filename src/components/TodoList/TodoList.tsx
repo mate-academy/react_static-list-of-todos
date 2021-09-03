389c4dd2111f12e1c44a147a1910bf476a0378fb
Volodymyr Khimiak <vv.himiak@gmail.com>
2021-09-03 15:18:32 +0300

    third commit

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { TodoInfo } from '../TodoInfo';
 import { UserInfo } from '../UserInfo';
 
+import './todoList.scss';
+
 type Props = {
   preparedTodos: TodoItem[];
 };
@@ -12,7 +14,7 @@ export const TodoList: React.FC<Props> = (props) => {
   return (
     <>
       {preparedTodos.map(todo => (
-        <ul key={todo.id}>
+        <ul key={todo.id} className="todoList">
           <TodoInfo todo={todo} />
           <li className="userInfo">{!todo.user ? 'null' : <UserInfo user={todo.user} />}</li>
         </ul>
