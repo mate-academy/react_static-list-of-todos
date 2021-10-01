676b9d32209ac1cce2d30287fd0391ffe5a508ba
Aleksey Tsybulko <tsybulko.aleksey@gmail.com>
2021-10-01 21:01:29 +0300

    solution

---
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+type Props = {
+  todo: Todo;
+};
+
+export const TodoInfo: React.FC<Props> = (props) => {
+  const { todo } = props;
+  const { title, completed } = todo;
+
+  return (
+    <>
+      <p>
+        {title}
+      </p>
+      <p>
+        {completed ? 'Completed' : 'In progress'}
+      </p>
+    </>
+  );
+};
