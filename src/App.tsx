9361d218d01e39782f7fa98cf49b8eedf162a4d4
Victor Shevchuk <callmevitok@gmail.com>
2021-10-09 17:01:36 +0300

    v01

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.scss';
 
-// import todos from './api/todos';
-// import users from './api/users';
+import todos from './api/todos';
+import users from './api/users';
+import { TodoList } from './components/TodoList';
+import { Todo } from './types/Todo';
+
+const preparedTodos: Todo[] = todos.map((todo) => {
+  const user = users.find(person => person.id === todo.userId) || null;
+
+  return Object.assign(todo, { user });
+});
 
 const App: React.FC = () => (
   <div className="App">
     <h1>Static list of todos</h1>
+    <TodoList todos={preparedTodos} />
   </div>
 );
 
