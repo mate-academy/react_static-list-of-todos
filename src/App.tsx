92a6d9f698fe481c5e8d5f117524df59795a03a6
Zahar Mychka <zikmychkas@gmail.com>
2021-10-11 14:08:16 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import './App.scss';
+import { TodoList } from './components/TodoList/TodoList';
+import { TodoTypes } from './types/TodoTypes';
+import todos from './api/todos';
+import users from './api/users';
 
-// import todos from './api/todos';
-// import users from './api/users';
+const preparedTodos: TodoTypes[] = todos.map(item => {
+  const userIndex = users.find(({ id }) => item.userId === id) || null;
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Static list of todos</h1>
-  </div>
-);
+  return {
+    ...item,
+    user: userIndex,
+  };
+});
+
+const App: React.FC = () => {
+  return (
+    <div className="todo">
+      <h1 className="todo__title">Static list of todos</h1>
+      {/* <div className="todo__legend">
+        <span className="todo__legend-item">Name</span>
+        <span className="todo__legend-item">Description</span>
+        <span className="todo__legend-item">Status</span>
+        <span className="todo__legend-item">Email</span>
+      </div> */}
+      <TodoList todoLists={preparedTodos} />
+    </div>
+  );
+};
 
 export default App;
