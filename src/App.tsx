e4a2f1d63e0620715e9ffeafe193604a21dcb6f5
Loza Marusa <marusa.loza@gmail.com>
2021-10-11 21:36:02 +0300

    react static list of todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.scss';
 
-// import todos from './api/todos';
-// import users from './api/users';
+import todos from './api/todos';
+import users from './api/users';
+import { ToDo } from './types/Todo';
+import { TodoList } from './todoList/TodoList';
+
+const preparedTodos: ToDo[] = todos.map(todo => {
+  const user = users.find(person => person.id) || null;
+
+  return { ...todo, user };
+});
 
 const App: React.FC = () => (
   <div className="App">
-    <h1>Static list of todos</h1>
+    <h1 className="app">Static list of todos</h1>
+    <TodoList todos={preparedTodos} />
   </div>
 );
 
