031c38757b4c0a399cb85564416f70132a1929e2
anastasiia <theyashkindzhus@gmail.com>
2021-10-12 12:52:00 +0300

    edited

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import todos from './api/todos';
+import users from './api/users';
 import './App.scss';
 
 import { TodoList } from './components/TodoList';
-import { preparedTodos } from './types/preparedTodos';
+
+const preparedTodos = todos.map(todo => ({
+  ...todo,
+  user: users.find(user => user.id === todo.userId) || null,
+}));
 
 const App: React.FC = () => (
   <div className="App">
--- src/types/preparedTodos.tsx
+++
@@ -1,7 +0,0 @@
-import todos from '../api/todos';
-import users from '../api/users';
-
-export const preparedTodos = todos.map(todo => ({
-  ...todo,
-  user: users.find(user => user.id === todo.userId) || null,
-}));
