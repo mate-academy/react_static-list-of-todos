48987935e195d7ab728a1ff3abf756894a07dbbf
Roman Toritsyn <romantoritsyn@gmail.com>
2021-10-13 11:50:47 +0300

    UserInfo added

--- src/components/TodoCard/TodoCard.tsx
+++ src/components/TodoCard/TodoCard.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Todo } from '../Types/Todo';
+import { UserInfo } from '../UserInfo/UserInfo';
 
 export const TodoCard: React.FC<{ todo: Todo; }> = ({ todo }) => (
   <div className="card">
     <span className="card__title">{todo.title}</span>
     <span className="card__person">
-      {todo.user?.name}
-      {' --- '}
-      {todo.user?.phone}
+      <UserInfo user={todo.user} />
     </span>
   </div>
 );
--- src/components/Types/Todo.tsx
+++ src/components/Types/Todo.tsx
@@ -1,5 +1,4 @@
-import { User } from "./User";
-
+import { User } from './User';
 
 export interface Todo {
   userId: number;
