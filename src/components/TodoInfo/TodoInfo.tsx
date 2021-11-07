1f69abfa84b2abfcc93aa6d90d948655184e94a5
Pavel Podlesnyi <podlesnyi.pavel.v@gmail.com>
2021-11-07 21:21:36 +0200

    remove | null and ? x 2

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -2,12 +2,12 @@ import './TodoInfoStyle.scss';
 import { TodoItem } from '../../types/TodoItem';
 
 type Props = {
-  todo: TodoItem | null;
+  todo: TodoItem;
 };
 
 export const TodoInfo: React.FC<Props> = ({ todo }) => (
   <>
-    <div>{todo?.title}</div>
-    <div>{todo?.completed ? 'completed' : 'not completed'}</div>
+    <div>{todo.title}</div>
+    <div>{todo.completed ? 'completed' : 'not completed'}</div>
   </>
 );
