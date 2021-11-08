aaaf527a21c44e4503a183c8bb2412f2e331553e
Pavel Podlesnyi <podlesnyi.pavel.v@gmail.com>
2021-11-08 10:42:44 +0200

    rename task to todo

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ import users from './api/users';
 import { TodoList } from './components/TodoList';
 import { TodoItem } from './types/TodoItem';
 
-const prepareTodos: TodoItem[] = todos.map(task => ({
-  ...task,
-  user: users.find(user => user.id === task.userId) || null,
+const prepareTodos: TodoItem[] = todos.map(todo => ({
+  ...todo,
+  user: users.find(user => user.id === todo.userId) || null,
 }));
 
 const App: React.FC = () => (
