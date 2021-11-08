a7284e10230d0a72c52f71ea81ddff2b8d672309
Anton Karpena <antoniokarpena@gmail.com>
2021-11-08 14:59:41 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import todos from './api/todos';
+import users from './api/users';
 import './App.scss';
+import { TodoList } from './components/TodoList';
 
 // import todos from './api/todos';
 // import users from './api/users';
+const preparedTodos = todos.map((todo) => ({
+  ...todo,
+  user: users.find((user) => user.id === todo.userId) || null,
+}));
 
 const App: React.FC = () => (
   <div className="App">
-    <h1>Static list of todos</h1>
+    <header className="header">
+      <h1 className="header__title">Static list of todos</h1>
+    </header>
+    <TodoList todos={preparedTodos} />
   </div>
 );
 
