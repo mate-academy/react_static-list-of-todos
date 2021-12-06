faefe33eb417cf346eb8870238ef468478dadcbd
lanebx <havchikos@gmail.com>
2021-12-06 23:25:48 +0200

    app block changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
 import React from 'react';
 import './App.scss';
+import { TodoList } from './components/TodoList';
+import usersFromServer from './api/users';
+import todosFromServer from './api/todos';
 
-// import todos from './api/todos';
-// import users from './api/users';
+const preparedTodos = todosFromServer.map((todo) => {
+  const userObj = usersFromServer.find((user) => user.id === todo.userId) || null;
+  let userEmail;
+
+  if (userObj !== null) {
+    userEmail = userObj.email;
+  } else {
+    userEmail = 'null';
+  }
+
+  const todoWithOwner = {
+    ...todo,
+    user: userEmail,
+  };
+
+  return todoWithOwner;
+});
 
 const App: React.FC = () => (
   <div className="App">
     <h1>Static list of todos</h1>
+    <TodoList todos={preparedTodos} />
   </div>
 );
 
