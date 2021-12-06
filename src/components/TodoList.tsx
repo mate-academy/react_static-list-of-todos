ddabbad5ef592c22929ed4268022ed3b5ceff76f
lanebx <havchikos@gmail.com>
2021-12-06 23:26:26 +0200

    TodoList block created

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { ITodo } from '../types/ITodo';
+import './TodoList.scss';
+import { Todo } from './Todo';
+
+type Props = {
+  todos: ITodo[],
+};
+
+export const TodoList: React.FC<Props> = ({ todos = [] }) => (
+  <div className="todos">
+    {todos.map(todo => (
+      <Todo
+        key={todo.id}
+        completed={todo.completed}
+        title={todo.title}
+        avtor={todo.user}
+      />
+    ))}
+  </div>
+);
