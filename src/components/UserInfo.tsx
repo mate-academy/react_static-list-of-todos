de50c3769730eea98d889877957a06d7263be8ac
Andriy Bulbuk <bulbuk.andriy@gmail.com>
2021-12-07 22:21:22 +0200

    Fixed user type

--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -3,9 +3,7 @@ import './UserInfo.scss';
 
 import { User } from '../types/User';
 
-type UserInfoList = Pick<User, 'name' | 'email'>;
-
-export const UserInfo: React.FC<UserInfoList> = ({ name, email }) => (
+export const UserInfo: React.FC<User> = ({ name, email }) => (
   <div className="userInfo">
     <span>{name}</span>
     <span className="userInfo__email">{email}</span>
