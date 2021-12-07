546ab19655f0971311a01bb01a41edf1db0c7cb4
Ihor Poliakov <48531156+junglq@users.noreply.github.com>
2021-12-07 15:43:22 +0200

    try one

---
+++ src/types.ts
@@ -0,0 +1,12 @@
+export interface User {
+  name: string;
+  email: string;
+}
+
+export interface Todo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+  user: User | null;
+}
