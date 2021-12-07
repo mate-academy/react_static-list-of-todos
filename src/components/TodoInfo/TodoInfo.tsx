c6a70c0e5ca78f0f7c47537e848100534293171a
Volodymyr Soltys <soltus.vovan@gmail.com>
2021-12-07 14:13:41 +0200

    Solution

---
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo/UserInfo';
+import './TodoInfo.scss';
+
+type Props = {
+  todo: Todo;
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  return (
+    <>
+      <h2 className="todoHeading">{todo.title}</h2>
+      <input type="checkbox" checked={todo.completed} />
+      {todo.user && <UserInfo user={todo.user} />}
+    </>
+  );
+};
