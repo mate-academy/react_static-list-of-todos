29f4c38d86fca645605227b8f3cadf47a79a54ab
nadiaH <nadiahrb79@gmailcom>
2021-12-08 02:29:00 +0200

    some styles

--- src/components/TodoInfo.tsx
+++ src/components/TodoInfo.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { UserInfo } from './UserInfo';
 import { Todo } from '../types/Todo';
+import './TodoInfo.scss';
 
 type Props = Omit<Todo, 'userId' | 'id'>;
 
@@ -8,10 +9,8 @@ export const TodoInfo: React.FC<Props> = ({
   title, completed, user,
 }) => (
   <>
-    <input className="check" type="checkbox" checked={completed} />
-    <h2 className="title">
-      {title}
-    </h2>
+    <p className={completed ? 'to-do' : 'done'}>{completed ? 'done' : 'to do'}</p>
+    <h2>{title}</h2>
     {user && <UserInfo name={user.name} email={user.email} />}
   </>
 );
