69b13a619092c5ed0efe27254115ad941f8017e2
KHTRE <KHTREE@gmail.com>
2022-01-10 18:46:04 +0200

    works

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,68 @@
 import React from 'react';
 import './App.scss';
+import todos from './api/todos';
+import users from './api/users';
+import { TodoList } from './components/TodoList';
+import './types/types';
 
-// import todos from './api/todos';
-// import users from './api/users';
+// interface Geo {
+//   lat: string,
+//   lng: string,
+// }
 
-const App: React.FC = () => (
+// interface Address {
+//   street: string,
+//   suite: string,
+//   city: string,
+//   zipcode: string,
+//   geo: Geo,
+// }
+
+// interface Company {
+//   name: string,
+//   catchPhrase: string,
+//   bs: string,
+// }
+
+// interface User {
+//   id: number,
+//   name: string,
+//   username: string,
+//   email: string,
+//   address: Address,
+//   phone: string,
+//   website: string,
+//   company: Company,
+// }
+
+// interface Todo {
+//   userId: number,
+//   id: number,
+//   title: string,
+//   completed: boolean,
+// }
+
+// interface PreparedTodo extends Todo {
+//   user: User | null;
+// }
+
+const preparedTodos: PreparedTodo[] = todos.map((todo: Todo) => {
+  const correctUser = users.find((user) => (user.id === todo.userId));
+
+  const prepared: PreparedTodo = {
+    userId: todo.userId,
+    id: todo.id,
+    title: todo.title,
+    completed: todo.completed,
+    user: correctUser || null,
+  };
+
+  return prepared;
+});
+
+const App: React.FC<User> = () => (
   <div className="App">
-    <h1>Static list of todos</h1>
+    <TodoList todo={preparedTodos} />
   </div>
 );
 
--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,5 +1,4 @@
 /* eslint-disable max-len */
-
 const todos = [
   {
     userId: 1,
