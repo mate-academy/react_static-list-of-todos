70475513c2237b3739ffbcaaa7fcf04d3ec6f717
Samson Sim <samson.sim@ukr.net>
2022-01-12 16:16:55 +0200

    solution

---
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -0,0 +1,17 @@
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo/UserInfo';
+import './todoInfo.scss';
+
+type Props = {
+  todo: Todo;
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => (
+  <div className="todo">
+    <UserInfo user={todo.user} />
+    <p className="todo__title">{`Todo: ${todo.title}`}</p>
+    {todo.completed
+      ? <span className="todo__status-completed">Completed</span>
+      : <span className="todo__status-notcompleted">Not completed</span>}
+  </div>
+);
---
+++ src/components/UserInfo/UserInfo.tsx
@@ -0,0 +1,15 @@
+import { User } from '../../types/User';
+import './userInfo.scss';
+
+type Props = {
+  user: User | null,
+};
+
+export const UserInfo: React.FC<Props> = ({ user }) => (
+  user && (
+    <div className="user">
+      <span className="user__name">{`${user.name}. `}</span>
+      <span className="user__email">{`Email: ${user.email}`}</span>
+    </div>
+  )
+);
