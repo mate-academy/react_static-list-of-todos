37fa274440fbf1846bc5359f2a6a299cfcee65d1
Vova Alputov <alputov1998@gmail.com>
2022-01-17 23:03:46 +0200

    done

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,11 @@ import './App.scss';
 import todos from './api/todos';
 import users from './api/users';
 import { TodoList } from './components/TodoList/TodoList';
-import { Todo } from './types/Todo';
+import { Props } from './types/Props';
 
 const preparedTodos = todos.map(todo => Object.assign(todo, {
   user: users.find(user => user.id === todo.userId || null),
 }));
-
-type Props = {
-  todoos: Todo[];
-};
 
 const App: React.FC<Props> = () => (
   <div className="App">
