31e1b12169f2748e3d2ddc45cfcad7ad67da6e5f
Aleksandra Mileant <aleksandramileant1@gmail.com>
2022-01-17 20:22:03 +0100

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -12,10 +12,10 @@ const preparedTodos: Todo[] = todos.map((todo) => ({
 }));
 
 const App: React.FC = () => (
-  <div className="App">
+  <>
     <h1>Static list of todos</h1>
     <TodoList todos={preparedTodos} />
-  </div>
+  </>
 );
 
 export default App;
--- src/TodoInfo/TodoInfo.tsx
+++ src/TodoInfo/TodoInfo.tsx
@@ -15,7 +15,7 @@ export const TodoInfo: React.FC<Todo> = ({
       {completed ? (
         <h3 className="card__completed">completed</h3>
       ) : (
-        <h3 className="card__notcompleted">not completed </h3>
+        <h3 className="card__notcompleted">not completed</h3>
       )}
     </div>
     {user && (<UserInfo {...user} />)}
