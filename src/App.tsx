3920b92e3fad71c18c89eb6124fcd5fc9f897508
Marina Tilniak <marinatilnak@gmail.com>
2022-01-17 23:35:14 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import './App.scss';
+import 'bulma/css/bulma.min.css';
 
-// import todos from './api/todos';
-// import users from './api/users';
+import todos from './api/todos';
+import users from './api/users';
+import { TodoList } from './components/TodoList';
+
+const preparedTodos: PreparedTodo[] = todos.map(
+  todo => ({
+    ...todo,
+    user: users.find(user => user.id === todo.userId) || null,
+  }),
+);
 
 const App: React.FC = () => (
-  <div className="App">
-    <h1>Static list of todos</h1>
-  </div>
+  <TodoList todos={preparedTodos} />
 );
 
 export default App;
