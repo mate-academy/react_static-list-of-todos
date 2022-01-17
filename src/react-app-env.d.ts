e6cd0c9d5a81f11f31b2f18b7404ea6fff020d38
Mykola Koval <zavul0n.ua@gmail.com>
2022-01-17 15:25:10 +0200

    solution

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,30 @@
 /// <reference types="react-scripts" />
+
+interface Todo {
+  userId: number,
+  id: number,
+  title: string,
+  completed: boolean,
+  user: User | null,
+}
+
+interface User {
+  id: number,
+  name: string,
+  username: string,
+  email: string,
+  address: Address,
+}
+
+interface Address {
+  street: string,
+  suite: string,
+  city: string,
+  zipcode: str,
+  geo: Geo,
+}
+
+interface Geo {
+  lat: string,
+  lng: string,
+}
