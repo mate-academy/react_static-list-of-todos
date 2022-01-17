e89b74f12128df1888cb61aed0e2e94facfcd520
Taras Zherebetskiy <tarasj76@gmail.com>
2022-01-17 15:58:18 +0200

    list of todos

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,33 @@
 /// <reference types="react-scripts" />
+
+interface Todo implements User {
+  userId: number,
+  id: number,
+  title: string,
+  completed: boolean,
+  user: User | null,
+}
+
+interface User {
+  id: number,
+  name: string,
+  username: string,
+  email: string,
+  address: {
+    street: string,
+    suite: string,
+    city: string,
+    zipcode: string,
+    geo: {
+      lat: string,
+      lng: string,
+    },
+  },
+  phone: string,
+  website: string,
+  company: {
+    name: string,
+    catchPhrase: string,
+    bs: string,
+  }
+}
