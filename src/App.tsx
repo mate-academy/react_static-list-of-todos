e88b0ae31ed2377e75776c0e26498334cfa7bc43
Nikita Polishchuk <theetempleovpsychicyouth@gmail.com>
2022-01-17 20:34:16 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.scss';
 
-// import todos from './api/todos';
-// import users from './api/users';
+import todos from './api/todos';
+import users from './api/users';
+
+import { TodoList } from './components/TodoList/TodoList';
+
+const preparedTodos: Todo[] = todos.map(task => ({
+  ...task,
+  user: users.find(user => user.id === task.userId) || null,
+}));
 
 const App: React.FC = () => (
-  <div className="App">
-    <h1>Static list of todos</h1>
+  <div className="App page">
+    <TodoList preparedTodos={preparedTodos} />
   </div>
 );
 
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="react-scripts" />
+
+interface Todo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+  user: User | null;
+}
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
