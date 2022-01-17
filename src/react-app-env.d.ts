318e457134a2637e477e750ba523a33377cca335
Vlad Harkusha <sivari888@gmail.com>
2022-01-17 15:11:53 +0200

    solution

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,18 @@
 /// <reference types="react-scripts" />
+
+interface Todo {
+  userId: number,
+  id: number,
+  title: string,
+  completed: boolean,
+}
+
+interface User {
+  id: number,
+  name: string,
+  email: string,
+}
+
+interface PrepearedTodo extends Todo {
+  user: User | null,
+}
