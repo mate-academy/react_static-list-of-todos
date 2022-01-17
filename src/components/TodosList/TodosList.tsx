4d60455607c8661bf771c8cf19f7421a95b1e0af
Alex Ivanov <bcmp4p1@gmail.com>
2022-01-17 21:04:06 +0200

    save1

--- src/components/TodosList/TodosList.tsx
+++ src/components/TodosList/TodosList.tsx
@@ -9,7 +9,7 @@ type Props = {
 export const TodosList: React.FC<Props> = ({ todos }) => (
   <ul className="todosList">
     {todos.map(todo => (
-      <li key={todo.id} className='todosList__item todo'>
+      <li key={todo.id} className="todosList__item todo">
         <TodoInfo {...todo} />
       </li>
     ))}
