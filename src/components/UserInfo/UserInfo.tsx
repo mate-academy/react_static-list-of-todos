77b212e8c8290f55ad61f609928e460f496b632f
Basil-Panasiuk <vasylpanasjuk2001@gmail.com>
2022-01-18 15:38:59 +0200

    refactoring

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
+import { User } from '../../types/User';
 
 import './UserInfo.scss';
 
-type Props = {
-  name: string;
-  email: string;
-};
+type Props = User;
 
 export const UserInfo: React.FC<Props> = ({ name, email }) => {
   return (
