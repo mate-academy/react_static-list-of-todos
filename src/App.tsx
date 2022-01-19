a88307ee3e60296e352f30718c926e632877d704
Iryna Zolko <koko_irina@ukr.net>
2022-01-19 03:42:52 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -13,20 +13,9 @@ export const preparedTodos: Todo[] = todos.map(todo => ({
 const App: React.FC = () => (
   <div className="page">
     <div className="page-content">
-      {/* <h1>Static list of todos</h1> */}
       <TodoList todos={preparedTodos} />
     </div>
   </div>
 );
 
 export default App;
-
-// {/* <div className="page">
-//     <div className="page-content">
-//       <MoviesList movies={moviesFromServer} />
-//     </div>
-
-//     <div className="sidebar">
-//       Sidebar will be here
-//     </div>
-//   </div> */}
---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './TodoList.scss';
+import { TodoInfo } from '../TodoInfo/TodoInfo';
+
+type Props = {
+  todos: Todo[],
+};
+
+export const TodoList: React.FC<Props> = ({ todos = [] }) => (
+  <div className="todos">
+    {todos.map(todo => (
+      <TodoInfo todo={todo} />
+    ))}
+  </div>
+);
