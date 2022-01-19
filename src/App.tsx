86f60dc29d741ecdf6311dfbc2c88f044380a408
Iryna Zolko <koko_irina@ukr.net>
2022-01-19 03:13:20 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import './App.scss';
 
-// import todos from './api/todos';
-// import users from './api/users';
+import todos from './api/todos';
+import users from './api/users';
+import { TodoList } from './components/TodoList/TodoList';
+
+export const preparedTodos: Todo[] = todos.map(todo => ({
+  ...todo,
+  user: users.find((user) => user.id === todo.userId) || null,
+}));
 
 const App: React.FC = () => (
-  <div className="App">
-    <h1>Static list of todos</h1>
+  <div className="page">
+    <div className="page-content">
+      {/* <h1>Static list of todos</h1> */}
+      <TodoList todos={preparedTodos} />
+    </div>
   </div>
 );
 
 export default App;
+
+// {/* <div className="page">
+//     <div className="page-content">
+//       <MoviesList movies={moviesFromServer} />
+//     </div>
+
+//     <div className="sidebar">
+//       Sidebar will be here
+//     </div>
+//   </div> */}
