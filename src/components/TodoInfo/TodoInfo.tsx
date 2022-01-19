de5ed75bf4bd18ded7c0dcc333e397ffda64f74b
Vova Alputov <alputov1998@gmail.com>
2022-01-19 15:23:08 +0200

    done

---
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import classname from 'classnames';
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo/UserInfo';
+import './TodoInfo.scss';
+
+export const TodoInfo: React.FC<{ todo: Todo; }> = ({ todo }) => (
+  <div
+    className={classname('todo-status', 'todo', {
+      'todo-status--completed': todo.completed,
+    })}
+    key={todo.id}
+  >
+    {todo.user && (
+      <UserInfo user={todo.user} />
+    )}
+    <h2 className="todo__task">{todo.title}</h2>
+    <h3>
+      {todo.completed ? ' Completed' : ' TO DO'}
+    </h3>
+  </div>
+);
