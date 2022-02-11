c540510611ef61098c7e210f2ebf44edd10d9c5d
Дима Краснобокий <krasnoboky.dima@gmail.com>
2022-02-11 10:46:59 +0200

    done...

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,62 @@
 import React from 'react';
 import './App.scss';
 
-// import todos from './api/todos';
-// import users from './api/users';
+import TodoList from './TodoList';
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Static list of todos</h1>
-  </div>
+import todos from './api/todos';
+import users from './api/users';
+
+interface User {
+  id: number,
+  name: string,
+  username: string,
+  email: string,
+  address: {
+    street: string,
+    suite: string,
+    city: string,
+    zipcode: string,
+    geo: {
+      lat: string,
+      lng: string,
+    },
+  },
+  phone: string,
+  website: string,
+  company: {
+    name: string,
+    catchPhrase: string,
+    bs: string,
+  },
+}
+
+interface NewItem {
+  userId: number,
+  id: number,
+  title: string,
+  completed: boolean,
+  user: User | null,
+}
+
+type Props = {
+  preparedTodos: NewItem[];
+};
+
+const preparedTodos = [...todos].map((item) => {
+  const newItem: NewItem = {
+    ...item,
+    user: users.find((person) => person.id === item.userId) || null,
+  };
+
+  return newItem;
+}).sort((a, b) => {
+  return a.userId - b.userId;
+});
+
+const App: React.FC<Props> = () => (
+  <TodoList
+    preparedTodos={preparedTodos}
+  />
 );
 
 export default App;
