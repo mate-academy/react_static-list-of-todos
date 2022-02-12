19c03a39c777187acc155654c56c681d3ef3ef2d
Misha Novikov <novikov1807@gmail.com>
2022-02-12 15:54:30 +0200

    Complite

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import TodoList from './components/TodoList';
 import './App.scss';
 
-// import todos from './api/todos';
-// import users from './api/users';
+import todos from './api/todos';
+import users from './api/users';
+
+const preparedTodos = todos
+  .map(todo => {
+    const user = users.find(u => u.id === todo.userId) || null;
+
+    return {
+      ...todo,
+      user,
+    };
+  });
 
 const App: React.FC = () => (
   <div className="App">
     <h1>Static list of todos</h1>
+    <TodoList todos={preparedTodos} />
   </div>
 );
 
---
+++ src/components/UserInfo/UserInfo.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type Props = {
+  name: string,
+  email: string,
+};
+
+export const UserInfo: React.FC<Props> = ({ name, email }) => (
+  <>
+    <p className="todos__name">{name}</p>
+    <p className="todos__email">{email}</p>
+  </>
+);
