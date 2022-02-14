0a8ace445119063576dbe82b77ecc2aa3675491c
Roma Boreiko <roma.boreiko@gmail.com>
2022-02-14 20:13:40 +0200

    Solution

---
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo/UserInfo';
+
+import './TodoInfo.scss';
+
+export const TodoInfo: React.FC<Todo> = ({ title, completed, user }) => {
+  const status = completed ? 'Completed' : 'Not completed';
+
+  return (
+    <div className="todo">
+      <p className="todo__title">{title}</p>
+      <span>{status}</span>
+      {user && <UserInfo {...user} />}
+    </div>
+  );
+};
