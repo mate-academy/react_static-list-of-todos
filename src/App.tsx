6b8092d78af778def5382d5f4c2e794917ed0904
Дима Краснобокий <krasnoboky.dima@gmail.com>
2022-02-15 16:30:05 +0200

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,18 @@
 import React from 'react';
 import './App.scss';
 
-import TodoList from './TodoList';
+import TodoList from './components/TodoList';
 
 import todos from './api/todos';
 import users from './api/users';
 
-interface User {
-  id: number,
-  name: string,
-  username: string,
-  email: string,
-  address: {
-    street: string,
-    suite: string,
-    city: string,
-    zipcode: string,
-    geo: {
-      lat: string,
-      lng: string,
-    },
-  },
-  phone: string,
-  website: string,
-  company: {
-    name: string,
-    catchPhrase: string,
-    bs: string,
-  },
-}
-
-interface NewItem {
-  userId: number,
-  id: number,
-  title: string,
-  completed: boolean,
-  user: User | null,
-}
+import { NewItem } from './components/interface/interface';
 
 type Props = {
   preparedTodos: NewItem[];
 };
 
-const preparedTodos = [...todos].map((item) => {
+const preparedTodos = todos.map((item) => {
   const newItem: NewItem = {
     ...item,
     user: users.find((person) => person.id === item.userId) || null,
