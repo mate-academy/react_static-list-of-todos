b00cf0cfa0fb3b91f04e6d62a3a321ce8f142ba9
Artem Paskal <artem.yogi@gmail.com>
2022-02-21 11:17:34 +0200

    Solution

--- src/Types/User.ts
+++ src/Types/User.ts
@@ -3,26 +3,4 @@ export interface User {
   name: string,
   username: string,
   email: string,
-
-  // id: number,
-  // name: string,
-  // username: string,
-  // email: string,
-  // address: {
-  //   street: string,
-  //   suite: string,
-  //   city: string,
-  //   zipcode: string,
-  //   geo: {
-  //     lat: string,
-  //     lng: string,
-  //   },
-  // },
-  // phone: string,
-  // website: string,
-  // company: {
-  //   name: string,
-  //   catchPhrase: string,
-  //   bs: string,
-  // },
 }
