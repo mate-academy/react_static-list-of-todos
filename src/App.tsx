3983da3083fae49b1093ca417810839753a6f0d4
CCyrillus <kyrill.kiril@gmail.com>
2022-02-22 22:54:50 +0200

    create html page, ande styled in sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.scss';
+import classNames from 'classnames';
 
 // import todos from './api/todos';
 // import users from './api/users';
@@ -7,6 +8,50 @@ import './App.scss';
 const App: React.FC = () => (
   <div className="App">
     <h1>Static list of todos</h1>
+    <ul className="todo-list">
+      <li className="todo-list__item">
+        <div className="todo-info">
+          <a className="todo-info__link" href="/">
+            <h2 className="todo-info__title">
+              Debitis Accusantium Ut Quo Facilis Nihil Quis Sapiente Necessitatibus
+            </h2>
+          </a>
+          <span className={classNames(
+            'todo-info__status',
+            {
+              'todo-info__status--done': 'completed',
+            },
+          )}
+          />
+        </div>
+        <div className="user-info">
+          <p className="user-info__item">Name</p>
+          <p className="user-info__item">UserName</p>
+          <p className="user-info__item">Email</p>
+        </div>
+      </li>
+      <li className="todo-list__item">
+        <div className="todo-info">
+          <a className="todo-info__link" href="/">
+            <h2 className="todo-info__title">
+              Dolorum Laboriosam Eos Qui Iure Aliquam
+            </h2>
+          </a>
+          <span className={classNames(
+            'todo-info__status',
+            {
+              'todo-info__status--done': 'completed',
+            },
+          )}
+          />
+        </div>
+        <div className="user-info">
+          <p className="user-info__item">Name#2</p>
+          <p className="user-info__item">UserName#2</p>
+          <p className="user-info__item">Email#2</p>
+        </div>
+      </li>
+    </ul>
   </div>
 );
 
