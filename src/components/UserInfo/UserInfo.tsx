b17223be9251957fbc2182c07b534bb85141ea25
PronimanPY <dprontenko@gmail.com>
2022-03-09 17:50:54 +0200

    Solution 2

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { User } from '../../types/User';
 
+// ...
+
 export const UserInfo: React.FC<User> = ({ name, email, phone }) => (
   <div className="user">
     <h3 className="user__name">{name}</h3>
