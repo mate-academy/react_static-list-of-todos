510e56e6c92ff6fc089ec4ca00f60e2dc0e34cd7
Mary <anokhinamasha27@gmail.com>
2022-03-13 21:28:11 +0200

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,11 @@ import React from 'react';
 import { TodoList } from './components/TodoList';
 
 import './App.scss';
-import { Todo } from './types/Todo';
 
 import todos from './api/todos';
 import users from './api/users';
 
-const preparedTodos: Todo[] = todos.map(todo => ({
+const preparedTodos = todos.map(todo => ({
   ...todo,
   user: users.find(user => user.id === todo.userId) || null,
 }));
