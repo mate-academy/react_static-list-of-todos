5d6e7c44d7a2948260e0ff587d9ff5ada317a4ae
Roman Kushyk <romankushyk0@gmail.com>
2022-03-28 19:13:04 +0300

    smallest fix

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -8,47 +8,45 @@ interface Props {
 }
 
 export const UserInfo: React.FC<Props> = ({ user }) => (
-  <>
-    <div className="todo-list__user-info card">
-      <h3 className="card__name">{user.name}</h3>
-      <div className="card__description description">
-        <h4 className="description__username">{`User: ${user.username}`}</h4>
+  <div className="todo-list__user-info card">
+    <h3 className="card__name">{user.name}</h3>
+    <div className="card__description description">
+      <h4 className="description__username">{`User: ${user.username}`}</h4>
 
-        <div className="description__info">
-          <div className="description__title">
-            Phone
-          </div>
-
-          <a href={`tel:${user.phone}`} className="description__link">
-            {user.phone}
-          </a>
+      <div className="description__info">
+        <div className="description__title">
+          Phone
         </div>
 
-        <div className="description__info">
-          <div className="description__title">
-            Email
-          </div>
+        <a href={`tel:${user.phone}`} className="description__link">
+          {user.phone}
+        </a>
+      </div>
 
-          <a href={`mailto:${user.email}`} className="description__link">
-            {user.email}
-          </a>
+      <div className="description__info">
+        <div className="description__title">
+          Email
         </div>
 
-        <div className="description__info">
-          <div className="description__title">
-            Website
-          </div>
+        <a href={`mailto:${user.email}`} className="description__link">
+          {user.email}
+        </a>
+      </div>
 
-          <a
-            href={`https://${user.website}`}
-            target="_blank"
-            className="description__link"
-            rel="noreferrer"
-          >
-            {user.website}
-          </a>
+      <div className="description__info">
+        <div className="description__title">
+          Website
         </div>
+
+        <a
+          href={`https://${user.website}`}
+          target="_blank"
+          className="description__link"
+          rel="noreferrer"
+        >
+          {user.website}
+        </a>
       </div>
     </div>
-  </>
+  </div>
 );
