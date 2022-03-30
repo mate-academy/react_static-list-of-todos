6957b5c4b763ddf63f0b354c2433287c265d6c29
Yuriy Shvorob <yuriyshvorob@Yuriys-MacBook-Pro.local>
2022-03-30 16:08:13 +0300

    [static list of todos] cypress tests added

--- cypress/integration/page.spec.js
+++ cypress/integration/page.spec.js
@@ -1,7 +1,40 @@
-// WRITE TESTS HERE
+/// <reference types="Cypress" />
+/// <reference types="../support" />
+
+import todos from '../../src/api/todos.ts'
+import users from '../../src/api/users.ts'
 
 describe('Page', () => {
-  it('should be visitable', () => {
+  it('visit the page', () => {
     cy.visit('/');
   });
+
+  it('have title "Static list of todos"', () => {
+    cy.getByDataCy('todo-list')
+    .should('have.text', 'Static list of todos');
+  });
+
+  it('consists of 200 posts', () => {
+    cy.getByDataCy('prepared-todos')
+    .should('have.length', todos.length)
+  });
+
+  it('have a proper title', () => {
+    cy.getByDataCy('todo')
+    .selectElement('todo-title', 0)
+    .should('have.text', todos[0].title)
+  });
+
+  it('have a proper author', () => {
+    cy.getByDataCy('todo')
+    .selectElement('user-name', 0)
+    .should('have.text', users[0].name)
+  });
+
+  it('have a proper email', () => {
+    cy.getByDataCy('todo')
+    .selectElement('user-email', 0)
+    .should('have.text', users[0].email)
+  });
+
 });
