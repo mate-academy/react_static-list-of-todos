79273f08ccbe51f74b0b16fe451db43ba043b972
Yuriy Shvorob <yuriyshvorob@Yuriys-MacBook-Pro.local>
2022-03-30 19:43:49 +0300

    [react static list of todos] suggested correction

--- cypress/integration/page.spec.js
+++ cypress/integration/page.spec.js
@@ -5,36 +5,36 @@ import todos from '../../src/api/todos.ts'
 import users from '../../src/api/users.ts'
 
 describe('Page', () => {
-  it('visit the page', () => {
+  before(() => {
     cy.visit('/');
   });
 
   it('have title "Static list of todos"', () => {
     cy.getByDataCy('todo-list')
-    .should('have.text', 'Static list of todos');
+      .should('have.text', 'Static list of todos');
   });
 
   it('consists of 200 posts', () => {
     cy.getByDataCy('prepared-todos')
-    .should('have.length', todos.length)
+      .should('have.length', todos.length);
   });
 
   it('have a proper title', () => {
     cy.getByDataCy('todo')
-    .selectElement('todo-title', 0)
-    .should('have.text', todos[0].title)
+      .selectElement('todo-title')
+      .should('have.text', todos[0].title);
   });
 
   it('have a proper author', () => {
     cy.getByDataCy('todo')
-    .selectElement('user-name', 0)
-    .should('have.text', users[0].name)
+      .selectElement('user-name', 0)
+      .should('have.text', users[0].name);
   });
 
   it('have a proper email', () => {
     cy.getByDataCy('todo')
-    .selectElement('user-email', 0)
-    .should('have.text', users[0].email)
+      .selectElement('user-email', 0)
+      .should('have.text', users[0].email);
   });
 
 });
