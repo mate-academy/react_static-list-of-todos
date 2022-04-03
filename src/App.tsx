bdbc61e9b55d32abcecb92f6c5721ea8b477426c
KristinaTar <kristina.tarashchuk@gmail.com>
2022-04-03 18:51:13 +0300

    todo list was created

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import './App.scss';
 
-// import todos from './api/todos';
-// import users from './api/users';
+import todos from './api/todos';
+import users from './api/users';
+import { Todo } from './types';
+import TodoList from './TodoList/TodoList';
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Static list of todos</h1>
-  </div>
-);
+const App: React.FC = () => {
+  const preparedTodos: Array<Todo> = todos.map(e => {
+    return {
+      ...e,
+      user: users.find(user => user.id === e.userId) || null,
+    };
+  });
+
+  return (
+    <div className="App">
+      <h1 className="title">Static list of todos</h1>
+      <TodoList preparedTodos={preparedTodos} />
+    </div>
+  );
+};
 
 export default App;
