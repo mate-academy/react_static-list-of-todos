616f98a976c0f9f0c77912b28c76f6ffac69253a
Yuriy Shvorob <yuriyshvorob@Yuriys-MacBook-Pro.local>
2022-04-27 19:21:24 +0300

    cypress ct tests added

---
+++ src/components/TodoInfo.spec.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { mount } from '@cypress/react';
+import { TodoInfo } from './TodoInfo';
+import todos from '../api/todos';
+
+describe('TodoInfo', () => {
+    it('todo should have a correct title', () => {
+        mount(<TodoInfo todo={todos[0]}/>)
+        cy.getByDataCy('title').should('have.text', todos[0].title)
+    })
+
+    it('todo should have a correct status', () => {
+        mount(<TodoInfo todo={todos[0]}/>)
+        cy.getByDataCy('status').should('have.text', "Not completed!");
+    })
+});
