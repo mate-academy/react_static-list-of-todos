daf6ce55328532a6b0a6458bdd4bf76f1f886bdb
yuriyshvorob <zyuraz1993@gmail.com>
2022-04-29 14:03:41 +0300

    files for future components added

---
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -0,0 +1,6 @@
+// Don't forget to import the React library
+
+// Create a `TodoInfo` component accepting a `todo` object and use it in the 
+// list to render `title`, `completed` status and `User`
+
+// Add a default export statement for TodoInfo component to use it in the other files
---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,6 @@
+// Don't forget to import the React library
+
+// Create a `TodoList` component accepting an array of `preparedTodos` and 
+// rendering them as a list
+
+// Add a default export statement for TodoInfo component to use it in the other files
