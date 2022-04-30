882533445d22f70772f5e87874deeadb29ddfdcc
Yegor Kochetkov <kegor.fx@gmail.com>
2022-04-30 23:43:24 +0300

    Solution

---
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { User } from '../../types/User';
+import { UserInfo } from '../UserInfo';
+import './TodoInfo.scss';
+
+type Props = {
+  title: string,
+  status: boolean,
+  user: User | null,
+};
+
+export const TodoInfo: React.FC<Props> = ({
+  title,
+  status,
+  user,
+}) => (
+  <>
+    <p className="todo-info">
+      <div className="todo-info__item">
+        <span className="todo-info__title">Task:</span>
+        {` ${title}`}
+      </div>
+      <div className="todo-info__item">
+        <span className="todo-info__title">Status:</span>
+        {` ${status ? 'Done' : 'Not ready'}`}
+      </div>
+    </p>
+    <UserInfo user={user} />
+  </>
+);
