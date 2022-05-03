204189bb43a8e744b665c1f40b9a3f1c37f0e191
Anastasiia Svintsova <svintsova.nastya@gmail.com>
2022-05-03 10:04:32 +0300

    Files were added

---
+++ src/components/UserInfo/UserInfo.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { User } from '../../types/User';
+import './UserInfo.scss';
+
+export const UserInfo: React.FC<User> = ({ name, email }) => (
+  <div className="user">
+    <div className="user__name">{name}</div>
+    <a href="mailto:{email}" className="user__email">{email}</a>
+  </div>
+);
