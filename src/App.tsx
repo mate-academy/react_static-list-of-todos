779e43a1b114a80adca27629d21c1ac4bf16e41d
Anastasiia Sharkova <sharkova.nastik@gmail.com>
2022-05-04 20:02:24 +0200

    todos PR

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import './App.scss';
+import { Users } from './types/User';
+import { Todos } from './types/Todo';
 
-// import todos from './api/todos';
-// import users from './api/users';
+import todos from './api/todos';
+import users from './api/users';
+import { TodoList } from './components/TodoList';
+
+export const PreparedTodos: Todos[] = todos.map((item) => (
+  {
+    ...item,
+    user: users.find((person: Users) => (item.userId === person.id)) || null,
+  }
+));
 
 const App: React.FC = () => (
   <div className="App">
-    <h1>Static list of todos</h1>
+    <h1 className="App__title">Static list of todos</h1>
+    <TodoList preparedTodos={PreparedTodos} />
   </div>
 );
 
