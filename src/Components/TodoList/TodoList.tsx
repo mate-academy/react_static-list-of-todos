34f6d2879fbfb8895b850668885d3fea711a877e
Olga <arnab@ukr.net>
2022-05-04 16:23:29 +0300

    Fixed naming

--- src/Components/TodoList/TodoList.tsx
+++ src/Components/TodoList/TodoList.tsx
@@ -3,12 +3,12 @@ import { TodoInfo } from '../TodoInfo/TodoInfo';
 import './TodoList.scss';
 
 type Props = {
-  preparedTodos: Todo [];
+  todos: Todo [];
 };
 
-export const TodoList: React.FC<Props> = ({ preparedTodos }) => (
+export const TodoList: React.FC<Props> = ({ todos }) => (
   <ul className="todoList">
-    {preparedTodos.map(todo => (
+    {todos.map(todo => (
       <li key={todo.id} className="todoList__item">
         <TodoInfo {...todo} />
       </li>
