6f6ea94f9f0f839b2689d97d451cbed529b99272
Ivan Boiko <ak47killall@gmail.com>
2022-05-05 18:23:22 +0200

    Update src/App.tsx
    Co-authored-by: Alisa Ivashkevych <57598825+aliceblunt8@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import users from './api/users';
 
 const preparedTodos: Todo[] = todos.map(todo => ({
   ...todo,
-  user: users.find(user => user.id === todo.userId),
+  user: users.find(user => user.id === todo.userId) || null,
 }));
 
 const App: React.FC = () => (
