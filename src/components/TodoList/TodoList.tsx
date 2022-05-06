384baef4c6061bdfb5aa73525621f4a7681b3c85
Andriy Fesych <andriyhimself@gmail.com>
2022-05-06 21:01:41 +0300

    Solution_0.1

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import './TodoList.scss';
-import classNames from 'classnames';
 
 import { ToDo } from '../../interfaces/ToDo';
-import { UserInfo } from '../UserInfo';
+import { TodoInfo } from '../TodoInfo';
 
 interface Props {
   preparedTodos: ToDo[],
@@ -11,43 +10,8 @@ interface Props {
 
 export const TodoList: React.FC<Props> = ({ preparedTodos }) => (
   <div className="todo-list">
-    {preparedTodos.map(({
-      id,
-      title,
-      completed,
-      User,
-    }) => (
-      <>
-        <div className={classNames('todo-card',
-          { 'todo-card__done': completed === true })}
-        >
-          <div className="todo-title">
-            <p>
-              Title:&#160;&#160;
-              {title}
-            </p>
-          </div>
-          <div>
-            Responsible person:&#160;
-            <br />
-          </div>
-          <div className="todo-person">
-            <p>
-              {User ? (
-                <UserInfo user={User} />
-              ) : 'user not assigned'}
-            </p>
-          </div>
-          <div>
-            {'Task ID: '}
-            {id}
-          </div>
-          <p>
-            status:&#160;
-            {completed ? 'Done' : 'Undone'}
-          </p>
-        </div>
-      </>
+    {preparedTodos.map((todo) => (
+      <TodoInfo card={todo} />
     ))}
   </div>
 );
