bc15762874b355633667561091e9a35821957d6a
Maryna <savna023@gmail.com>
2022-05-28 17:45:58 +0300

    add some styles

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { User } from '../../react-app-env';
+import './UserInfo.scss';
 
 type Props = {
   user: User;
 };
 
 const UserInfo: React.FC<Props> = ({ user }) => (
-  <>
-    <strong>{user.name}</strong>
+  <div className="user">
+    <strong data-cy="username">{user.name}</strong>
     <p data-cy="email">{user.email}</p>
-  </>
+  </div>
 );
 
 export default UserInfo;
