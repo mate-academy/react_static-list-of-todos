e8579fbb3cac97c0e33c44116b51de06c12713be
Anastasiia Nikita <anastasiya.folk@gmail.com>
2022-05-30 15:12:38 +0300

    Solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,24 @@
-// Don't forget to import the React library
+import React from 'react';
+import { PrepareTodos } from '../../react-app-env';
+import { UserInfo } from '../UserInfo';
+import './TodoInfo.scss';
+
+interface Props {
+  todo: PrepareTodos,
+}
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => (
+  <div className="todos">
+    <ul>
+      <strong>{`Todo ${todo.id}`}</strong>
+      <li data-cy="title">{todo.title}</li>
+      <li data-cy="status">{`${todo.completed}`}</li>
+      {todo.user && (
+        <UserInfo user={todo.user} />
+      )}
+    </ul>
+  </div>
+);
 
 // Create a `TodoInfo` component accepting a `todo` object and use it in the
 // list to render `title`, `completed` status and `User`
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,20 @@
-// Don't forget to import the React library
+import React from 'react';
+import { PrepareTodos } from '../../react-app-env';
+import { TodoInfo } from '../TodoInfo';
+
+interface Props {
+  todos: PrepareTodos[];
+}
+
+export const TodoList: React.FC<Props> = ({ todos }) => (
+  <ul>
+    {todos.map(todo => (
+      <React.Fragment key={todo.id}>
+        <TodoInfo todo={todo} />
+      </React.Fragment>
+    ))}
+  </ul>
+);
 
 // Create a `TodoList` component accepting an array of `preparedTodos` and
 // rendering them as a list
