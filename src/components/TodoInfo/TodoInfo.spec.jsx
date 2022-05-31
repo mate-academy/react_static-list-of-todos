34b679e4865d7548f875752659773c08f5c42029
Yaroslav Kvasnytsia <qwerty300067@gmail.com>
2022-05-31 15:54:43 +0300

    todos1.0

--- src/components/TodoInfo/TodoInfo.spec.jsx
+++ src/components/TodoInfo/TodoInfo.spec.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { mount } from '@cypress/react';
-import { TodoInfo } from './TodoInfo';
+import TodoInfo from './TodoInfo';
 import todos from '../../api/todos';
 
 describe('TodoInfo', () => {
   it('should have a correct title', () => {
+    // eslint-disable-next-line react/jsx-filename-extension
     mount(<TodoInfo todo={todos[0]} />);
     cy.getByDataCy('title')
       .should('have.text', todos[0].title);
--- src/components/TodoList/TodoList.spec.jsx
+++ src/components/TodoList/TodoList.spec.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { mount } from '@cypress/react';
-import { TodoList } from './TodoList';
+import TodoList from './TodoList';
 import todos from '../../api/todos';
 
 describe('TodoList', () => {
   it('should have a correct length', () => {
+    // eslint-disable-next-line react/jsx-filename-extension
     mount(<TodoList todos={todos} />);
     cy.get('li')
       .should('have.length', todos.length);
