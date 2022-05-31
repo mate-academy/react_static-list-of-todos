833451dc19f3d467e1d4355beefe1959ab24a9a1
Andrii Rutkovskii <greateman94@gmail.com>
2022-05-31 15:14:16 +0300

    some solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.scss';
-
-// import todos from './api/todos';
-// import users from './api/users';
+import { preparedTodos } from './components/PreparedTodos/index';
+import { TodoList } from './components/TodoList/TodoList';
 
 const App: React.FC = () => (
-  <div className="App">
-    <h1>Static list of todos</h1>
+  <div className="App container">
+    <h1 className="subtitle is-1">Static list of todos</h1>
+    <TodoList todosList={preparedTodos} />
   </div>
 );
 
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,6 +1,22 @@
 // Don't forget to import the React library
+import React from 'react';
+import { PreparedTodos } from '../../react-app-env';
+import { TodoInfo } from '../TodoInfo/TodoInfo';
 
 // Create a `TodoList` component accepting an array of `preparedTodos` and
 // rendering them as a list
+interface Props {
+  todosList: PreparedTodos[],
+}
+
+export const TodoList: React.FC<Props> = ({ todosList }) => {
+  return (
+    <ul className="block">
+      {todosList.map(el => (
+        <TodoInfo todoInfo={el} />
+      ))}
+    </ul>
+  );
+};
 
 // Add a default export statement for TodoInfo component to use it in the other files
