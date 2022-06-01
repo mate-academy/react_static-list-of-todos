6065db3e5371aea23f887505e3e716660e64e8c7
LukaszGryziewicz <lukaszgryziewicz@gmail.com>
2022-06-01 14:43:04 +0200

    now rendering todoList

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import './App.scss';
+import todos from './api/todos';
+import users from './api/users';
+import { PreparedTodo } from './types';
+import { TodoList } from './components/TodoList';
 
-// import todos from './api/todos';
-// import users from './api/users';
+const preparedTodos: PreparedTodo[] = todos.map((todo) => {
+  const user = users.find(person => person.id === todo.userId) || null;
+
+  return (
+    {
+      ...todo,
+      user,
+    }
+  );
+});
 
 const App: React.FC = () => (
   <div className="App">
-    <h1>Static list of todos</h1>
+    <TodoList preparedTodos={preparedTodos} />
   </div>
 );
 
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,5 +1,30 @@
-// Don't forget to import the React library
+import React from 'react';
+import { PreparedTodo } from '../../types';
 
+type Props = {
+  preparedTodos: PreparedTodo[];
+};
+
+export const TodoList: React.FC<Props> = ({ preparedTodos }) => (
+  <ul>
+    {preparedTodos.map(todo => (
+      <li key={todo.id}>
+        <h2>{`Task: ${todo.title}`}</h2>
+        <p>
+          {todo.completed
+            ? ('Completed')
+            : ('Not Completed')}
+        </p>
+        {todo.user && (
+          <>
+            <p>{todo.user.name}</p>
+            <p>{todo.user.email}</p>
+          </>
+        )}
+      </li>
+    ))}
+  </ul>
+);
 // Create a `TodoList` component accepting an array of `preparedTodos` and
 // rendering them as a list
 
