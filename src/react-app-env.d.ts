84240ff66879f38fecd0d8f85a025ae424aa4dd0
Lebogang Mogane <mogane420@gmail.com>
2022-06-05 17:27:00 +0200

    Solution

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="react-scripts" />
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+export interface ToDos {
+  title: string;
+  completed: boolean;
+  user: User | null;
+  id: number;
+  userId: number;
+}
