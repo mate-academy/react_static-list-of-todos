e0b3be1c8e4ef44a95402c9bc43bfa1901e0b3b9
Danylo-Dziuban <dz.danylo@gmail.com>
2022-06-06 00:30:31 +0300

    Solution

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,19 @@
-// Don't forget to import the React library
+import React from 'react';
+
+import { PreparedTodos } from '../../types';
+import { TodoInfo } from '../TodoInfo';
+
+type Props = {
+  prepared: PreparedTodos[],
+};
+
+export const TodoList: React.FC<Props> = ({ prepared }) => {
+  return (
+    <>
+      {prepared.map((todo: PreparedTodos) => <TodoInfo prepared={todo} />)}
+    </>
+  );
+};
 
 // Create a `TodoList` component accepting an array of `preparedTodos` and
 // rendering them as a list
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,4 +1,32 @@
-// Don't forget to import the React library
+import React from 'react';
+
+import { User } from '../../types';
+
+type Props = {
+  user: User | null,
+};
+
+export const UserInfo: React.FC<Props> = ({ user }) => {
+  return (user && (
+    <div>
+      <span data-cy="username" className="fs-4">
+        {`Assigned by: ${user.username}`}
+      </span>
+      <br />
+      <span data-cy="email" className="fs-6">
+        {`Email: ${user.email}`}
+      </span>
+      <br />
+      <span className="fs-6">
+        {`Adress: ${user.address.suite}, ${user.address.street}, ${user.address.city}`}
+      </span>
+      <br />
+      <span className="fs-6">
+        {`Phone: ${user.phone}`}
+      </span>
+    </div>
+  ));
+};
 
 // Create a `UserInfo` component accepting a `user` object and use it to render
 // a `todo.user` in the list with some styling. (Show at least a `name` and an
