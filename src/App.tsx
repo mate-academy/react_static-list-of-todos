d5f6fa697964df90bef2c1c15c2c46e8c1de3a2a
Karina Prozner <prozner.karina@gmail.com>
2022-06-07 22:08:11 +0200

    some fixes

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,11 @@ import './App.scss';
 
 import todos from './api/todos';
 import users from './api/users';
-import { User } from './components/UserInfo';
 import { TodoList } from './components/TodoList';
 import { Todo } from './components/TodoInfo';
 
-const currentUsers: User[] = users.map(user => user);
 const preparedTodos: Todo[] = todos.map(obj => {
-  const foundUser = currentUsers.find(user => user.id === obj.userId);
+  const foundUser = users.find(user => user.id === obj.userId);
   const temp:Todo = {
     ...obj,
     user: foundUser === undefined ? null : foundUser,
--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -12,20 +12,13 @@ export interface Todo {
 }
 
 export const TodoInfo: React.FC<Todo> = (todo: Todo) => {
-  if (todo.user === null) {
-    return (
-      <div className="task">
-        <p data-cy="title">{`Title: ${todo.title}`}</p>
-        <p data-cy="status">{`Completed: ${todo.completed.toString()}`}</p>
-      </div>
-    );
-  }
-
   return (
     <div>
       <h3 data-cy="title">{`Title: ${todo.title}`}</h3>
       <p data-cy="status">{`Completed: ${todo.completed.toString()}`}</p>
-      <p><UserInfo {...todo.user} /></p>
+      {todo.user && (
+        <p><UserInfo {...todo.user} /></p>
+      )}
     </div>
   );
 };
