181849a39a8eac69bdd8ff9870f867ab377d48a2
efdalyalcin <efdalyalcin@hotmail.com>
2022-06-07 13:13:54 +0300

    interfaces splitted, implicit return is implemented

--- src/app.typedefs.ts
+++ src/app.typedefs.ts
@@ -1,25 +1,31 @@
+interface Geo {
+  lat: string;
+  lng: string;
+}
+
+interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+}
+
+interface Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
+
 export interface User {
   id: number;
   name: string;
   username: string;
   email: string;
-  address: {
-    street: string;
-    suite: string;
-    city: string;
-    zipcode: string;
-    geo: {
-      lat: string;
-      lng: string;
-    };
-  };
+  address: Address;
   phone: string;
   website: string;
-  company: {
-    name: string;
-    catchPhrase: string;
-    bs: string;
-  };
+  company: Company;
 }
 
 export interface Todo{
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -7,16 +7,14 @@ interface Props {
   preparedTodos: PreparedTodos[];
 }
 
-export const TodoList: FC<Props> = ({ preparedTodos }) => {
-  return (
-    <ul className="todo-list">
-      {preparedTodos.map(todo => {
-        return (
-          <li key={todo.id}>
-            <TodoInfo todo={todo} />
-          </li>
-        );
-      })}
-    </ul>
-  );
-};
+export const TodoList: FC<Props> = ({ preparedTodos }) => (
+  <ul className="todo-list">
+    {preparedTodos.map(todo => {
+      return (
+        <li key={todo.id}>
+          <TodoInfo todo={todo} />
+        </li>
+      );
+    })}
+  </ul>
+);
