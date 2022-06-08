036006f2892c53dfd4df9512fd71e8ede5185323
Oleksandr Ivanchenko <aleksandroc92@gmail.com>
2022-06-08 17:48:58 +0300

    add export default TodoInfo

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,9 +1,3 @@
-// Don't forget to import the React library
-
-// Create a `TodoInfo` component accepting a `todo` object and use it in the
-// list to render `title`, `completed` status and `User`
-
-// Add a default export statement for TodoInfo component to use it in the other files
 import React from 'react';
 import { PreparedTodo } from '../../react-app-env';
 import { UserInfo } from '../UserInfo/UserInfo';
@@ -29,3 +23,5 @@ export const TodoInfo : React.FC<Props> = ({ todo }) => (
     </ul>
   </li>
 );
+
+export default TodoInfo;
