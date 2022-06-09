93f6b389bce9dcf2bac4bf6ea4092bb9e4272bfb
Oleksandr Ivanchenko <aleksandroc92@gmail.com>
2022-06-09 14:53:55 +0300

    add key in ul

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -3,21 +3,17 @@ import { PreparedTodo } from '../../react-app-env';
 import { TodoInfo } from '../TodoInfo/TodoInfo';
 import './TodoList.scss';
 
-// Create a `TodoList` component accepting an array of `preparedTodos` and
-// rendering them as a list
-
-// Add a default export statement for TodoInfo component to use it in the other files
 interface Props {
   preparedTodos: PreparedTodo[]
 }
 
-export const TodoList : React.FC<Props> = ({ preparedTodos }: {
-  preparedTodos: PreparedTodo[]
-}) => (
+export const TodoList : React.FC<Props> = ({ preparedTodos }) => (
   <ul className="list">
     {
       preparedTodos.map((todo: PreparedTodo) => (
-        <TodoInfo todo={todo} />
+        <React.Fragment key={todo.id}>
+          <TodoInfo todo={todo} />
+        </React.Fragment>
       ))
     }
   </ul>
