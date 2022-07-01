2ac8239e5c89d472bd95cd096dc568f381961bdf
mira1zu <mira1zu733@gmail.com>
2022-07-01 21:49:06 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
+
 import './App.scss';
 
-// import todos from './api/todos';
-// import users from './api/users';
+import todos from './api/todos';
+import users from './api/users';
+
+import { TodoList } from './components/TodoList/TodoList';
+
+let currentUserIndex = 0;
+
+export const preparedTodos = todos.map((todo) => {
+  currentUserIndex = users[currentUserIndex].id === todo.userId
+    ? currentUserIndex
+    : users.findIndex((user) => user.id === todo.userId);
+
+  return {
+    ...todo,
+    user: users[currentUserIndex] || null,
+  };
+});
 
 const App: React.FC = () => (
   <div className="App">
     <h1>Static list of todos</h1>
+    <TodoList preparedTodos={preparedTodos} />
   </div>
 );
 
--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,6 +1,47 @@
-// Don't forget to import the React library
+import React from 'react';
 
-// Create a `TodoInfo` component accepting a `todo` object and use it in the
-// list to render `title`, `completed` status and `User`
+import './TodoInfo.scss';
 
-// Add a default export statement for TodoInfo component to use it in the other files
+import { UserInfo } from '../UserInfo';
+
+import User from '../../types/User';
+
+type Props = {
+  title: string;
+  completed: boolean;
+  user: User | null;
+};
+
+export const TodoInfo: React.FC<Props> = ({ title, completed, user }) => (
+  <div className={completed
+    ? 'TodoInfo TodoInfo--done'
+    : 'TodoInfo'}
+  >
+    <h2 className="TodoInfo__title" data-cy="title">{`${title}`}</h2>
+    <span>
+      {'Status: '}
+      <span
+        className={completed
+          ? 'TodoInfo__status TodoInfo__status--done'
+          : 'TodoInfo__status'}
+        data-cy="status"
+      >
+        {`${completed ? '' : 'Not'} completed!`}
+      </span>
+    </span>
+
+    <hr className="TodoInfo__line" />
+
+    {user !== null
+      ? (
+        <UserInfo
+          name={user?.name}
+          username={user?.username}
+          email={user?.email}
+        />
+      )
+      : (
+        <div>User does not exist</div>
+      )}
+  </div>
+);
