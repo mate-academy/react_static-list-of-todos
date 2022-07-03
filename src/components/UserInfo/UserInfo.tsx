38093400c5d450239494f681697cf8d0380905b0
Roman Marunkevich <vinnibea@gmail.com>
2022-07-03 23:34:31 +0300

    solution

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -9,21 +9,21 @@ interface User {
 export const UserInfo: React.FC<User> = ({ name, email, username }) => (
   <div className="todo-list__user">
     <p className="todo-list__main">
-      <span className="todo-list__email">
+      <span className="todo-list__field">
         Username:
       </span>
       {username}
     </p>
 
     <p className="todo-list__main" data-cy="username">
-      <span className="todo-list__email">
+      <span className="todo-list__field">
         Name:
       </span>
       {name}
     </p>
 
     <p className="todo-list__main" data-cy="email">
-      <span className="todo-list__email">
+      <span className="todo-list__field">
         Email:
       </span>
       {email}
