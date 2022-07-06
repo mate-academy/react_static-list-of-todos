dea8e840445465c5bc9e8712ec3ddb13fd0d3dab
Sasha Havryliuk <princesasha9178@icloud.com>
2022-07-06 20:55:27 +0300

    Solution1

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.scss';
 
 import todos from './api/todos';
 import users from './api/users';
+
 import TodoList from './components/TodoList/TodoList';
 
 const preparedTodos = todos.map(todo => ({
