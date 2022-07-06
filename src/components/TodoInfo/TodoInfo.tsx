d12fb78c3ba9cba749c7e1000d64f08c3d60878a
Vladyslav Supruniuk <vladsupruniuk@gmail.com>
2022-07-06 16:17:33 +0300

    Some improvements

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -7,9 +7,17 @@ type Props = Todo;
 
 const TodoInfo: React.FC<Props> = ({ title, completed, userId }) => (
   <>
-    <div className="todo-info">{`Task: ${title}`}</div>
-    <div className="todo-info" data-cy="status">{`Status: ${completed ? 'Done' : 'In progress'}`}</div>
-    <div className="todo-info">{`User: ${userId}`}</div>
+    <div className="todo-info">
+      {`Task: ${title}`}
+    </div>
+
+    <div className="todo-info" data-cy="status">
+      {`Status: ${completed ? 'Done' : 'In progress'}`}
+    </div>
+
+    <div className="todo-info">
+      {`User: ${userId}`}
+    </div>
   </>
 );
 
