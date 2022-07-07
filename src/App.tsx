6ec1a74209ffc3412b9730545aca7a4368f9099a
Bogdan Pyshchyk <bohdan.pyshchyk@gmail.com>
2022-07-07 16:01:49 +0300

    Todos 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.scss';
 
-// import todos from './api/todos';
-// import users from './api/users';
+import todos from './api/todos';
+import users from './api/users';
+import { TodoList } from './components/TodoList/TodoList';
+
+const newTodos = todos.map(todo => {
+  return {
+    ...todo,
+    user: users.find(user => user.id === todo.userId)
+    || null,
+  };
+});
 
 const App: React.FC = () => (
-  <div className="App">
-    <h1>Static list of todos</h1>
+  <div className="container">
+    <h1 className="todo">Static list of todos</h1>
+    <TodoList newTodo={newTodos} />
   </div>
 );
 
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,7 +1,21 @@
-// Don't forget to import the React library
+import React from 'react';
 
-// Create a `UserInfo` component accepting a `user` object and use it to render
-// a `todo.user` in the list with some styling. (Show at least a `name` and an
-// `email` of the `user`)
+interface User {
+  name: string;
+  email: string;
+}
 
-// Add a default export statement for UserInfo component to use it in the other files
+type Props = {
+  user: User | null;
+};
+
+export const UserInfo: React.FC<Props | null> = ({ user }) => (
+  <>
+    {user && (
+      <>
+        <h2 data-cy="username">{user.name}</h2>
+        <h3 data-cy="email">{user.email}</h3>
+      </>
+    )}
+  </>
+);
