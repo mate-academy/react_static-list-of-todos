a09160a49a89c0685d0e05f266e197b3a8b3d8ea
Dmytro Mostovyi <7kozita@gmail.com>
2022-07-07 20:45:12 +0300

    Solution

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,6 +1,36 @@
-// Don't forget to import the React library
+import React from 'react';
+import TodoInfo from '../TodoInfo/TodoInfo';
+import UserInfo from '../UserInfo/UserInfo';
 
-// Create a `TodoList` component accepting an array of `preparedTodos` and
-// rendering them as a list
+interface Todo {
+  userId: number,
+  id: number,
+  title: string,
+  completed: boolean,
+  user: User | null,
+}
 
-// Add a default export statement for TodoInfo component to use it in the other files
+interface User {
+  name: string,
+  email: string,
+}
+
+type Props = {
+  todoList: Todo[]
+};
+
+const TodoList: React.FC<Props> = ({ todoList }) => (
+  <ul className="list">
+    {todoList.map(todo => (
+      <li className="list__item">
+        <UserInfo user={todo.user} />
+        <TodoInfo
+          title={todo.title}
+          completed={todo.completed}
+        />
+      </li>
+    ))}
+  </ul>
+);
+
+export default TodoList;
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,7 +1,28 @@
-// Don't forget to import the React library
+import React from 'react';
 
-// Create a `UserInfo` component accepting a `user` object and use it to render
-// a `todo.user` in the list with some styling. (Show at least a `name` and an
-// `email` of the `user`)
+interface User {
+  name: string,
+  email: string,
+}
 
-// Add a default export statement for UserInfo component to use it in the other files
+type Props = {
+  user: User | null
+};
+
+const UserInfo: React.FC<Props> = ({ user }) => (
+  <>
+    {user && (
+      <>
+        <h2 data-cy="username">{user.name}</h2>
+        <h3
+          className="list__mail"
+          data-cy="email"
+        >
+          {user.email}
+        </h3>
+      </>
+    )}
+  </>
+);
+
+export default UserInfo;
