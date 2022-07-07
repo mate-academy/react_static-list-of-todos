b9f4ee729b1653a520cb4a9e2f0a0f22ee11cf34
Mariia Nedilko <mary.nedilko1@gmail.com>
2022-07-07 15:05:21 +0200

    Solution created

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,6 +1,21 @@
-// Don't forget to import the React library
+import React from 'react';
 
 // Create a `TodoInfo` component accepting a `todo` object and use it in the
 // list to render `title`, `completed` status and `User`
 
 // Add a default export statement for TodoInfo component to use it in the other files
+type Prop = {
+  title: string
+  completed: boolean
+};
+
+export const TodoInfo: React.FC<Prop> = ({ title, completed }) => (
+  <>
+    <p className="todos__title" data-cy="title">{title}</p>
+    <p
+      data-cy="status"
+    >
+      {completed ? 'Done' : 'Not completed!'}
+    </p>
+  </>
+);
