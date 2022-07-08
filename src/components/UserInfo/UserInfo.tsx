afb1e1ff68b10649acc5a54c06fca645dedaac43
Alina Melentieva <melenteva2906@gmail.com>
2022-07-08 13:56:12 +0300

    SOlution

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,7 +1,19 @@
-// Don't forget to import the React library
+import { FC } from 'react';
+import { User } from '../types';
 
-// Create a `UserInfo` component accepting a `user` object and use it to render
-// a `todo.user` in the list with some styling. (Show at least a `name` and an
-// `email` of the `user`)
+interface UserInfoProps {
+  user: User | null;
+}
 
-// Add a default export statement for UserInfo component to use it in the other files
+const UserInfo: FC<UserInfoProps> = ({ user }) => (
+  <>
+    {user && (
+      <>
+        <h4 data-cy="username">{user.name}</h4>
+        <p data-cy="email">{user.email}</p>
+      </>
+    )}
+  </>
+);
+
+export default UserInfo;
