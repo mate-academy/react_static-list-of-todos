e722beb6d0901a910e9c73c7d447f1bb1d8c97f3
Illia Bielichenko <ibelich12@gmail.com>
2022-07-10 17:35:18 +0300

    add some small fixes

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -5,26 +5,33 @@ type Props = {
   todo: Todo;
 };
 
-export const TodoInfo: React.FC<Props> = ({ todo }) => (
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const { title, completed, user } = todo;
 
-  <li className="todos-list__todoInfo todoInfo">
-    <div className="todoInfo__title">
-      Task name:
-      {todo.title}
-    </div>
-    <div className={todo.completed ? 'todoInfo__completed'
-      : 'todoInfo__not-completed'}
-    >
-      Task status:
-      {todo.completed ? 'completed' : 'not completed'}
-    </div>
-    <div>
-      Manager name:
-      {todo.user ? todo.user.name : 'no information'}
-    </div>
-    <div>
-      Manager email:
-      {todo.user ? todo.user.email : 'no information'}
-    </div>
-  </li>
-);
+  return (
+    <li className="todos-list__todoInfo todoInfo">
+      <div className="todoInfo__title">
+        Task name:
+        {' '}
+        {title}
+      </div>
+      <div className={completed ? 'todoInfo__completed'
+        : 'todoInfo__not-completed'}
+      >
+        Task status:
+        {' '}
+        {completed ? 'completed' : 'not completed'}
+      </div>
+      <div>
+        User name:
+        {' '}
+        {user ? user.name : 'no information'}
+      </div>
+      <div>
+        User email:
+        {' '}
+        {user ? user.email : 'no information'}
+      </div>
+    </li>
+  );
+};
