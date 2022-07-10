2f8809834ff6555d0119e65fe523d8db525c73eb
Denys Politov <dpolitov2001@gmail.com>
2022-07-10 16:04:05 +0300

    First commit

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,6 +1,33 @@
-// Don't forget to import the React library
+import React from 'react';
+import './TodoInfo.scss';
 
-// Create a `TodoInfo` component accepting a `todo` object and use it in the
-// list to render `title`, `completed` status and `User`
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo/UserInfo';
 
-// Add a default export statement for TodoInfo component to use it in the other files
+type Props = {
+  todo: Todo;
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const { title, completed, user } = todo;
+
+  return (
+    <div className="todo-card" key={todo.id}>
+      <p data-cy="title">
+        <b>Title: </b>
+        {title}
+      </p>
+
+      <UserInfo user={user} />
+
+      <p data-cy="status">
+        <b>Status: </b>
+        {
+          completed
+            ? <span className="todo-card__status--done">Complete</span>
+            : <span className="todo-card__status--todo">Not completed!</span>
+        }
+      </p>
+    </div>
+  );
+};
