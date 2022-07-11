357fbd6c9b82af39eb2c3a6c93fb05dc03a95728
Luidmyla Omelchenko <lui.omelika@gmail.com>
2022-07-11 08:58:10 +0300

    finished TodoList

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,4 +1,18 @@
-// Add the required types and props
-export const UserInfo = () => (
-  <>UserInfo markup</>
+import { User } from '../../types/User';
+
+type Props = {
+  user: User | null;
+};
+
+export const UserInfo: React.FC<Props> = ({ user }) => (
+  <>
+    {user && (
+      <>
+        <p>{user.name}</p>
+        <a className="UserInfo" href={user.email}>
+          {user.email}
+        </a>
+      </>
+    )}
+  </>
 );
