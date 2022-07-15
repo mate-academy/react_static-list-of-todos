eb6ee4851610542429812866820dfe0fbdcf251a
Vlad Moysieienko <Moysieienko03@gmail.com>
2022-07-15 14:21:27 +0300

    Sol

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.scss';
 
-// import todos from './api/todos';
-// import users from './api/users';
+import { TodoList } from './components/TodoList/TodoList';
+
+import todos from './api/todos';
+import users from './api/users';
+import { prepareTodos } from './components/helper';
+
+const preparedTodos = prepareTodos(todos, users);
 
 const App: React.FC = () => (
   <div className="App">
-    <h1>Static list of todos</h1>
+    <TodoList todos={preparedTodos} />
   </div>
 );
 
