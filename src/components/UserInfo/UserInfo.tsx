2b2af735e624d32f731f98ed6dd2a65ef7481451
IvanG6 <64322485+IvanG6@users.noreply.github.com>
2022-07-20 18:38:38 +0300

    static-list-of-todos

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,4 +1,14 @@
+import { User } from '../../types/User';
+
 // Add the required types and props
-export const UserInfo = () => (
-  <>UserInfo markup</>
+type Props = {
+  user: User | null;
+};
+
+export const UserInfo: React.FC<Props> = ({ user }) => (
+  user && (
+    <a className="UserInfo" href={`mailto:${user.email}`}>
+      {user.name}
+    </a>
+  )
 );
