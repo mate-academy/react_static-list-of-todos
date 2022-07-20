bcc71213eceec43b2d660daab49c86ca73cfc35f
Mikhail Danylenko <migellyaka@gmail.com>
2022-07-20 15:04:42 +0300

    Solution fixed, user, if it exists, is render

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -13,7 +13,7 @@ export const TodoInfo: React.FC<Props> = ({ todo }) => (
     }
     >
       <h2 className="TodoInfo__title">{todo.title}</h2>
-      <UserInfo user={todo.user} />
+      {todo.user && <UserInfo user={todo.user} />}
     </article>
   </>
 );
