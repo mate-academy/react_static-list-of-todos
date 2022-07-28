9f6f7d16928675b706896f4d8d27272e81df119d
IvanG6 <64322485+IvanG6@users.noreply.github.com>
2022-07-28 09:56:00 +0300

    fixed

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -13,7 +13,9 @@ export const TodoInfo: React.FC<Props> = ({ todo }) => (
       className={cn({ TodoInfo: true, 'TodoInfo--completed': todo.completed })}
     >
       <h2 className="TodoInfo__title">{todo.title}</h2>
-      <UserInfo user={todo.user} />
+      { todo.user
+        && <UserInfo user={todo.user} /> }
+
     </article>
   </>
 );
