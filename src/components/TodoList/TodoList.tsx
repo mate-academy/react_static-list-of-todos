a3f2e6c5fb646f34b92368c3b6bfd2f2fd7a448b
Leoned Pustovit <lrerednawl@gmail.com>
2022-08-02 16:36:00 +0300

    Solution

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,16 @@
-// Add the required types and props
-export const TodoList = () => (
-  <>TodoList markup</>
+import { Todo } from '../../types/Todo';
+import { TodoInfo } from '../TodoInfo';
+
+type Todos = {
+  todos: Todo[];
+};
+
+export const TodoList: React.FC<Todos> = ({ todos }) => (
+  <section className="TodoList">
+
+    {todos.map(todo => (
+      <TodoInfo todo={todo} />
+
+    ))}
+  </section>
 );
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,4 +1,11 @@
-// Add the required types and props
-export const UserInfo = () => (
-  <>UserInfo markup</>
+import { User } from '../../types/User';
+
+type ReceivedUser = {
+  user: User | null;
+};
+
+export const UserInfo: React.FC<ReceivedUser> = ({ user }) => (
+  <a className="UserInfo" href={`mailto:${user ? user.email : null}`}>
+    {user ? user.name : null}
+  </a>
 );
