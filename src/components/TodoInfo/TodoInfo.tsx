d84d0cb18c1d3b1f55c69afdcdec6472361c1b7b
Oleksìi <bardyshev@gmail.com>
2022-08-03 14:25:25 +0300

    The ternary operator has been replaced with &&

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -8,9 +8,7 @@ type Props = {
 };
 
 export const TodoInfo: React.FC<Props> = ({ todo }) => (
-  <article className={`TodoInfo ${todo.completed ? (
-    'TodoInfo--completed') : ('')}`}
-  >
+  <article className={`TodoInfo ${todo.completed && 'TodoInfo--completed'}`}>
     <h2 className="TodoInfo__title">{todo.title}</h2>
 
     {todo.user && <UserInfo user={todo.user} />}
