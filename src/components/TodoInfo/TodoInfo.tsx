476650cbb1a0bf0fea43aa79e91b409e90dd9a1a
PavloMarkov <r9markovpn@gmail.com>
2022-08-03 15:03:42 +0300

    Solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,30 @@
-// Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+import React from 'react';
+import { Todo as TodoType } from '../../types/Todo';
+import { UserInfo } from '../UserInfo/UserInfo';
+
+interface Props {
+  todo: TodoType;
+}
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  return (
+    <article className={
+      todo.completed === true
+        ? 'TodoInfo TodoInfo--completed'
+        : 'TodoInfo'
+    }
+    >
+      <h2 className="TodoInfo__title">
+        {todo.title}
+      </h2>
+      { todo.user
+        ? (
+          <UserInfo
+            user={todo.user}
+            key={todo.user?.id}
+          />
+        )
+        : (<p>No user for this task</p>)}
+    </article>
+  );
+};
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,4 +1,19 @@
-// Add the required types and props
-export const UserInfo = () => (
-  <>UserInfo markup</>
-);
+import React from 'react';
+import { User } from '../../types/User';
+
+interface Props {
+  user: User;
+}
+
+export const UserInfo: React.FC<Props> = ({ user }) => {
+  const mail = `mailto:${user.email}`;
+
+  return (
+    <a
+      className="UserInfo"
+      href={mail}
+    >
+      {user.name}
+    </a>
+  );
+};
