0324e9ac27088cdd09b4544531ba22d804b3a256
Sergii Maniurov <maniurov_sv@ukr.net>
2022-08-03 14:12:05 +0300

    Solution

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -2,9 +2,7 @@ import { User } from '../../types/User';
 import './UserInfo.scss';
 
 export const UserInfo: React.FC<User> = ({ name, email }) => (
-  <>
-    <a className="UserInfo" href={`mailto:${email}`}>
-      {name}
-    </a>
-  </>
+  <a className="UserInfo" href={`mailto:${email}`}>
+    {name}
+  </a>
 );
