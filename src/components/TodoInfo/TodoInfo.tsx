db1cce1084b9cdbdeb42815153e6a86ca44df4aa
Sergii Maniurov <maniurov_sv@ukr.net>
2022-08-03 17:20:36 +0300

    Solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -11,16 +11,13 @@ export const TodoInfo: React.FC<Props> = ({ todo }) => {
   const { title, completed, user } = todo;
 
   return (
-    <>
-      <article
-        className={classNames('TodoInfo', {
-          'TodoInfo--completed': completed,
-        })}
-      >
-        <h2 className="TodoInfo__title">{title}</h2>
-
-        {user ? <UserInfo {...user} /> : null}
-      </article>
-    </>
+    <article
+      className={classNames('TodoInfo', {
+        'TodoInfo--completed': completed,
+      })}
+    >
+      <h2 className="TodoInfo__title">{title}</h2>
+      {user ? <UserInfo {...user} /> : null}
+    </article>
   );
 };
