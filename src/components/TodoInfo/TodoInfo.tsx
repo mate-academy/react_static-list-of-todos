bffdda62f805d3b389f73c32547a06c5c304c011
Oleksìi <bardyshev@gmail.com>
2022-08-03 15:00:41 +0300

    ClassNames library was used

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 
 import { UserInfo } from '../UserInfo';
 import { Todo } from '../../types/Todo';
@@ -8,7 +9,10 @@ type Props = {
 };
 
 export const TodoInfo: React.FC<Props> = ({ todo }) => (
-  <article className={`TodoInfo ${todo.completed && 'TodoInfo--completed'}`}>
+  <article className={classNames(
+    'TodoInfo', { 'TodoInfo--completed': todo.completed },
+  )}
+  >
     <h2 className="TodoInfo__title">{todo.title}</h2>
 
     {todo.user && <UserInfo user={todo.user} />}
