25490766393b65f06209fff48bfe91f97e18df0d
Volodymyr Dolishnii <leedeelee3@gmail.com>
2022-08-03 18:30:00 +0300

    fixed

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -9,9 +9,9 @@ type Props = {
 export const TodoList: React.FC<Props> = ({ todos }) => (
   <section className="TodoList">
     {
-      todos.map(todo => {
-        return <TodoInfo key={todo.id} todo={todo} />;
-      })
+      todos.map(todo => (
+        <TodoInfo key={todo.id} todo={todo} />
+      ))
     }
   </section>
 );
