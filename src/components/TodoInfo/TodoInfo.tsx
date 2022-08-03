cd4bf98ec372731c152662d782645af510077070
Volodymyr Dolishnii <leedeelee3@gmail.com>
2022-08-03 15:49:00 +0300

    fixed issues

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -15,6 +15,6 @@ export const TodoInfo: React.FC<Props> = ({ todo }) => (
   >
     <h2 className="TodoInfo__title">{todo.title}</h2>
 
-    <UserInfo user={todo.user} />
+    {todo.user && (<UserInfo user={todo.user} />)}
   </article>
 );
