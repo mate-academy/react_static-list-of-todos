fc1e71737b9de8acf0a603093c85e1fe13b961e7
Yevhen Nikolaienko <illnino380@gmail.com>
2022-08-03 20:02:16 +0300

    Implemented static todo app with react components

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,30 @@
-// Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+import React from 'react';
+import { UserInfo } from '../UserInfo';
+import { Todo as TodoItem } from '../../types/Todo';
+import './TodoInfo.scss';
+
+interface Todo {
+  todo: TodoItem;
+}
+
+export const TodoInfo: React.FC<Todo> = (props) => {
+  const { todo } = props;
+
+  return (
+    <article
+      className={
+        `TodoInfo${
+          todo.completed
+            ? ' TodoInfo--completed'
+            : ''
+        }`
+      }
+    >
+      <h2 className="TodoInfo__title">
+        {todo.title}
+      </h2>
+
+      <UserInfo user={todo.user} />
+    </article>
+  );
+};
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,4 +1,20 @@
-// Add the required types and props
-export const UserInfo = () => (
-  <>UserInfo markup</>
-);
+import React from 'react';
+import { User as UserItem } from '../../types/User';
+import './UserInfo.scss';
+
+type User = {
+  user: UserItem | null;
+};
+
+export const UserInfo: React.FC<User> = (props) => {
+  const { user } = props;
+
+  return (
+    <a
+      className="UserInfo"
+      href={`mailto:${user?.email}`}
+    >
+      {user?.name}
+    </a>
+  );
+};
