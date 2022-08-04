7ba6aa2526d0f92dd042003046320dcdc6253c90
Danil Hodytskyi <danil.hodytskyi1105@gmail.com>
2022-08-04 11:28:28 +0300

    implement static list of todos

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import { TodoInfo } from "../TodoInfo";
-import { Todo } from "../../types/Todo";
+import React from 'react';
+import { TodoInfo } from '../TodoInfo';
+import { Todo } from '../../types/Todo';
 
 type Props = {
   todos: Todo[]
-}
+};
 
-export const TodoList: React.FC<Props> = ({todos}) => (
+export const TodoList: React.FC<Props> = ({ todos }) => (
   <section className="ToDoList">
     {todos.map(todo => <TodoInfo todo={todo} key={todo.id} />)}
   </section>
