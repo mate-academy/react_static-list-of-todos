8c02cd12f912461aaeee5b97233643daf168f748
Gena Kupriiuk <gamesgolden04@gmail.com>
2022-08-04 14:11:23 +0300

    Solution corrected according to the checklist

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -12,20 +12,18 @@ export const TodoInfo: React.FC<Props> = ({ todo }) => {
   const { title, completed, user } = todo;
 
   return (
-    <>
-      <article className={classNames(
-        'TodoInfo',
-        {
-          'TodoInfo--completed': completed,
-        },
-      )}
-      >
-        <h2 className="TodoInfo__title">{title}</h2>
-      </article>
+    <article className={classNames(
+      'TodoInfo',
+      {
+        'TodoInfo--completed': completed,
+      },
+    )}
+    >
+      <h2 className="TodoInfo__title">{title}</h2>
 
       {user && (
         <UserInfo user={user} />
       )}
-    </>
+    </article>
   );
 };
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -9,9 +9,7 @@ type Props = {
 };
 
 export const TodoList: React.FC<Props> = ({ todos }) => (
-  <>
-    <section className="TodoList">
-      {todos.map(todo => <TodoInfo todo={todo} key={todo.id} />)}
-    </section>
-  </>
+  <section className="TodoList">
+    {todos.map(todo => <TodoInfo todo={todo} key={todo.id} />)}
+  </section>
 );
