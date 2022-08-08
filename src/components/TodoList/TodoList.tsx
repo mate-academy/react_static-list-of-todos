51ba67eb83ccdf29edaa98c7c225b0bb34a8b2b0
Vitalii-Heiko <papus@ukr.net.com>
2022-08-08 13:38:09 +0300

    first commit

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,22 @@
+import { TodoInfo } from '../TodoInfo';
+import { Todo } from '../../types/Todo';
+
 // Add the required types and props
-export const TodoList = () => (
-  <>TodoList markup</>
+type Props = {
+  todos: Todo[],
+};
+
+export const TodoList: React.FC<Props> = (props) => (
+  <>
+    <section className="TodoList">
+      {props.todos.map(todo => (
+        <TodoInfo
+          key={todo.id}
+          title={todo.title}
+          completed={todo.completed}
+          user={todo.user}
+        />
+      ))}
+    </section>
+  </>
 );
