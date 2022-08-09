074f5bbfa6e8ea7d2bc341d920a86f21141e7a46
Koba_ <kovbasinskyv@gmail.com>
2022-08-09 17:49:35 +0300

    solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,22 @@
 // Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo';
+
+type Props = {
+  todo: Todo
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => (
+  <>
+    <article
+      key={todo.id}
+      className={
+        todo.completed ? 'TodoInfo TodoInfo--completed' : 'TodoInfo'
+      }
+    >
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+
+      <UserInfo user={todo.user} />
+    </article>
+  </>
 );
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,22 @@
 // Add the required types and props
-export const TodoList = () => (
-  <>TodoList markup</>
+import React from 'react';
+import { Todo } from '../../types/Todo';
+import { TodoInfo } from '../TodoInfo';
+
+type Props = {
+  preparedTodos: Todo[]
+};
+
+export const TodoList: React.FC<Props> = ({ preparedTodos }) => (
+  <>
+    <div className="App">
+      <h1 className="App__title">Static list of todos</h1>
+
+      <section className="TodoList">
+        {preparedTodos.map(todos => (
+          <TodoInfo todo={todos} />
+        ))}
+      </section>
+    </div>
+  </>
 );
