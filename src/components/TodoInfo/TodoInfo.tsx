d6db852c87ee355f768008740972807870066111
Bogdan Prokopenko <pb.dvlpr@gmail.com>
2022-08-10 18:57:06 +0300

    React Static List Of Todos

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,26 @@
+import React from 'react';
+import classNames from 'classnames';
+import { UserInfo } from '../UserInfo/UserInfo';
+import { Todo } from '../../types/Todo';
+
+type Prop = {
+  todo: Todo;
+};
+
 // Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
+export const TodoInfo: React.FC<Prop> = ({ todo }) => (
+  <>
+    <article className={
+      classNames('TodoInfo', {
+        'TodoInfo--completed': todo.completed,
+      })
+    }
+    >
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+
+      {todo.user && (
+        <UserInfo user={todo.user} />
+      )}
+    </article>
+  </>
 );
