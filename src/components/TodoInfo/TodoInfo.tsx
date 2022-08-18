65b035c5552b740fbe81a083381f9336d3b84ef2
Vladyslava Buzova <dev.vladyslava@gmail.com>
2022-08-19 01:05:34 +0800

    first solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,20 @@
+import { UserInfo } from '../UserInfo/UserInfo';
+
 // Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
+
+import { Todo } from '../../types/Todo';
+import { User } from '../../types/User';
+
+type Props = {
+  todo: Todo,
+  users: User[],
+};
+
+export const TodoInfo:React.FC<Props> = ({ todo, users }) => (
+  <>
+
+    <h2 className="TodoInfo__title">{todo.title}</h2>
+
+    <UserInfo users={users} />
+  </>
 );
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,4 +1,23 @@
 // Add the required types and props
-export const UserInfo = () => (
-  <>UserInfo markup</>
+
+import { User } from '../../types/User';
+
+type Props = {
+  users: User[],
+};
+
+export const UserInfo:React.FC<Props> = ({ users }) => (
+  <>
+
+    {users.map(user => (
+      <a
+        className="UserInfo"
+        href={`mailto:${user.email}`}
+        key={user.id}
+      >
+        {user.name}
+      </a>
+    ))}
+
+  </>
 );
