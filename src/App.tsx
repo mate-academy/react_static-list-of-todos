006a2fee20c85e299b966ddaef10af160d88b985
Aliud13 <lidiamishchuk@gmail.com>
2022-08-24 20:28:29 +0100

    solution2

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import usersFromServer from './api/users';
 
 import { User } from './types/User';
 import { Todo } from './types/Todo';
+import { TodoList } from './components/TodoList';
 
 function getUser(userId: number): User | null {
   const foundUser = usersFromServer.find(user => user.id === userId);
@@ -25,6 +26,7 @@ console.log(todos);
 const App: React.FC = () => (
   <div className="App">
     <h1 className="App__title">Static list of todos</h1>
+    <TodoList todos={todos} />
   </div>
 );
 
