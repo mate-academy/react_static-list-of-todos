91b8e86d9c15fdc30fc58a093cb22411762b4919
Nazar Kulchevych <gamebandit38@gmail.com>
2022-08-25 14:58:16 +0300

    done destructurization

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -7,17 +7,21 @@ type Props = {
   todo: Todo;
 };
 
-export const TodoInfo: React.FC<Props> = ({ todo }) => (
-  <article
-    className={classNames(
-      'TodoInfo',
-      {
-        'TodoInfo--completed': todo.completed,
-      },
-    )}
-  >
-    <h2 className="TodoInfo__title">{todo.title}</h2>
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const { title, completed, user } = todo;
 
-    {todo.user && <UserInfo user={todo.user} />}
-  </article>
-);
+  return (
+    <article
+      className={classNames(
+        'TodoInfo',
+        {
+          'TodoInfo--completed': completed,
+        },
+      )}
+    >
+      <h2 className="TodoInfo__title">{title}</h2>
+
+      {user && <UserInfo user={user} />}
+    </article>
+  );
+};
