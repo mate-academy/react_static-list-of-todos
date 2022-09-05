8ff881cfd49042626dd103c7d1c0cabe035df0e1
Mark Shapka <mark.shapka1@gmail.com>
2022-09-05 19:40:00 +0300

    Task_react-static-list-of-todos

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,32 @@
+import React from 'react';
+import classNames from 'classnames';
+
+import { Todo } from '../../types/Todo';
+import { TodoInfo } from '../TodoInfo';
+import { UserInfo } from '../UserInfo';
+
 // Add the required types and props
-export const TodoList = () => (
-  <>TodoList markup</>
+type Props = {
+  todos: Todo[];
+};
+
+export const TodoList: React.FC<Props> = ({ todos = [] }) => (
+  <section className="TodoList">
+    {todos.map((todo) => (
+      <article
+        className={classNames(
+          'TodoInfo',
+          { 'TodoInfo--completed': todo.completed },
+        )}
+        key={todo.id}
+      >
+        <TodoInfo title={todo.title} />
+        {
+          todo.user
+            ? <UserInfo name={todo.user.name} email={todo.user.email} />
+            : ''
+        }
+      </article>
+    ))}
+  </section>
 );
