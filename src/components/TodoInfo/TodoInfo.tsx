49fc32c64489b28e4c852d090602817fb772ff90
Konstantin Ivanchenko <89596175+Konsttantin@users.noreply.github.com>
2022-09-06 18:29:42 +0300

    classNames style fix

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -8,9 +8,12 @@ type Props = {
 
 export const TodoInfo: React.FC<Props> = ({ todo }) => (
   <article
-    className={classNames('TodoInfo', {
-      'TodoInfo--completed': todo.completed,
-    })}
+    className={classNames(
+      'TodoInfo',
+      {
+        'TodoInfo--completed': todo.completed,
+      },
+    )}
   >
     <h2 className="TodoInfo__title">{todo.title}</h2>
 
