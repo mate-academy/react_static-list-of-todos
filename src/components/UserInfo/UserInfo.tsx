2c1ee766e2584e60c1e457239e8ea6a615e1072c
Oksana Melnyk <kseniadanyluk1994@gmail.com>
2022-09-07 17:20:47 +0300

    added changes according to comments

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -6,9 +6,7 @@ type Props = {
 };
 
 export const UserInfo: React.FC<Props> = ({ user: { name, email } }) => (
-  <>
-    <a className="UserInfo" href={`mailto:${email}`}>
-      {name}
-    </a>
-  </>
+  <a className="UserInfo" href={`mailto:${email}`}>
+    {name}
+  </a>
 );
