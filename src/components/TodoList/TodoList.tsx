081259137ea298a130d129096e86b1667f5f4c75
Pavel Ohiiko <timurid346@gmail.com>
2022-09-07 23:54:59 +0300

    Added develop branch

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,14 +1,4 @@
-import { TodoInfo } from '../TodoInfo';
-import { Todo } from '../../types/Todo';
-
-type Props = {
-  todos: Todo[];
-};
-
-export const TodoList: React.FC <Props> = ({ todos }) => (
-  <section className="TodoList">
-    {todos.map(todo => (
-      <TodoInfo todo={todo} key={todo.id} />
-    ))}
-  </section>
+// Add the required types and props
+export const TodoList = () => (
+  <>TodoList markup</>
 );
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,11 +1,4 @@
-import { User } from '../../types/User';
-
-type Props = {
-  user: User;
-};
-
-export const UserInfo:React.FC<Props> = ({ user }) => (
-  <a className="UserInfo" href={`mailto:${user.email}`}>
-    {user.name}
-  </a>
+// Add the required types and props
+export const UserInfo = () => (
+  <>UserInfo markup</>
 );
