7fc4fd6e8623bc1c4c274fb5aec4abd2064138f8
Max Kravchenko <max.kravchenko@gmail.com>
2022-09-07 17:55:03 +0300

    Make React Static list of todos.

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,11 @@ import usersFromServer from './api/users';
 import { User } from './types/User';
 import { Todo } from './types/Todo';
 
+import { TodoList } from './components/TodoList/TodoList';
+
 function getUser(userId: number): User | null {
   const foundUser = usersFromServer.find(user => user.id === userId);
 
-  // if there is no user with a given userId
   return foundUser || null;
 }
 
@@ -22,28 +23,6 @@ export const todos: Todo[] = todosFromServer.map(todo => ({
 export const App: React.FC = () => (
   <div className="App">
     <h1 className="App__title">Static list of todos</h1>
-    <section className="TodoList">
-      {todos.map((todo) => (
-        todo.completed
-          ? (
-            <article className="TodoInfo TodoInfo--completed">
-              <h2 className="TodoInfo__title">{todo.title}</h2>
-
-              <a className="UserInfo" href={todo.user?.email}>
-                {todo.user?.name}
-              </a>
-            </article>
-          )
-          : (
-            <article className="TodoInfo">
-              <h2 className="TodoInfo__title">{todo.title}</h2>
-
-              <a className="UserInfo" href={todo.user?.email}>
-                {todo.user?.name}
-              </a>
-            </article>
-          )
-      ))}
-    </section>
+    <TodoList todos={todos} />
   </div>
 );
--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,37 @@
-// Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+import React from 'react';
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo/UserInfo';
+import './TodoInfo.scss';
+
+type Props = {
+  todo: Todo;
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const {
+    title,
+    completed,
+  } = todo;
+
+  return (
+    completed
+      ? (
+        <article className="TodoInfo TodoInfo--completed">
+          <h2 className="TodoInfo__title">{title}</h2>
+          <UserInfo
+            key={todo.user?.id}
+            user={todo?.user}
+          />
+        </article>
+      )
+      : (
+        <article className="TodoInfo">
+          <h2 className="TodoInfo__title">{title}</h2>
+          <UserInfo
+            key={todo.user?.id}
+            user={todo?.user}
+          />
+        </article>
+      )
+  );
+};
