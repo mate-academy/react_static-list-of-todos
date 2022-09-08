6368188c86b291adf6dc9dbfd12654c319a01b0e
Oksana Kyryienko <kseniyals1609@gmail.com>
2022-09-08 17:55:12 +0300

    solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,28 @@
-// Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+import React from 'react';
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo';
+
+type Props = {
+  todo: Todo;
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const {
+    title,
+    user,
+    completed,
+  } = todo;
+
+  return (
+    <article className={
+      completed === true
+        ? 'TodoInfo TodoInfo--completed'
+        : 'TodoInfo'
+    }
+    >
+
+      <h2 className="TodoInfo__title">{title}</h2>
+      {user && <UserInfo user={user} />}
+    </article>
+  );
+};
