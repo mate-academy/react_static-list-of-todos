21f31bc0817a5500237fcc5dc019f38c63e0e42d
Ganna Krainiuk <anya.krainiuk@gmail.com>
2022-09-08 15:57:40 +0300

    react static list of todos solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,22 @@
-// Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+import React from 'react';
+import { Todo } from '../../types/Todo';
+
+import { UserInfo } from '../UserInfo/UserInfo';
+
+type TodoType = {
+  todo: Todo;
+};
+
+export const TodoInfo: React.FC<TodoType> = ({ todo }) => {
+  const { title, user, completed } = todo;
+
+  return (
+    <article className={completed
+      ? 'TodoInfo TodoInfo--completed'
+      : 'TodoInfo'}
+    >
+      <h2 className="TodoInfo__title">{title}</h2>
+      {user && <UserInfo user={user} />}
+    </article>
+  );
+};
