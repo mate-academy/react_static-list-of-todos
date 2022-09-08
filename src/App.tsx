57e9df3a4c51ac9d8dea52f432032f6972c2a56c
Sasha Gordijchuk <san4ez1717@gmail.com>
2022-09-08 14:59:55 +0300

    static lost of todos

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import usersFromServer from './api/users';
 
 import { User } from './types/User';
 import { Todo } from './types/Todo';
+import { TodoList } from './components/TodoList';
 
 function getUser(userId: number): User | null {
   const foundUser = usersFromServer.find(user => user.id === userId);
@@ -22,6 +23,8 @@ export const todos: Todo[] = todosFromServer.map(todo => ({
 export const App: React.FC = () => (
   <div className="App">
     <h1 className="App__title">Static list of todos</h1>
+
+    <TodoList todos={todos} />
 
     <section className="TodoList">
       <article className="TodoInfo TodoInfo--completed">
