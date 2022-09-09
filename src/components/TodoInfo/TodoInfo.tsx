f0df3b62d11d00dc9e7a3792cb2b972e56a61885
Oleksandr Sobchuk <osobchuk1@gmail.com>
2022-09-09 20:32:20 +0300

    React TODO list

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,19 @@
-// Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo';
+
+type TodoType = {
+  todo: Todo;
+};
+
+export const TodoInfo: React.FC<TodoType> = ({ todo }) => {
+  const { title, completed } = todo;
+
+  return (
+    <>
+      <article className={`TodoInfo ${completed === true ? 'TodoInfo--completed' : null}`}>
+        <h2 className="TodoInfo__title">{title}</h2>
+        {todo.user ? <UserInfo user={todo.user} /> : 'Anonymous'}
+      </article>
+    </>
+  );
+};
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,14 @@
-// Add the required types and props
-export const TodoList = () => (
-  <>TodoList markup</>
+import { Todo } from '../../types/Todo';
+import { TodoInfo } from '../TodoInfo';
+
+type TodoType = {
+  todos: Todo[];
+};
+
+export const TodoList: React.FC<TodoType> = ({ todos }) => (
+  <>
+    <section className="TodoList">
+      {todos.map(task => <TodoInfo key={task.id} todo={task} />)}
+    </section>
+  </>
 );
