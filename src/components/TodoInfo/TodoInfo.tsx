f78105786bee5d382cc133586b172fcc51c0dc3f
Vladyslav Gusakovskiy <vladyslav.gusakovskiy@gmail.com>
2022-09-11 00:04:41 +0300

    Classnames module was installed and used on classNames

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 import { Todo } from '../../types/Todo';
 import { UserInfo } from '../UserInfo';
 
@@ -14,8 +15,14 @@ export const TodoInfo: React.FC<Props> = ({ todo }) => {
   } = todo;
 
   return (
-    <article className={completed ? 'TodoInfo TodoInfo--completed'
-      : 'TodoInfo'}
+    <article className={
+      classNames(
+        'TodoInfo',
+        {
+          'TodoInfo--completed': completed,
+        },
+      )
+    }
     >
       <h2 className="TodoInfo__title">
         {title}
