d48e39ca47091ac0a79e651eb0660a71c56901bf
Shmelyov Oleg <shmelyov.oleg13@gmail.com>
2022-09-11 18:53:09 +0300

    add: TodoInfo, UserInfo, Todolist components

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,11 @@
-// Add the required types and props
-export const TodoList = () => (
-  <>TodoList markup</>
+import { FC } from 'react';
+import { Todo } from '../../types/Todo';
+import { TodoInfo } from '../TodoInfo';
+
+export const TodoList: FC<{ todos:Array<Todo> }> = ({ todos }) => (
+  <section className="TodoList">
+    {todos.map((todo) => (
+      <TodoInfo todo={todo} key={todo.id} />
+    ))}
+  </section>
 );
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,4 +1,10 @@
+import { FC } from 'react';
+import { User } from '../../types/User';
+import './UserInfo.scss';
+
 // Add the required types and props
-export const UserInfo = () => (
-  <>UserInfo markup</>
+export const UserInfo: FC<{ user:User }> = ({ user }) => (
+  <a className="UserInfo" href={`mailto:${user.email}`}>
+    {user.name}
+  </a>
 );
