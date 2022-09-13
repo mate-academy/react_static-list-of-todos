72d40087762974406587eb38ffee7e93ab8029b1
Sergey Pistelev <sergey.pistelev@gmail.com>
2022-09-13 18:32:15 +0300

    REACT static list of todos solution

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,4 +1,16 @@
+import React from 'react';
+import { User } from '../../types/User';
+
+type Props = {
+  user: User;
+};
 // Add the required types and props
-export const UserInfo = () => (
-  <>UserInfo markup</>
-);
+export const UserInfo: React.FC<Props> = ({ user }) => {
+  const { name, email } = user;
+
+  return (
+    <a className="UserInfo" href={`mailto:${email}`}>
+      {name}
+    </a>
+  );
+};
