c51b2c57c89ae1c28cd9c8abf6b75821ecb83017
Oleksandr Sobchuk <osobchuk1@gmail.com>
2022-09-14 20:49:34 +0300

    added classNames library

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import { Todo } from '../../types/Todo';
 import { UserInfo } from '../UserInfo';
 
@@ -9,7 +10,10 @@ export const TodoInfo: React.FC<TodoType> = ({ todo }) => {
   const { title, completed } = todo;
 
   return (
-    <article className={`TodoInfo ${completed === true ? 'TodoInfo--completed' : null}`}>
+    <article className={classNames('TodoInfo', {
+      'TodoInfo--completed': completed === true,
+    })}
+    >
       <h2 className="TodoInfo__title">{title}</h2>
       {todo.user
         ? <UserInfo user={todo.user} />
