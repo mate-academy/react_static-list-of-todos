62d35c385320e3240e4d53d9e2fe0ffdab7c0dee
Oleksandr Malyshev <sanyokmalyshev@gmail.com>
2022-09-25 20:39:39 +0700

    Task done

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import usersFromServer from './api/users';
 
 import { User } from './types/User';
 import { Todo } from './types/Todo';
+import { TodoList } from './components/TodoList';
 
 function getUser(userId: number): User | null {
   const foundUser = usersFromServer.find(user => user.id === userId);
@@ -24,6 +25,10 @@ export const App: React.FC = () => (
     <h1 className="App__title">Static list of todos</h1>
 
     <section className="TodoList">
+      <TodoList todos={todos} />
+    </section>
+
+    {/* <section className="TodoList">
       <article className="TodoInfo TodoInfo--completed">
         <h2 className="TodoInfo__title">HTML</h2>
 
@@ -55,6 +60,6 @@ export const App: React.FC = () => (
           Clementine Bauch
         </a>
       </article>
-    </section>
+    </section> */}
   </div>
 );
