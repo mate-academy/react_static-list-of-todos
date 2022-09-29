9d01410d9b6cc9eafeb8a97684488d5b4de8d3e1
Anastasiia Mahalias <anastasiabatychenko@gmail.com>
2022-09-29 16:57:03 +0300

    Task completed

---
+++ src/components/TodoInfo/TodoInfo.spec.tsx
@@ -0,0 +1,89 @@
+/* eslint-disable max-len */
+/* eslint-disable react/jsx-filename-extension */
+
+import React from 'react';
+import { mount } from '@cypress/react';
+import { TodoInfo } from './TodoInfo';
+
+describe('TodoInfo', () => {
+  describe('', () => {
+    beforeEach(() => {
+      const user1 = {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincere@april.biz',
+      };
+
+      const todo = {
+        userId: 1,
+        id: 1,
+        title: 'Learn HTML',
+        completed: true,
+        user: user1,
+      };
+
+      mount(<TodoInfo todo={todo} />);
+    });
+
+    it('should have a correct title', () => {
+      cy.get('.TodoInfo__title').should('have.text', 'Learn HTML');
+    });
+
+    it('should add TodoInfo--completed class for a completed todo', () => {
+      cy.get('.TodoInfo').should('have.class', 'TodoInfo--completed');
+    });
+
+    it('should have UserInfo', () => {
+      cy.get('.UserInfo').should('have.text', 'Leanne Graham');
+    });
+  });
+
+  describe('', () => {
+    it('should NOT add TodoInfo--completed class for not completed todo', () => {
+      const user2 = {
+        id: 2,
+        name: 'Ervin Howell',
+        username: 'Antonette',
+        email: 'Shanna@melissa.tv',
+      };
+
+      const completedTodo = {
+        userId: 2,
+        id: 10,
+        title: 'Learn CSS',
+        completed: false,
+        user: user2,
+      };
+
+      mount(<TodoInfo todo={completedTodo} />);
+
+      cy.get('.TodoInfo').should('not.have.class', 'TodoInfo--completed');
+    });
+
+    it('should work with the other todo', () => {
+      const user3 = {
+        id: 3,
+        name: 'Clementine Bauch',
+        username: 'Samantha',
+        email: 'Nathan@yesenia.net',
+      };
+
+      const todo48 = {
+        userId: 3,
+        id: 48,
+        title: 'sit reprehenderit omnis quia',
+        completed: true,
+        user: user3,
+      };
+
+      mount(<TodoInfo todo={todo48} />);
+
+      cy.get('.TodoInfo__title')
+        .should('have.text', 'sit reprehenderit omnis quia');
+
+      cy.get('.TodoInfo').should('have.class', 'TodoInfo--completed');
+      cy.get('.UserInfo').should('have.text', 'Clementine Bauch');
+    });
+  });
+});
---
+++ src/components/TodoList/TodoList.spec.tsx
@@ -0,0 +1,75 @@
+/* eslint-disable react/jsx-filename-extension */
+
+import React from 'react';
+import { mount } from '@cypress/react';
+import { TodoList } from './TodoList';
+
+describe('TodoList', () => {
+  describe('', () => {
+    beforeEach(() => {
+      const user1 = {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincere@april.biz',
+      };
+
+      const user2 = {
+        id: 2,
+        name: 'Ervin Howell',
+        username: 'Antonette',
+        email: 'Shanna@melissa.tv',
+      };
+
+      const todos = [
+        {
+          userId: 1,
+          id: 1,
+          title: 'First Todo',
+          completed: false,
+          user: user1,
+        },
+        {
+          userId: 1,
+          id: 2,
+          title: 'Second Todo',
+          completed: false,
+          user: user1,
+        },
+        {
+          userId: 2,
+          id: 3,
+          title: 'Third Todo',
+          completed: false,
+          user: user2,
+        },
+        {
+          userId: 2,
+          id: 4,
+          title: 'Fourth Todo',
+          completed: true,
+          user: user2,
+        },
+      ];
+
+      mount(<TodoList todos={todos} />);
+    });
+
+    it('should contain all the todos', () => {
+      cy.get('.TodoInfo').should('have.length', 4);
+    });
+
+    it('should render todos in the correct order', () => {
+      cy.get('.TodoInfo__title').eq(0).should('have.text', 'First Todo');
+      cy.get('.TodoInfo__title').eq(3).should('have.text', 'Fourth Todo');
+    });
+  });
+
+  describe('', () => {
+    it('should render no todos is received an empty array', () => {
+      mount(<TodoList todos={[]} />);
+
+      cy.get('.TodoInfo').should('not.exist');
+    });
+  });
+});
