0fa49a4df5f38e81c810725df219522f50d3311e
Ihor Andrianov <andrianoia@gmail.com>
2022-10-02 22:00:41 +0300

    solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,27 @@
 // Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+import React from 'react';
+import { UserInfo } from '../UserInfo';
+import { Todo } from '../../types/Todo';
+
+type TodoProp = {
+  todo: Todo;
+};
+
+export const TodoInfo: React.FC<TodoProp> = ({ todo }) => {
+  const {
+    id, userId, title, completed, user,
+  } = todo;
+  const isCompleted = completed
+    ? 'TodoInfo TodoInfo--completed'
+    : 'TodoInfo';
+
+  return (
+    <article className={isCompleted} key={id}>
+      <h2 className="TodoInfo__title">
+        {title}
+      </h2>
+
+      <UserInfo user={user} key={userId} />
+    </article>
+  );
+};
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,4 +1,22 @@
 // Add the required types and props
-export const UserInfo = () => (
-  <>UserInfo markup</>
-);
+import React from 'react';
+import { User } from '../../types/User';
+
+type UserProps = {
+  user: User | null;
+};
+
+export const UserInfo: React.FC<UserProps> = ({ user }) => {
+  if (user) {
+    const { email, name } = user;
+    const mailTo = `mailto:${email}`;
+
+    return (
+      <a className="UserInfo" href={mailTo}>
+        {name}
+      </a>
+    );
+  }
+
+  return <></>;
+};
