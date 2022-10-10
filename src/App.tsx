d02bbee575e04f6a595545fec02fad6857ef6809
grafit-off <graf.off.it@gmail.com>
2022-10-10 17:54:13 +0300

    feat: todo components were implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import 'bulma';
+
 import './App.scss';
 
 import todosFromServer from './api/todos';
@@ -6,11 +8,11 @@ import usersFromServer from './api/users';
 
 import { User } from './types/User';
 import { Todo } from './types/Todo';
+import { TodoList } from './components/TodoList';
 
 function getUser(userId: number): User | null {
   const foundUser = usersFromServer.find(user => user.id === userId);
 
-  // if there is no user with a given userId
   return foundUser || null;
 }
 
@@ -20,41 +22,11 @@ export const todos: Todo[] = todosFromServer.map(todo => ({
 }));
 
 export const App: React.FC = () => (
-  <div className="App">
-    <h1 className="App__title">Static list of todos</h1>
+  <section className="App container section">
+    <h1 className="App__title title is-1">Static list of todos</h1>
 
-    <section className="TodoList">
-      <article className="TodoInfo TodoInfo--completed">
-        <h2 className="TodoInfo__title">HTML</h2>
-
-        <a className="UserInfo" href="mailto:Sincere@april.biz">
-          Leanne Graham
-        </a>
-      </article>
-
-      <article className="TodoInfo TodoInfo--completed">
-        <h2 className="TodoInfo__title">CSS</h2>
-
-        <a className="UserInfo" href="mailto:Sincere@april.biz">
-          Leanne Graham
-        </a>
-      </article>
-
-      <article className="TodoInfo TodoInfo--completed">
-        <h2 className="TodoInfo__title">JS</h2>
-
-        <a className="UserInfo" href="mailto:Shanna@melissa.tv">
-          Ervin Howell
-        </a>
-      </article>
-
-      <article className="TodoInfo">
-        <h2 className="TodoInfo__title">React</h2>
-
-        <a className="UserInfo" href="mailto:Nathan@yesenia.net">
-          Clementine Bauch
-        </a>
-      </article>
-    </section>
-  </div>
+    <div className="TodoList">
+      <TodoList todos={todos} />
+    </div>
+  </section>
 );
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,28 @@
-// Add the required types and props
-export const TodoList = () => (
-  <>TodoList markup</>
+import classNames from 'classnames';
+
+import { TodoInfo } from '../TodoInfo';
+
+import { Todo } from '../../types/Todo';
+
+type Props = {
+  todos: Todo[];
+};
+
+export const TodoList: React.FC<Props> = ({ todos }) => (
+  <ul>
+    {todos.map((todo) => (
+      <li
+        key={todo.id}
+        className={classNames(
+          'message',
+          {
+            'is-danger': todo.completed,
+            'is-primary': !todo.completed,
+          },
+        )}
+      >
+        <TodoInfo todo={todo} />
+      </li>
+    ))}
+  </ul>
 );
