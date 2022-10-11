ee60640cf34a597a4f91384bcdf09a60b126c1d9
Myroslav Diiak <diiakm23819@gmail.com>
2022-10-11 21:45:59 +0300

    solution

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,20 @@
-// Add the required types and props
-export const TodoList = () => (
-  <>TodoList markup</>
+import React from 'react';
+
+import { Todo } from '../../types/Todo';
+import { TodoInfo } from '../TodoInfo';
+
+type Props = {
+  todos: Todo[]
+};
+
+export const TodoList: React.FC<Props> = ({ todos }) => (
+  <>
+    <ul className="TodoList">
+      {todos.map(todo => (
+        <li key={todo.id}>
+          <TodoInfo todo={todo} />
+        </li>
+      ))}
+    </ul>
+  </>
 );
