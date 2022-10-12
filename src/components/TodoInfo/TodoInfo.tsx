bee70ba1ff78be677637907fd3410918904257a3
Andriy Hladkevych <andriyglad@gmail.com>
2022-10-12 19:10:23 +0300

    Todo List completed

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,31 @@
 // Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
+import React from 'react';
+import classNames from 'classnames';
+
+import { UserInfo } from '../UserInfo';
+
+import { Todo } from '../../types/Todo';
+
+type Props = {
+  todo: Todo,
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => (
+  <article
+    className={classNames(
+      'TodoInfo',
+      {
+        'TodoInfo--completed': todo.completed === true,
+      },
+    )}
+    key={todo.id}
+  >
+    <h2 className="TodoInfo__title">{todo.title}</h2>
+
+    {
+      todo.user
+        ? <UserInfo user={todo.user} />
+        : 'Unknown'
+    }
+  </article>
 );
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,18 @@
 // Add the required types and props
-export const TodoList = () => (
-  <>TodoList markup</>
+import React from 'react';
+import { Todo } from '../../types/Todo';
+import { TodoInfo } from '../TodoInfo';
+
+type Props = {
+  todos: Todo[],
+};
+
+export const TodoList: React.FC<Props> = ({ todos }) => (
+  <>
+    {
+      todos.map(todo => (
+        <TodoInfo todo={todo} key={todo.id} />
+      ))
+    }
+  </>
 );
