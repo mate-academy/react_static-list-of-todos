95f7615a3aeab84021911869b7767d10d46111eb
Dima <ddikey1@gmail.com>
2022-10-13 01:44:19 +0300

    Solution of "Todos List" task

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,12 @@
-/// <reference types="react-scripts" />
+export interface User {
+  id: number,
+  name: string,
+  email: string,
+}
+export interface Todo {
+  id: number,
+  userId: number,
+  title: string,
+  completed: boolean,
+  user: User | null,
+}
