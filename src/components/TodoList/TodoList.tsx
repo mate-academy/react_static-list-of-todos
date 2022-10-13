313022c5f01c7041724ff6cd42e49891c8e7a258
George.kom <komolovua@gmail.com>
2022-10-14 02:58:46 +0300

    App.jsx splited according to following rules

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,17 @@
-// Add the required types and props
-export const TodoList = () => (
-  <>TodoList markup</>
-);
+import { TodoInfo } from '../TodoInfo';
+import { Todo } from '../../types/Todo';
+
+interface Prop {
+  todos: Todo[];
+}
+export const TodoList: React.FC<Prop> = ({ todos }) => {
+  const list = todos.map((task) => (
+    <TodoInfo key={task.id.toString()} todo={task} />
+  ));
+
+  return (
+    <>
+      {list || null}
+    </>
+  );
+};
