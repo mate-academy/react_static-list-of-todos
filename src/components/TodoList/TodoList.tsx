15cc7b65376e44f56234c5c9cc1e5aa0638cdc03
Boris Kerekesha <kerekesha90@ukr.net>
2022-10-13 11:59:31 +0300

    react static list

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,17 @@
-// Add the required types and props
-export const TodoList = () => (
-  <>TodoList markup</>
+import React from 'react';
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo';
+
+type TodosItem = {
+  todosItem: Todo[]
+};
+
+export const TodoList: React.FC<TodosItem> = ({ todosItem }) => (
+  <ul>
+    {todosItem.map(todo => (
+      <li key={todo.id}>
+        <UserInfo {...todo} />
+      </li>
+    ))}
+  </ul>
 );
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,4 +1,60 @@
-// Add the required types and props
-export const UserInfo = () => (
-  <>UserInfo markup</>
+import React from 'react';
+import classNames from 'classnames';
+import { Todo } from '../../types/Todo';
+
+export const UserInfo: React.FC<Todo> = ({
+  userId,
+  id,
+  title,
+  completed,
+  user,
+}) => (
+  <article
+    className={classNames('TodoInfo', { 'TodoInfo--completed': completed })}
+  >
+    <h2
+      className="TodoInfo__title"
+    >
+      {title}
+    </h2>
+    <a className="UserInfo" href="mailto:Sincere@april.biz">
+      <span>
+        userId:
+        {userId}
+      </span>
+      <br />
+      <span>
+        id:
+        {id}
+      </span>
+      <br />
+      <span>
+        title:
+        {title}
+      </span>
+      <br />
+      <span>
+        completed:
+        {completed ? 'true' : 'false'}
+      </span>
+      {user
+        ? (
+          <ul>
+            <span>
+              user
+            </span>
+            <li>
+              {user?.name}
+            </li>
+            <li>
+              {user?.id}
+            </li>
+            <li>
+              {user?.email}
+            </li>
+          </ul>
+        )
+        : ''}
+    </a>
+  </article>
 );
