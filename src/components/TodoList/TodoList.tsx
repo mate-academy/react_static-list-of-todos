374f1814f86e1d491695e16011d6dd3dc4dc565f
Nikita Ovchinnikov <nikitaovch99@gmail.com>
2022-10-13 16:32:42 +0300

    added key with unique id

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -8,6 +8,6 @@ type Props = {
 
 export const TodoList: React.FC<Props> = ({ todos }) => (
   <section className="TodoList">
-    {todos.map((todo) => <TodoInfo todo={todo} />)}
+    {todos.map((todo) => <TodoInfo todo={todo} key={todo.id} />)}
   </section>
 );
