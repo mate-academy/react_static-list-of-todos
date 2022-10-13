a4019537bdc0445b044eedf90ad2bcf94fc0834a
Serhii Hluhovskyi <glugovsky@proton.me>
2022-10-13 15:49:04 +0300

    Added semantic tags to the list

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -3,9 +3,11 @@ import { Todo } from '../../types/Todo';
 import { TodoInfo } from '../TodoInfo';
 
 export const TodoList: React.FC<{ todos: Todo[] }> = ({ todos }) => (
-  <section className="TodoList">
+  <ul className="TodoList">
     {todos.map(todo => (
-      <TodoInfo todo={todo} key={todo.id} />
+      <li>
+        <TodoInfo todo={todo} key={todo.id} />
+      </li>
     ))}
-  </section>
+  </ul>
 );
