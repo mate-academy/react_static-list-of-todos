7b08ecde67407b3fd71d760c8a8120a745d6370a
Anna Borodaienko <anna.borodayenko@gmail.com>
2022-10-13 12:07:38 +0300

    Added semantic list tags, deleted redundant code

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -11,17 +11,16 @@ export const TodoInfo: React.FC<Props> = ({ todo }) => {
   const { title, completed } = todo;
 
   return (
-    <>
-      <article
-        className={classNames('TodoInfo', { 'TodoInfo--completed': completed })}
-      >
-        <h2 className="TodoInfo__title">{title}</h2>
+    <li
+      className={classNames('TodoInfo', { 'TodoInfo--completed': completed })}
+    >
+      <h2 className="TodoInfo__title">
+        {title}
+      </h2>
 
-        {(todo.user) && (
-          <UserInfo user={todo.user} />
-        )}
-
-      </article>
-    </>
+      {(todo.user) && (
+        <UserInfo user={todo.user} />
+      )}
+    </li>
   );
 };
