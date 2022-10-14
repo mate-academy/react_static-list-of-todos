51286f38145d454fcff26b5425dcf9422830886b
igor-1987 <igor.gayvoronsky@gmail.com>
2022-10-14 20:32:18 +0300

    v1: todos list was separated

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,31 @@
-// Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+import classNames from 'classnames';
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo';
+import './TodoInfo.scss';
+
+type Props = {
+  todo: Todo;
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const {
+    title,
+    completed,
+    user,
+  } = todo;
+
+  return (
+    <article
+      className={classNames(
+        'TodoInfo',
+        { 'TodoInfo--completed': completed },
+      )}
+    >
+      <h2 className="TodoInfo__title">
+        { title || 'Todo was not founded!' }
+      </h2>
+
+      {user && <UserInfo user={user} />}
+    </article>
+  );
+};
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,4 +1,24 @@
-// Add the required types and props
-export const UserInfo = () => (
-  <>UserInfo markup</>
-);
+import { User } from '../../types/User';
+import './UserInfo.scss';
+
+type Props = {
+  user: User
+};
+
+export const UserInfo: React.FC<Props> = ({ user }) => {
+  const {
+    name,
+    email,
+  } = user;
+
+  return !user
+    ? <span className="UserInfo">User name was not founded!</span>
+    : (
+      <a
+        className="UserInfo"
+        href={email && `mailto:${email}`}
+      >
+        { name && name }
+      </a>
+    );
+};
