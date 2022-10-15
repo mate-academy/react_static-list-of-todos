aa106c1d70b23ea3934d47714b6326508495fd48
Andriy Andrieiev <proph7000@gmail.com>
2022-10-15 20:14:27 +0300

    Fixes after code review

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
+import classNames from 'classnames';
+import { User } from '../../types/User';
+import { UserInfo } from '../UserInfo';
 
 type Props = {
   title: string;
+  completed: boolean;
+  user: User | null;
+  id: number;
 };
 
-export const TodoInfo: React.FC<Props> = ({ title }) => (
-  <h2 className="TodoInfo__title">
-    { title }
-  </h2>
+function capitalized(string: string): string {
+  return string.slice(0, 1).toUpperCase() + string.slice(1);
+}
+
+export const TodoInfo: React.FC<Props> = ({ title, completed, user }) => (
+  <article
+    className={classNames(
+      'TodoInfo',
+      {
+        'TodoInfo--completed': completed,
+      },
+    )}
+  >
+    <h2 className="TodoInfo__title">
+      { capitalized(title) }
+    </h2>
+
+    {user
+      && <UserInfo {...user} />}
+  </article>
 );
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,32 +1,14 @@
-import classNames from 'classnames';
 import { Todo } from '../../types/Todo';
 import { TodoInfo } from '../TodoInfo';
-import { UserInfo } from '../UserInfo';
 
 type Props = {
   todosServer: Todo[];
 };
 
-function capitalized(string: string): string {
-  return string.slice(0, 1).toUpperCase() + string.slice(1);
-}
-
 export const TodoList: React.FC<Props> = ({ todosServer = [] }) => (
   <section className="TodoList">
     {todosServer.map((todo) => (
-      <article
-        key={todo.id}
-        className={classNames(
-          'TodoInfo',
-          {
-            'TodoInfo--completed': todo.completed,
-          },
-        )}
-      >
-        <TodoInfo title={capitalized(todo.title)} />
-        {todo.user
-          && <UserInfo name={todo.user.name} email={todo.user.email} />}
-      </article>
+      <TodoInfo {...todo} key={todo.id} />
     ))}
   </section>
 );
