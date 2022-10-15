f7543c9d26f99c10cd0fd29d0003eb144ed6cf89
Arsen-Shpak <a.shpakovich2000@gmail.com>
2022-10-15 13:13:47 +0300

    react_static_list

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,22 @@
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo';
+
 // Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
+type Props = {
+  todo: Todo;
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => (
+  <>
+    <article
+      className={
+        todo.completed
+          ? 'TodoInfo TodoInfo--completed'
+          : 'TodoInfo'
+      }
+    >
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+      {todo.user && <UserInfo user={todo.user} />}
+    </article>
+  </>
 );
