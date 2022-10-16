7d36f2e0fbdb26be079fc862ce2693c780c9f1a4
Iryna Borniak <la.aerea.1@gmail.com>
2022-10-16 03:30:09 +0300

    feat: add classNames

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import { Todo } from '../../types/Todo';
 import { UserInfo } from '../UserInfo/UserInfo';
 
@@ -14,7 +15,12 @@ export const TodoInfo: React.FC<Props> = ({ todo }) => {
 
   return (
     <article
-      className={completed ? 'TodoInfo TodoInfo--completed' : 'TodoInfo'}
+      className={classNames(
+        'TodoInfo',
+        {
+          'TodoInfo--completed': completed,
+        },
+      )}
     >
       <h2 className="TodoInfo__title">
         {title}
