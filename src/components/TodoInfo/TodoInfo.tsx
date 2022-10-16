908a64ef406c89b2bae02dacb881eb29b8435d28
Vova Dzhuryk <dzhurykvovik@gmail.com>
2022-10-16 17:41:37 +0300

    implemented Static list

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,22 @@
-// Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+import React from 'react';
+import classNames from 'classnames';
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo';
+
+export const TodoInfo: React.FC<{ todo: Todo }> = ({ todo }) => {
+  const { title, completed, user } = todo;
+
+  return (
+    <article
+      className={classNames(
+        'TodoInfo',
+        { 'TodoInfo--completed': completed },
+      )}
+    >
+      <h2 className="TodoInfo__title">{title}</h2>
+      {user && (
+        <UserInfo user={user} />
+      )}
+    </article>
+  );
+};
