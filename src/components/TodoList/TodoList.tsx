c3441c0ff821153273e627d6dc90cfa98edafd90
Roman Veremchuk <roman.veremchuk.ki.2020@lpnu.ua>
2022-10-18 20:39:57 +0300

    use semantic tags

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -8,9 +8,13 @@ type Props = {
 };
 
 export const TodoList: React.FC<Props> = ({ todos }) => (
-  <section className="TodoList">
-    {todos.map(todo => (
-      <TodoInfo todo={todo} key={todo.id} />
-    ))}
-  </section>
+  <ul className="TodoList">
+    {
+      todos.map((todo) => (
+        <li key={todo.id}>
+          <TodoInfo todo={todo} />
+        </li>
+      ))
+    }
+  </ul>
 );
