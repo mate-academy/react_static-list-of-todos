921487227a85743aef548580d217036251cfcb6a
poznianski <poznianski@gmail.com>
2022-10-19 17:32:14 +0300

    made destructuring and added key for list

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -7,10 +7,15 @@ type Props = {
 };
 
 export const UserInfo: React.FC<Props> = ({ user }) => {
+  const {
+    email,
+    name,
+  } = user;
+
   return (
     <>
-      <a className="UserInfo" href={`mailto:${user.email}`}>
-        {user.name}
+      <a className="UserInfo" href={`mailto:${email}`}>
+        {name}
       </a>
     </>
   );
