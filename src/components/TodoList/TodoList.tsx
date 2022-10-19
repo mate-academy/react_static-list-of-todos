b02c1a53af5c3df3f9a2a081267f2938587a298d
Danil <dasha8854567@gmail.com>
2022-10-19 16:20:19 +0300

    commit1

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,24 @@
-// Add the required types and props
-export const TodoList = () => (
-  <>TodoList markup</>
+import classNames from 'classnames';
+import { TodoInfo } from '../TodoInfo';
+import { Todo } from '../../types/Todo';
+
+type Props = {
+  todosAr: Todo[],
+};
+
+export const TodoList: React.FC<Props> = ({ todosAr }) => (
+  <section className="TodoList">
+    {todosAr.map((todo) => (
+      <article
+        className={classNames(
+          'TodoInfo',
+          { 'TodoInfo--completed': todo.completed },
+        )}
+        key={todo.id}
+      >
+        <TodoInfo {...todo} />
+      </article>
+    ))}
+
+  </section>
 );
--- src/types/User.ts
+++ src/types/User.ts
@@ -1,5 +1,5 @@
 export interface User {
-  id: number,
-  name: string,
-  email: string,
+  id?: number,
+  name?: string,
+  email?: string,
 }
