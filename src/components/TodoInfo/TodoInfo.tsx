67549d9963956b17d690d3ec773ab2eb7c4e342a
Artem Hulko <agulko96@gmail.com>
2022-10-22 17:01:11 +0300

    fixed some mistakes

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 import { Todo } from '../../types/Todo';
 import { UserInfo } from '../UserInfo';
 
@@ -7,17 +8,17 @@ type Props = {
 };
 
 export const TodoInfo: React.FC<Props> = ({ todo }) => {
-  const classSet = todo.completed
-    ? 'TodoInfo TodoInfo--completed'
-    : 'TodoInfo';
-
   return (
-    <>
-      <article className={classSet}>
-        <h2 className="TodoInfo__title">{todo.title}</h2>
+    <article className={classNames(
+      'TodoInfo',
+      { 'TodoInfo--completed': todo.completed },
+    )}
+    >
+      <h2 className="TodoInfo__title">{todo.title}</h2>
 
+      {todo.user && (
         <UserInfo user={todo.user} />
-      </article>
-    </>
+      )}
+    </article>
   );
 };
