f3e4e994aa30b850afe5cc80385b834c1262ea91
Valeria Petropavlovska <llerriya>
2022-10-24 15:57:05 +0300

    destruction was used

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -7,16 +7,24 @@ type Props = {
   todo: Todo
 };
 
-export const TodoInfo: React.FC<Props> = ({ todo }) => (
-  <article
-    className={classNames(
-      'TodoInfo',
-      { 'TodoInfo--completed': todo.completed === true },
-    )}
-  >
-    <h2 className={classNames('TodoInfo__title')}>{todo.title}</h2>
-    {todo.user && (
-      <UserInfo user={todo.user} />
-    )}
-  </article>
-);
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const {
+    user,
+    title,
+    completed,
+  } = todo;
+
+  return (
+    <article
+      className={classNames(
+        'TodoInfo',
+        { 'TodoInfo--completed': completed === true },
+      )}
+    >
+      <h2 className="TodoInfo__title">{title}</h2>
+      {user && (
+        <UserInfo user={user} />
+      )}
+    </article>
+  );
+};
