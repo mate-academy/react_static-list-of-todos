9b7fcfea4211a22e20e5848eee0bc5c3eb1b1130
Gizem Yilmaz <gizemeceylmaz@gmail.com>
2022-10-25 16:05:02 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import './App.scss';
 import todosFromServer from './api/todos';
 import usersFromServer from './api/users';
-
 import { User } from './types/User';
 import { Todo } from './types/Todo';
 import { TodoList } from './components/TodoList';
 
 function getUser(userId: number): User | null {
   const foundUser = usersFromServer.find(user => user.id === userId);
-  // if there is no user with a given userId
 
   return foundUser || null;
 }
