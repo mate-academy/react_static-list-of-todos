2e3f8d7cf90ae37437b37ce2b588ba772a509c78
Sapna Choudhary <sapnachoudhary97491@gmail.com>
2022-10-26 21:05:27 +0200

    Solution for list of todos

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,17 @@
+import { Todo } from '../../types/Todo';
+import { TodoInfo } from '../TodoInfo';
+
 // Add the required types and props
-export const TodoList = () => (
-  <>TodoList markup</>
+type Props = {
+  todos: Todo[];
+};
+
+export const TodoList: React.FC<Props> = ({ todos }) => (
+  <>
+    <section className="TodoList">
+      {todos.map(currentTodo => (
+        <TodoInfo todo={currentTodo} />
+      ))}
+    </section>
+  </>
 );
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,4 +1,14 @@
+import { User } from '../../types/User';
+
 // Add the required types and props
-export const UserInfo = () => (
-  <>UserInfo markup</>
+type Props = {
+  user: User;
+};
+
+export const UserInfo: React.FC<Props> = ({ user }) => (
+  <>
+    <a className="UserInfo" href={`mailto:${user.email}`}>
+      {user.name}
+    </a>
+  </>
 );
