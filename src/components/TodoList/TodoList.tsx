949407d2540955eca2aedcf78539174fe8eabd12
Maks Manalatii <manmaks122@gmail.com>
2022-10-27 12:57:29 +0300

    make code clearly

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -9,6 +9,8 @@ type Props = {
 
 export const TodoList: React.FC<Props> = ({ todos }) => (
   <section className="TodoList">
-    {todos.map(todo => <TodoInfo todo={todo} key={todo.id} />)}
+    {todos.map(todo => (
+      <TodoInfo todo={todo} key={todo.id} />
+    ))}
   </section>
 );
