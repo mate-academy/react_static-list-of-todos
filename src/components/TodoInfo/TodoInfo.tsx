d91e88106501940d58df88f85a7f7e2141c185b5
Artem66 <melodpi@gmail.com>
2022-11-16 12:54:40 +0000

    split App

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,26 @@
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo';
+
 // Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+
+type Props = {
+  item: Todo
+};
+
+export const TodoInfo: React.FC<Props> = ({ item }) => {
+  const {
+    completed,
+    title,
+    user,
+  } = item;
+
+  return (
+    <article className={!completed
+      ? 'TodoInfo'
+      : 'TodoInfo  TodoInfo--completed'}
+    >
+      <h2 className="TodoInfo__title">{title}</h2>
+      {user && <UserInfo user={user} />}
+    </article>
+  );
+};
