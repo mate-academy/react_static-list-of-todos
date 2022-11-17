d068307022de258469628a9f274f27e7561ce0a6
Yaro Bey <eltrece1@gmail.com>
2022-11-17 23:33:57 +0300

    another try

--- cypress/integration/page.spec.js
+++ cypress/integration/page.spec.js
@@ -10,7 +10,7 @@ describe('Page', () => {
   it('should render todos in correct order', () => {
     cy.get('.TodoInfo__title').eq(0).should('have.text', 'delectus aut autem');
     cy.get('.TodoInfo__title').eq(199).should('have.text', 'ipsam aperiam voluptates qui');
-  });
+  });ru
 
   it('should render corresponding users', () => {
     cy.get('.UserInfo').should('have.length', 200);
