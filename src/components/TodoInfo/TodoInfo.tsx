c9a92845422fc974a9b7290af7a5be915372557e
Hlazman <glazman.b@gmail.com>
2022-11-17 15:40:09 +0200

    add destructuring assignment

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -6,14 +6,18 @@ type Props = {
   todo: Todo;
 };
 
-export const TodoInfo: React.FC<Props> = ({ todo }) => (
-  <article className={classNames(
-    'TodoInfo', { 'TodoInfo--completed': todo.completed },
-  )}
-  >
-    <h2 className="TodoInfo__title">{todo.title}</h2>
-    {todo.user && (
-      <UserInfo user={todo.user} />
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const { user, completed, title } = todo;
+
+  return (
+    <article className={classNames(
+      'TodoInfo', { 'TodoInfo--completed': completed },
     )}
-  </article>
-);
+    >
+      <h2 className="TodoInfo__title">{title}</h2>
+      {user && (
+        <UserInfo user={user} />
+      )}
+    </article>
+  );
+};
