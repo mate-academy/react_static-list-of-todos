3d7fae65c0cb5b3bc9e5e4e65647115678ac9e68
Danyla Krygin <underworldsan@gmail.com>
2022-11-18 15:15:43 +0200

    Made correction

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -11,7 +11,10 @@ export const UserInfo: React.FC<Props> = ({ user }) => {
   } = user;
 
   return (
-    <a className="UserInfo" href={`mailto:${email}`}>
+    <a
+      className="UserInfo"
+      href={`mailto:${email}`}
+    >
       {name}
     </a>
   );
