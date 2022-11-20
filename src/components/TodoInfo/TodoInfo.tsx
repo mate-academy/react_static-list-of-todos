1d6f320daf85f9432fa267193b82363fea40698f
Sergii Iatsenko <sergii.iatsenko@gmail.com>
2022-11-21 00:04:59 +0200

    Solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,9 @@
-// Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
+import React from 'react';
+
+type Props = {
+  title: string;
+};
+
+export const TodoInfo: React.FC<Props> = ({ title }) => (
+  <h2 className="TodoInfo__title">{title}</h2>
 );
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,32 @@
-// Add the required types and props
-export const TodoList = () => (
-  <>TodoList markup</>
+import React from 'react';
+import classNames from 'classnames';
+
+import { Todo } from '../../types/Todo';
+import { TodoInfo } from '../TodoInfo/TodoInfo';
+import { UserInfo } from '../UserInfo/UserInfo';
+
+type Props = {
+  todos: Todo[];
+};
+
+export const TodoList: React.FC<Props> = ({ todos = [] }) => (
+  <section className="TodoList">
+    {todos.map(todo => (
+      <article
+        key={todo.id}
+        className={classNames(
+          'TodoInfo',
+          {
+            'TodoInfo--completed': todo.completed,
+          },
+        )}
+      >
+        <TodoInfo title={todo.title} />
+
+        {todo.user
+         && (<UserInfo name={todo.user.name} email={todo.user.email} />)}
+
+      </article>
+    ))}
+  </section>
 );
