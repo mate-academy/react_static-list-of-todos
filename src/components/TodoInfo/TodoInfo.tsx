11d35778e5b9e8f2938dc280ccd50baf893c339a
Kostya Berehoviy <kostyaber3@gmail.com>
2022-11-21 14:31:14 +0200

    List Of Todos Solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,23 @@
-// Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+import './TodoInfo.scss';
+
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo';
+
+type Props = {
+  todo: Todo
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const { title, user, completed } = todo;
+
+  return (
+    <article className={completed
+      ? 'TodoInfo TodoInfo--completed'
+      : 'TodoInfo'}
+    >
+      <h2 className="TodoInfo__title">{title}</h2>
+
+      <UserInfo user={user} />
+    </article>
+  );
+};
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,4 +1,24 @@
-// Add the required types and props
-export const UserInfo = () => (
-  <>UserInfo markup</>
-);
+import './UserInfo.scss';
+import { User } from '../../types/User';
+
+type Props = {
+  user: User | null
+};
+
+export const UserInfo:React.FC<Props> = ({ user }) => {
+  if (user) {
+    const { email, name } = user;
+
+    return (
+      <a className="UserInfo" href={email}>
+        {name}
+      </a>
+    );
+  }
+
+  return (
+    <a className="UserInfo" href="Email is not defined">
+      User is not defined
+    </a>
+  );
+};
