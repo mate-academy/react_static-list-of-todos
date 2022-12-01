31919ed4ded0f7a6e8e92a07bf63e81a1f9e5e70
Vitalii Derlemenko <vitaliidox@gmail.com>
2022-12-01 21:56:21 +0200

    Solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,30 @@
 // Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo';
+
+type TodoItem = {
+  props: Todo
+};
+
+export const TodoInfo: React.FC<TodoItem> = ({ props }) => {
+  // const {
+  //   title,
+  //   completed,
+  //   user,
+  // } = props;
+
+  if (props.user) {
+    return (
+      <article className={`TodoInfo${props.completed && ' TodoInfo--completed'}`}>
+        <h2 className="TodoInfo__title">{props.title}</h2>
+        <UserInfo user={props.user} />
+      </article>
+    );
+  }
+
+  return (
+    <article className={`TodoInfo${props.completed && ' TodoInfo--completed'}`}>
+      <h2 className="TodoInfo__title">{props.title}</h2>
+    </article>
+  );
+};
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,4 +1,16 @@
 // Add the required types and props
-export const UserInfo = () => (
-  <>UserInfo markup</>
-);
+import { User } from '../../types/User';
+
+type UserData = {
+  user: User
+};
+
+export const UserInfo: React.FC<UserData> = ({ user = {} }) => {
+  // const { name, email } = user;
+
+  return (
+    <a className="UserInfo" href={`mailto:${user.email}`}>
+      {user.name}
+    </a>
+  );
+};
