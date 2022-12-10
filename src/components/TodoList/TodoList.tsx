21364f22f0747d8cb8d5576edfe16bf426d8abad
Umut Deniz Darendeli <108900524+UmizDemud@users.noreply.github.com>
2022-12-10 22:34:43 +0300

    Update src/components/TodoList/TodoList.tsx
    Co-authored-by: Misha Grinko <glukym@gmail.com>

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -8,8 +8,8 @@ type Props = {
 
 export const TodoList: React.FC<Props> = ({ todos }) => (
   <section className="TodoList">
-    {
-      todos.map(todo => <TodoInfo key={todo.id} todo={todo} />)
-    }
+    {todos.map(todo => (
+      <TodoInfo key={todo.id} todo={todo} />
+    ))}
   </section>
 );
