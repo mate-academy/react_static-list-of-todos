46dc1187e586d7a0aace7bff9a7903d86c3a4641
Anton Martyniuk <antonmartyniuk@gmail.com>
2022-12-18 16:13:34 +0200

    Solution react_static_list_of_todos

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,15 @@
-// Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
+import React from 'react';
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo/UserInfo';
+
+type Props = {
+  data: Todo;
+};
+
+export const TodoInfo: React.FC<Props> = ({ data }) => (
+  <>
+    <h2 className="TodoInfo__title">{data.title}</h2>
+
+    <UserInfo user={data.user} />
+  </>
 );
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,22 @@
-// Add the required types and props
-export const TodoList = () => (
-  <>TodoList markup</>
+import React from 'react';
+import { Todo } from '../../types/Todo';
+import { TodoInfo } from '../TodoInfo/TodoInfo';
+
+type Props = {
+  todos: Todo[];
+};
+
+export const TodoList: React.FC<Props> = ({ todos }) => (
+  <>
+    <section className="TodoList">
+      {todos.map(todo => (
+        <article
+          className={`TodoInfo ${todo.completed && 'TodoInfo--completed'}`}
+          key={todo.id}
+        >
+          <TodoInfo data={todo} />
+        </article>
+      ))}
+    </section>
+  </>
 );
