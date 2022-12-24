5b8984984be7e3b720b443aa38b2e816d2de102a
Nurdan Okcu Tanich <nurokcudan@gmail.com>
2022-12-24 19:35:44 +0100

    added destructuring for todoinfo

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,6 @@ export const todos: Todo[] = todosFromServer.map(todo => ({
 export const App: React.FC = () => (
   <div className="App">
     <h1 className="App__title">Static list of todos</h1>
-
     <TodoList todos={todos} />
   </div>
 );
--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -2,23 +2,26 @@ import classNames from 'classnames';
 import { Todo } from '../../types/Todo';
 import { UserInfo } from '../UserInfo';
 
-// Add the required types and props
 type Props = {
   todo: Todo;
 };
 
-export const TodoInfo:React.FC<Props> = ({ todo }) => (
-  <article
-    className={classNames(
-      'TodoInfo',
-      {
-        'TodoInfo--completed': todo.completed,
-      },
-    )}
-  >
+export const TodoInfo:React.FC<Props> = ({ todo }) => {
+  const { title, completed, user } = todo;
 
-    <h2 className="TodoInfo__title">{todo.title}</h2>
+  return (
+    <article
+      className={classNames(
+        'TodoInfo',
+        {
+          'TodoInfo--completed': completed,
+        },
+      )}
+    >
 
-    {todo.user && <UserInfo user={todo.user} />}
-  </article>
-);
+      <h2 className="TodoInfo__title">{title}</h2>
+
+      {user && <UserInfo user={user} />}
+    </article>
+  );
+};
