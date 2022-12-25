32c4822a56a9c5532068bbf54959b8c4a975909f
Nadiia Koch <nadia.dovganenko@gmail.com>
2022-12-25 12:32:06 +0200

    List of TODOs

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.scss';
 
+import { TodoList } from './components/TodoList';
+
 import todosFromServer from './api/todos';
 import usersFromServer from './api/users';
 
@@ -22,39 +24,6 @@ export const todos: Todo[] = todosFromServer.map(todo => ({
 export const App: React.FC = () => (
   <div className="App">
     <h1 className="App__title">Static list of todos</h1>
-
-    <section className="TodoList">
-      <article className="TodoInfo TodoInfo--completed">
-        <h2 className="TodoInfo__title">HTML</h2>
-
-        <a className="UserInfo" href="mailto:Sincere@april.biz">
-          Leanne Graham
-        </a>
-      </article>
-
-      <article className="TodoInfo TodoInfo--completed">
-        <h2 className="TodoInfo__title">CSS</h2>
-
-        <a className="UserInfo" href="mailto:Sincere@april.biz">
-          Leanne Graham
-        </a>
-      </article>
-
-      <article className="TodoInfo TodoInfo--completed">
-        <h2 className="TodoInfo__title">JS</h2>
-
-        <a className="UserInfo" href="mailto:Shanna@melissa.tv">
-          Ervin Howell
-        </a>
-      </article>
-
-      <article className="TodoInfo">
-        <h2 className="TodoInfo__title">React</h2>
-
-        <a className="UserInfo" href="mailto:Nathan@yesenia.net">
-          Clementine Bauch
-        </a>
-      </article>
-    </section>
+    <TodoList todos={todos} />
   </div>
 );
