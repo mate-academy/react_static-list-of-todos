5c302960c2ad0c73b6a58f352c90a9f96903ac1a
Ruslan Pashkov <pashkov@email.com>
2022-12-27 22:17:17 +0200

    Fix code style

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -10,10 +10,7 @@ type Props = {
 export const TodoList: React.FC<Props> = ({ todos }) => (
   <section className="TodoList">
     {todos.map(todo => (
-      <TodoInfo
-        key={todo.id}
-        todo={todo}
-      />
+      <TodoInfo key={todo.id} todo={todo} />
     ))}
   </section>
 );
