07f556bb3beb790645337b73692bfeb55a5bb529
Panianchuk M <panianchuk@gmail.com>
2022-12-27 19:45:38 +0200

    add className

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import { UserInfo } from '../UserInfo';
 import { Todo } from '../../types/Todo';
 
@@ -7,11 +8,13 @@ type Props = {
 
 export const TodoInfo: React.FC<Props> = ({ todo }) => {
   return (
-    <>
-      <article className={`TodoInfo ${todo.completed && 'TodoInfo--completed'}`}>
-        <h2 className="TodoInfo__title">{todo.title}</h2>
-        {todo.user && <UserInfo user={todo.user} />}
-      </article>
-    </>
+    <article className={classNames(
+      'TodoInfo',
+      { 'TodoInfo--completed': todo.completed },
+    )}
+    >
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+      {todo.user && <UserInfo user={todo.user} />}
+    </article>
   );
 };
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -6,9 +6,7 @@ type Props = {
 };
 
 export const TodoList: React.FC<Props> = ({ todos }) => (
-  <>
-    <section className="TodoList">
-      {todos.map(todo => (<TodoInfo todo={todo} key={todo.id} />))}
-    </section>
-  </>
+  <section className="TodoList">
+    {todos.map(todo => (<TodoInfo todo={todo} key={todo.id} />))}
+  </section>
 );
