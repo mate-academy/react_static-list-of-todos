77155fd3c11e98b2fd02ffbd35b27e99758fb1a4
Diana Demkiv <dianademkiv789@gmail.com>
2022-12-27 20:47:27 +0200

    fixed

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -6,16 +6,19 @@ type Props = {
   todo: Todo,
 };
 
-export const TodoInfo: React.FC<Props> = ({ todo }) => (
-  <article className={classNames('TodoInfo',
-    {
-      'TodoInfo--completed': todo.completed,
-    })}
-  >
-    <h2 className="TodoInfo__title">
-      {todo.title}
-    </h2>
-    {todo.user && <UserInfo user={todo.user} />}
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const { title, user, completed } = todo;
 
-  </article>
-);
+  return (
+    <article className={classNames('TodoInfo',
+      {
+        'TodoInfo--completed': completed,
+      })}
+    >
+      <h2 className="TodoInfo__title">
+        {title}
+      </h2>
+      {user && <UserInfo user={user} />}
+    </article>
+  );
+};
