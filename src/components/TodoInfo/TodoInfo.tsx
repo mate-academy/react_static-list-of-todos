8a735f13cea40b932ca55619b10b81d378902b03
Elias Montauti <egsm147@gmail.com>
2022-12-28 12:50:15 +0000

    Solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,14 +1,20 @@
-// Add the required types and props
-
-import { Todo } from '../../types/Todo';
+import cn from 'classnames';
 import { UserInfo } from '../UserInfo';
+import { Todo } from '../../types/Todo';
 
 type Todos = {
   element: Todo;
 };
 
 export const TodoInfo: React.FC<Todos> = ({ element }) => (
-  <article className="TodoInfo TodoInfo--completed">
+  <article
+    className={cn(
+      'TodoInfo',
+      {
+        'TodoInfo--completed': element.completed,
+      },
+    )}
+  >
     <h2 className="TodoInfo__title">{`${element.title}`}</h2>
     {element.user && (
       <UserInfo
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,5 +1,5 @@
-import { Todo } from '../../types/Todo';
 import { TodoInfo } from '../TodoInfo';
+import { Todo } from '../../types/Todo';
 
 type List = {
   elements: Todo[];
