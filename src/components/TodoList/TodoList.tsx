e932064cf7388a3528f69d12b8d221527e076403
VladimirEdelmann <VladimirEdelmann@gmail.com>
2022-12-28 06:56:26 +0200

    Add unique key

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -8,7 +8,7 @@ type Props = {
 export const TodoList: React.FC<Props> = ({ todos }) => (
   <section className="TodoList">
     {
-      todos.map(todo => <TodoInfo todo={todo} />)
+      todos.map(todo => <TodoInfo key={todo.id} todo={todo} />)
     }
   </section>
 );
