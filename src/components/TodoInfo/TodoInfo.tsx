311366c57d11d056a84d3bc33d7081d49fc52653
Nurdan Okcu Tanich <nurokcudan@gmail.com>
2022-12-28 12:59:41 +0100

    fixed accordingly to styling suggestions

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -19,9 +19,13 @@ export const TodoInfo:React.FC<Props> = ({ todo }) => {
       )}
     >
 
-      <h2 className="TodoInfo__title">{title}</h2>
+      <h2 className="TodoInfo__title">
+        {title}
+      </h2>
 
-      {user && <UserInfo user={user} />}
+      {user && (
+        <UserInfo user={user} />
+      )}
     </article>
   );
 };
