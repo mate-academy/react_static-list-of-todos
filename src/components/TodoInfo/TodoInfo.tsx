a150a8a2bc9040084752e6c21fd18dc46581aa82
Mykhailyk Vadym <ihappyspam@gmail.com>
2022-12-28 20:51:37 +0200

    destructuring was created

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -6,16 +6,24 @@ interface Props {
   todo: Todo;
 }
 
-export const TodoInfo: React.FC<Props> = ({ todo }) => (
-  <>
-    <article
-      className={todo.completed
-        ? 'TodoInfo TodoInfo--completed'
-        : 'TodoInfo'}
-    >
-      <h2 className="TodoInfo__title">{todo.title}</h2>
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const {
+    completed,
+    title,
+    user,
+  } = todo;
 
-      {todo.user && <UserInfo user={todo.user} /> }
-    </article>
-  </>
-);
+  return (
+    <>
+      <article
+        className={completed
+          ? 'TodoInfo TodoInfo--completed'
+          : 'TodoInfo'}
+      >
+        <h2 className="TodoInfo__title">{title}</h2>
+
+        {user && <UserInfo user={user} /> }
+      </article>
+    </>
+  );
+};
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -5,10 +5,17 @@ interface Props {
   user: User;
 }
 
-export const UserInfo: React.FC<Props> = ({ user }) => (
-  <>
-    <a className="UserInfo" href={`mailto:${user.email}`}>
-      {user.name}
-    </a>
-  </>
-);
+export const UserInfo: React.FC<Props> = ({ user }) => {
+  const {
+    email,
+    name,
+  } = user;
+
+  return (
+    <>
+      <a className="UserInfo" href={`mailto:${email}`}>
+        {name}
+      </a>
+    </>
+  );
+};
