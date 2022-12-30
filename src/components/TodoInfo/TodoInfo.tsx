b854473922d21e3d138e42f389e5211f84981718
JRijo <99342891+John-Stiff@users.noreply.github.com>
2022-12-30 11:58:16 +0100

    separated FCs from App.tsx

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,29 @@
-// Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+import React from 'react';
+import cn from 'classnames';
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo/UserInfo';
+
+type Props = {
+  todo: Todo;
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const {
+    completed, title, user, id,
+  } = todo;
+
+  return (
+    <article
+      key={id}
+      className={cn('TodoInfo', {
+        'TodoInfo--completed': completed,
+      })}
+    >
+      <h2 className="TodoInfo__title">{title}</h2>
+
+      {user && (
+        <UserInfo user={user} />
+      )}
+    </article>
+  );
+};
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,55 +1,9 @@
 import React from 'react';
-import cn from 'classnames';
 import { Todo } from '../../types/Todo';
-import { User } from '../../types/User';
+import { TodoInfo } from '../TodoInfo/TodoInfo';
 
 type Props = {
   todos: Todo[];
-};
-
-type Props2 = {
-  todo: Todo;
-};
-
-type Props3 = {
-  user: User;
-};
-
-export const UserInfo: React.FC<Props3> = ({ user }) => {
-  const {
-    name,
-    email,
-  } = user;
-
-  return (
-    <a className="UserInfo" href={`mailto:${email}`}>
-      {name}
-    </a>
-  );
-};
-
-export const TodoInfo: React.FC<Props2> = ({ todo }) => {
-  const {
-    completed,
-    title,
-    user,
-    id,
-  } = todo;
-
-  return (
-    <article
-      key={id}
-      className={cn('TodoInfo', {
-        'TodoInfo--completed': completed,
-      })}
-    >
-      <h2 className="TodoInfo__title">{title}</h2>
-
-      {user && (
-        <UserInfo user={user} />
-      )}
-    </article>
-  );
 };
 
 export const TodoList: React.FC<Props> = ({ todos }) => (
