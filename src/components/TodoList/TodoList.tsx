ebc99aac886ae3f6992259e7d1b2265e643a0a1b
JRijo <99342891+John-Stiff@users.noreply.github.com>
2022-12-30 11:46:57 +0100

    created functional components

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,61 @@
-// Add the required types and props
-export const TodoList = () => (
-  <>TodoList markup</>
+import React from 'react';
+import cn from 'classnames';
+import { Todo } from '../../types/Todo';
+import { User } from '../../types/User';
+
+type Props = {
+  todos: Todo[];
+};
+
+type Props2 = {
+  todo: Todo;
+};
+
+type Props3 = {
+  user: User;
+};
+
+export const UserInfo: React.FC<Props3> = ({ user }) => {
+  const {
+    name,
+    email,
+  } = user;
+
+  return (
+    <a className="UserInfo" href={`mailto:${email}`}>
+      {name}
+    </a>
+  );
+};
+
+export const TodoInfo: React.FC<Props2> = ({ todo }) => {
+  const {
+    completed,
+    title,
+    user,
+    id,
+  } = todo;
+
+  return (
+    <article
+      key={id}
+      className={cn('TodoInfo', {
+        'TodoInfo--completed': completed,
+      })}
+    >
+      <h2 className="TodoInfo__title">{title}</h2>
+
+      {user && (
+        <UserInfo user={user} />
+      )}
+    </article>
+  );
+};
+
+export const TodoList: React.FC<Props> = ({ todos }) => (
+  <section className="TodoList">
+    {todos.map((todo) => (
+      <TodoInfo key={todo.id} todo={todo} />
+    ))}
+  </section>
 );
