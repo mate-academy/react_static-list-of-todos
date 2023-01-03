a476b6d996beacc4ec1f57a0776f57b96554030a
Daryna Zinchenko <zinchenko.daryna@gmail.com>
2023-01-03 13:18:32 +0100

    fixed

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -9,10 +9,7 @@ type Props = {
 export const TodoList: React.FC<Props> = ({ todos }) => (
   <section className="TodoList">
     {todos.map(todo => (
-      <TodoInfo
-        todo={todo}
-        key={todo.id}
-      />
+      <TodoInfo todo={todo} key={todo.id} />
     ))}
   </section>
 );
