6e1c3dfdec17ed71a2793ff22270116b4301b8d8
Babis <babisanagnostou94@gmail.com>
2023-01-03 19:01:30 +0200

    Added classnames

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import cn from 'classnames';
 import { Todo } from '../../types/Todo';
 import { UserInfo } from '../UserInfo/UserInfo';
 
@@ -13,10 +14,14 @@ export const TodoInfo:React.FC<Props> = ({ todo }) => {
     user,
   } = todo;
 
-  const isCompleted = completed ? 'TodoInfo--completed' : '';
-
   return (
-    <article className={`TodoInfo ${isCompleted}`}>
+    <article className={cn(
+      'TodoInfo',
+      {
+        'TodoInfo--completed': completed,
+      },
+    )}
+    >
       <h2 className="TodoInfo__title">{title}</h2>
 
       {user && <UserInfo user={user} />}
