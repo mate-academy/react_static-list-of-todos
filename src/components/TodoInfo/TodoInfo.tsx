520a12890103c83f6df58cd217aabec1d3bb7480
Oleksandr Dehtiarov <retabo.kainrou@gmail.com>
2023-01-04 13:04:05 +0200

    Fixed code

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,23 +1,35 @@
 import React from 'react';
+import './TodoInfo.scss';
+
 import classNames from 'classnames';
 import { Todo } from '../../types/Todo';
 import { UserInfo } from '../UserInfo';
-import './TodoInfo.scss';
 
-type Props = {
-  todo: Todo,
+interface Props {
+  todo: Todo;
+}
+
+export const TodoInfo:React.FC<Props> = ({ todo }) => {
+  const {
+    title,
+    completed,
+    user,
+  } = todo;
+
+  return (
+    <article
+      className={classNames(
+        'TodoInfo',
+        {
+          'TodoInfo--completed': completed,
+        },
+      )}
+    >
+      <h2 className="TodoInfo__title">{title}</h2>
+
+      {user && (
+        <UserInfo user={user} />
+      )}
+    </article>
+  );
 };
-
-export const TodoInfo: React.FC<Props> = ({ todo }) => (
-  <article
-    className={classNames(
-      'TodoInfo',
-      { 'TodoInfo--completed': todo.completed },
-    )}
-  >
-    <h2 className="TodoInfo__title">{todo.title}</h2>
-    {todo.user && (
-      <UserInfo user={todo.user} />
-    )}
-  </article>
-);
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
-
-import { User } from '../../types/User';
 import './UserInfo.scss';
 
-type Props = {
-  user: User;
-};
+import { User } from '../../types/User';
 
-export const UserInfo: React.FC<Props> = ({ user }) => (
-  <a className="UserInfo" href={`mailto:${user.email}`}>
-    {user.name}
-  </a>
-);
+interface Props {
+  user: User
+}
+
+export const UserInfo:React.FC<Props> = ({ user }) => {
+  const {
+    name,
+    email,
+  } = user;
+
+  return (
+    <a className="UserInfo" href={`mailto:${email}`}>
+      {name}
+    </a>
+  );
+};
