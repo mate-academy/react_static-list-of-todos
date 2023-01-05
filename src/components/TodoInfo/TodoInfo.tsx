43de965900cd93720f8bf5ad55028253d2e2080b
Olena Yanovska <alena.yn.adw@gmail.com>
2023-01-06 01:17:43 +0200

    destructuring added

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -7,16 +7,22 @@ type Props = {
   todo: Todo;
 };
 
-export const TodoInfo: React.FC<Props> = ({ todo }) => (
-  <article
-    className={cn(
-      'TodoInfo',
-      { 'TodoInfo--completed': todo.completed },
-    )}
-  >
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const {
+    completed,
+    title,
+    user,
+  } = todo;
 
-    <h2 className="TodoInfo__title">{todo.title}</h2>
-
-    {todo.user && <UserInfo user={todo.user} />}
-  </article>
-);
+  return (
+    <article
+      className={cn(
+        'TodoInfo',
+        { 'TodoInfo--completed': completed },
+      )}
+    >
+      <h2 className="TodoInfo__title">{title}</h2>
+      {user && <UserInfo user={user} />}
+    </article>
+  );
+};
