7c9524ad942e074b9d2cdbb82b4fbdbdc430f60b
Eugene P <eugenep@MacBook-Pro.local>
2023-01-15 16:55:24 +0200

    staticListOfTodos v0.1

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,16 @@
-// Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
+import { Todo } from "../../types/Todo";
+import { UserInfo } from "../UserInfo";
+
+interface TodoInfoProps {
+  todo: Todo;
+}
+
+export const TodoInfo = ({ todo }: TodoInfoProps) => (
+  <article
+    className={todo.completed ? "TodoInfo TodoInfo--completed" : "TodoInfo"}
+  >
+    <h2 className="TodoInfo__title">{todo.title}</h2>
+
+    <UserInfo user={todo.user} />
+  </article>
 );
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,4 +1,7 @@
-// Add the required types and props
-export const UserInfo = () => (
-  <>UserInfo markup</>
+import { User } from "../../types/User";
+
+export const UserInfo = ({ user }: { user: User | null }) => (
+  <a className="UserInfo" href={`mailto:${user?.email}`}>
+    {user?.name}
+  </a>
 );
