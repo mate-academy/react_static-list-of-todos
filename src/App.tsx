4bf94f42573f8eaa942215084b7c42afe4e90815
Romeo Kiss <kiss.romeo.richard@gmail.com>
2023-01-20 15:14:32 +0200

    Splitted into components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.scss';
-import classNames from 'classnames';
 import todosFromServer from './api/todos';
 import usersFromServer from './api/users';
 
 import { User } from './types/User';
 import { Todo } from './types/Todo';
+import { TodoList } from './components/TodoList';
 
 function getUser(userId: number): User | null {
   const foundUser = usersFromServer.find(user => user.id === userId);
@@ -22,22 +22,6 @@ export const todos: Todo[] = todosFromServer.map(todo => ({
 export const App: React.FC = () => (
   <div className="App">
     <h1 className="App__title">Static list of todos</h1>
-
-    <section className="TodoList">
-      {todos.map(todo => (
-        <article
-          className={classNames('TodoInfo', {
-            'TodoInfo--completed': todo.completed,
-          })}
-          key={todo.id}
-        >
-          <h2 className="TodoInfo__title">{todo.title}</h2>
-
-          <a className="UserInfo" href={`mailto:${todo.user?.email}`}>
-            {todo.user?.name}
-          </a>
-        </article>
-      ))}
-    </section>
+    <TodoList todos={todos} />
   </div>
 );
