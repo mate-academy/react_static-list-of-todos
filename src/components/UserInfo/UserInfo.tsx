e3e144ac367d4cd8e942b4fdfab352d03dda0948
miamarichka <ilchyshyn.marichka@gmail.com>
2023-01-21 10:06:52 +0300

    done

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -9,10 +9,8 @@ export const UserInfo: React.FC<Props> = ({ user }) => {
   const { email, name } = user;
 
   return (
-    <>
-      <a className="UserInfo" href={`mailto:${email}`}>
-        {name}
-      </a>
-    </>
+    <a className="UserInfo" href={`mailto:${email}`}>
+      {name}
+    </a>
   );
 };
