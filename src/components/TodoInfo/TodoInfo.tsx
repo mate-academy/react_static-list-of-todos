cd3c55dbf8f3fe161666982ec440d5d59c3c7a92
Konstantin Burnazaki <kostas.burnazaki@gmail.com>
2023-01-22 21:58:29 +0200

    initial

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,17 @@
-// Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
+import { FC } from 'react';
+import classNames from 'classnames';
+
+import { UserInfo } from '../UserInfo';
+
+import { Todo } from '../../types/Todo';
+
+export const TodoInfo: FC<Todo> = ({ completed, title, user }) => (
+  <article
+    className={classNames('TodoInfo', {
+      'TodoInfo--completed': completed === true,
+    })}
+  >
+    <h2 className="TodoInfo__title">{title}</h2>
+    {user && <UserInfo {...user} />}
+  </article>
 );
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,4 +1,12 @@
-// Add the required types and props
-export const UserInfo = () => (
-  <>UserInfo markup</>
-);
+import { FC } from 'react';
+import { User } from '../../types/User';
+
+export const UserInfo: FC<User> = ({ email, name }) => {
+  const link = `mailto:${email}`;
+
+  return (
+    <a className="UserInfo" href={link}>
+      {name}
+    </a>
+  );
+};
