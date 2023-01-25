9b71cd2a55917fd45c2edb5201e8c815594bf97f
Olya Zaiets <zayets.olga@gmail.com>
2023-01-25 23:56:26 +0100

    added changes

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -16,12 +16,11 @@ export const TodoInfo: React.FC<Props> = ({ todo }) => {
   } = todo;
 
   return (
-    <article className={classNames(
-      'TodoInfo',
-      {
+    <article className={
+      classNames('TodoInfo', {
         'TodoInfo--completed': completed,
-      },
-    )}
+      })
+    }
     >
       <h2 className="TodoInfo__title">{title}</h2>
       {user && <UserInfo user={user} />}
