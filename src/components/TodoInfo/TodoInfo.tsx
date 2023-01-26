a211abdf36eeb9cc52853d55cb81085457666151
Tretiak Serhii <tretyak8936@gmail.com>
2023-01-26 17:21:10 +0200

    changed scss

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -3,6 +3,8 @@ import classNames from 'classnames';
 import { Todo } from '../../types/Todo';
 import { UserInfo } from '../UserInfo';
 
+import './TodoInfo.scss';
+
 type Props = {
   todo: Todo,
 };
