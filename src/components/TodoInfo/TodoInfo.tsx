9ed8241831f3913c935016740b3b440393bedb2a
Dima Korol <korovaidima@gmail.com>
2023-01-31 20:30:37 +0100

    solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,40 @@
-// Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+import classNames from 'classnames';
+
+import { User } from '../../types/User';
+import { UserInfo } from '../UserInfo';
+
+type Props = {
+  todo: {
+    userId: number,
+    id: number,
+    title: string,
+    completed: boolean,
+    user: User | null,
+  };
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const {
+    title,
+    completed,
+    user,
+  } = todo;
+
+  return (
+    <>
+      <article
+        className={classNames(
+          'TodoInfo',
+          {
+            'TodoInfo--completed': completed === true,
+          },
+        )}
+      >
+        {user
+          && (
+            <UserInfo title={title} user={user} />
+          )}
+      </article>
+    </>
+  );
+};
---
+++ src/todos.ts
@@ -0,0 +1,17 @@
+import todosFromServer from './api/todos';
+import usersFromServer from './api/users';
+
+import { User } from './types/User';
+import { Todo } from './types/Todo';
+
+function getUser(userId: number): User | null {
+  const foundUser = usersFromServer.find(user => user.id === userId);
+
+  // if there is no user with a given userId
+  return foundUser || null;
+}
+
+export const todos: Todo[] = todosFromServer.map(todo => ({
+  ...todo,
+  user: getUser(todo.userId),
+}));
