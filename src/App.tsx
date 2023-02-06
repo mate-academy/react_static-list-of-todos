8d4237b2914de23e1c475d8a1e39bd04e5d2e1d2
Karadzha Mykola <nicholaskaradzha@gmail.com>
2023-02-06 20:57:08 +0000

    react static list of todos task completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.scss';
-
 import todosFromServer from './api/todos';
 import usersFromServer from './api/users';
-
 import { User } from './types/User';
 import { Todo } from './types/Todo';
+import { TodoInfo } from './components/TodoInfo';
 
 function getUser(userId: number): User | null {
   const foundUser = usersFromServer.find(user => user.id === userId);
@@ -24,37 +23,9 @@ export const App: React.FC = () => (
     <h1 className="App__title">Static list of todos</h1>
 
     <section className="TodoList">
-      <article className="TodoInfo TodoInfo--completed">
-        <h2 className="TodoInfo__title">HTML</h2>
-
-        <a className="UserInfo" href="mailto:Sincere@april.biz">
-          Leanne Graham
-        </a>
-      </article>
-
-      <article className="TodoInfo TodoInfo--completed">
-        <h2 className="TodoInfo__title">CSS</h2>
-
-        <a className="UserInfo" href="mailto:Sincere@april.biz">
-          Leanne Graham
-        </a>
-      </article>
-
-      <article className="TodoInfo TodoInfo--completed">
-        <h2 className="TodoInfo__title">JS</h2>
-
-        <a className="UserInfo" href="mailto:Shanna@melissa.tv">
-          Ervin Howell
-        </a>
-      </article>
-
-      <article className="TodoInfo">
-        <h2 className="TodoInfo__title">React</h2>
-
-        <a className="UserInfo" href="mailto:Nathan@yesenia.net">
-          Clementine Bauch
-        </a>
-      </article>
+      {todos.map(
+        todo => <TodoInfo {...todo} key={todo.id} />,
+      )}
     </section>
   </div>
 );
