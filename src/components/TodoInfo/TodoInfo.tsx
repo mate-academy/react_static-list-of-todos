e3e1e16689f13281f687db1cf6ff1e1cd334a95c
Skuratovskyi Vladyslav <vladskur0369@gmail.com>
2023-02-07 22:09:04 +0200

    Solution v0.1

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,35 @@
+import classNames from 'classnames';
+import { UserInfo } from '../UserInfo/UserInfo';
+
+import users from '../../api/users';
+
+import { Todo } from '../../types/Todo';
+import { User } from '../../types/User';
+
 // Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+
+function findPeople(user: User | null, userid:number) {
+  return users
+    .find((people) => people.id === userid || people.name === user?.name);
+}
+
+export const TodoInfo = ({ todo }: { todo: Todo }) => {
+  const {
+    userId,
+    // id,
+    title,
+    completed,
+    user,
+  } = todo;
+  const userData = findPeople(user, userId);
+
+  return (
+    <article className={
+      classNames('TodoInfo', { 'TodoInfo--completed': completed })
+    }
+    >
+      <h2 className="TodoInfo__title">{title}</h2>
+      {userData === undefined ? null : <UserInfo user={userData} />}
+    </article>
+  );
+};
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,17 @@
+import { TodoInfo } from '../TodoInfo/TodoInfo';
+import { Todo } from '../../types/Todo';
+
+type TodoType = {
+  todos: Todo[];
+};
+
 // Add the required types and props
-export const TodoList = () => (
-  <>TodoList markup</>
+export const TodoList = ({ todos }: TodoType) => (
+  <section className="TodoList">
+    { todos.map((todosTask) => {
+      return (
+        <TodoInfo todo={todosTask} key={todosTask.userId} />
+      );
+    })}
+  </section>
 );
