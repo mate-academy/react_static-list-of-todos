12af512d69ce79c00ab4da3b79e127ad3f1dc3ca
Aleks <offsset@gmail.com>
2023-02-08 16:14:57 +0200

    Solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -3,15 +3,19 @@ import { User } from '../../types/User';
 
 type Propss = {
   title: string;
-  person: User ;
+  person: User | null ;
 };
 
 export const TodoInfo: React.FC<Propss> = ({ title, person }) => (
   <article className="TodoInfo TodoInfo--completed">
     <h2 className="TodoInfo__title">{title}</h2>
-    <UserInfo
-      name={person.name}
-      email={person.email}
-    />
+    {
+      person && (
+        <UserInfo
+          name={person.name}
+          email={person.email}
+        />
+      )
+    }
   </article>
 );
