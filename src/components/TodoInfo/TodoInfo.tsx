fb8bafccb2a47867fe26747430ea0f894d9cf81d
Anton Chetveryk <chetverykantonma@gmail.com>
2023-02-09 16:28:02 +0200

    done

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,20 @@
-// Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+/* eslint-disable max-len */
+import classNames from 'classnames';
+import React from 'react';
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo';
+
+type TodoInfoProps = {
+  todo: Todo
+};
+
+export const TodoInfo:React.FC<TodoInfoProps> = ({ todo }) => {
+  const { title, user, completed } = todo;
+
+  return (
+    <article className={classNames('TodoInfo', { 'TodoInfo--completed': completed })}>
+      <h2 className="TodoInfo__title">{title}</h2>
+      <UserInfo user={user} />
+    </article>
+  );
+};
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,4 +1,20 @@
-// Add the required types and props
-export const UserInfo = () => (
-  <>UserInfo markup</>
-);
+import React from 'react';
+import { User } from '../../types/User';
+
+type UserInfoProps = {
+  user: User | null
+};
+
+export const UserInfo:React.FC<UserInfoProps> = ({ user }) => {
+  if (user) {
+    const { email, name } = user;
+
+    return (
+      <a className="UserInfo" href={email}>
+        {name}
+      </a>
+    );
+  }
+
+  return null;
+};
