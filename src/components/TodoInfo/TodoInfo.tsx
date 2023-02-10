92ee872ab1efa9cd7016b89bb32f06446af4edc2
Skuratovskyi Vladyslav <vladskur0369@gmail.com>
2023-02-10 16:28:22 +0200

    Solution v0.5: add little changes

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -9,23 +9,21 @@ import { User } from '../../types/User';
 // Add the required types and props
 
 function findPeople(user: User | null, userId: number) {
-  return users
-    .find((people) => people.id === userId || people.name === user?.name);
+  return (
+    users.find(
+      (people) => people.id === userId || people.name === user?.name,
+    ) || null);
 }
 
 export const TodoInfo = ({ todo }: { todo: Todo }) => {
   const {
-    userId,
-    title,
-    completed,
-    user,
+    userId, title, completed, user,
   } = todo;
   const userData = findPeople(user, userId);
 
   return (
-    <article className={
-      classNames('TodoInfo', { 'TodoInfo--completed': completed })
-    }
+    <article
+      className={classNames('TodoInfo', { 'TodoInfo--completed': completed })}
     >
       <h2 className="TodoInfo__title">{title}</h2>
       {userData && <UserInfo user={userData} />}
