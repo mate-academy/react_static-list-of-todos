ecebec4a8f304c9f645130d64404abd076f0a583
Nick Busarhin <busarginkolia@gmail.com>
2023-02-12 21:38:45 +0200

    toDo List

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,28 @@
-// Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+import React from 'react';
+import { UserInfo } from '../UserInfo';
+import { Todo } from '../../types/Todo';
+
+type Props = {
+  task: Todo;
+};
+
+export const TodoInfo: React.FC<Props> = ({ task }) => {
+  const {
+    completed,
+    user,
+    title,
+  } = task;
+
+  return (
+    <>
+      <article
+        className={`TodoInfo ${completed === true
+          ? 'TodoInfo--completed'
+          : ''}`}
+      >
+        <h2 className="TodoInfo__title">{title}</h2>
+        {user ? <UserInfo user={user} /> : ''}
+      </article>
+    </>
+  );
+};
