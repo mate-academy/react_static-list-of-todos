926d4b344f2eb315626faefff1cc3d00fb4c349a
Vitalii Skomskiy <skomskiy@ukr.net>
2023-02-16 22:18:46 +0200

    React commit

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,24 @@
-// Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
+import { Todo } from '../../types/Todo';
+// import { User } from '../../types/User';
+import { UserInfo } from '../UserInfo';
+
+type Props = {
+  todo: Todo,
+};
+
+export const TodoInfo: React.FC<Props> = ({
+  todo: {
+    user,
+    title,
+    completed,
+  },
+}) => (
+  <article className={completed
+    ? 'TodoInfo--completed TodoInfo'
+    : 'TodoInfo'}
+  >
+    <h2 className="TodoInfo__title">{title}</h2>
+
+    <UserInfo user={user} />
+  </article>
 );
