5b0c99ced4ae6f3092d4cb784190df3aa2812c8f
Bohdan Rudenko <RudBeee@gmail.com>
2023-02-16 13:11:03 +0200

    first try

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,17 @@
 // Add the required types and props
-export const TodoList = () => (
-  <>TodoList markup</>
+// import todos from '../../api/todos';
+// import classNames from 'classnames';
+import { Todo } from '../../types/Todo';
+import { TodoInfo } from '../TodoInfo';
+
+type TodoType = {
+  todos: Todo[]
+};
+
+export const TodoList: React.FC<TodoType> = ({ todos = [] }) => (
+  <>
+    {todos.map(todo => (
+      <TodoInfo key={todo.id} todo={todo} />
+    ))}
+  </>
 );
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,4 +1,8 @@
-// Add the required types and props
-export const UserInfo = () => (
-  <>UserInfo markup</>
+export const UserInfo = ({ ...user }) => (
+  <a
+    className="UserInfo"
+    href={`mailto:${user.user?.email}`}
+  >
+    {user.user?.name}
+  </a>
 );
