28eb68c0320da01152afd5142cf8b030ee2f0021
Kate <ekaterina2002aleks@gmail.com>
2023-02-19 16:21:35 +0200

    Solution

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -10,7 +10,10 @@ export const UserInfo:React.FC<Props> = ({ user }) => {
   } = user;
 
   return (
-    <a className="UserInfo" href={`mailto:${email}`}>
+    <a
+      className="UserInfo"
+      href={`mailto:${email}`}
+    >
       {name}
     </a>
   );
