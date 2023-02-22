ccbbd9809ca272156ecf3ebfda40e09449ae51a5
Sofiia Triasko <sofia13tryasko@gmail.com>
2023-02-22 22:10:03 +0200

    commit 1

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,19 @@
 // Add the required types and props
-export const TodoList = () => (
-  <>TodoList markup</>
+import { TodoInfo } from '../TodoInfo';
+import { Todo } from '../../types/Todo';
+
+type Todos = {
+  todos: Todo[];
+};
+export const TodoList = ({ todos } : Todos) => (
+  <ul>
+    {todos.map(todo => (
+      <article className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}>
+        <li key={todo.id}>
+          <TodoInfo todo={todo} />
+        </li>
+      </article>
+    ))}
+  </ul>
+
 );
