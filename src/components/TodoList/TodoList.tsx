00d997a2b6e24c9e8ed038d52aac0e85d369a911
Sergey Kovyazin <mr.roland1989@gmail.com>
2023-02-28 03:18:09 +0200

    style fix

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -5,14 +5,20 @@ type Props = {
   todos?: Todo[]
 };
 
-export const TodoList: React.FC<Props> = ({ todos }) => (
-  <section className="TodoList">
-    <ul>
-      {todos && todos.map((todo) => (
-        <li key={todo.id}>
-          <TodoInfo todo={todo} />
-        </li>
-      ))}
-    </ul>
-  </section>
-);
+export const TodoList: React.FC<Props> = ({ todos }) => {
+  const myStyle = {
+    listStyleType: 'none',
+  };
+
+  return (
+    <section className="TodoList">
+      <ul style={myStyle}>
+        {todos && todos.map((todo) => (
+          <li key={todo.id}>
+            <TodoInfo todo={todo} />
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
