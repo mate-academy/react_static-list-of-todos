5cb74d492ec390fa935ee977a6442a0cc6f44d77
Oleksii Pishchuhin <aliliolek@gmail.com>
2023-03-01 22:46:13 +0100

    sol 1.0

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,10 @@
+import { TodoInfo } from '../TodoInfo';
+
 // Add the required types and props
-export const TodoList = () => (
-  <>TodoList markup</>
+export const TodoList = ({ todos = [] }) => (
+  <section className="TodoList">
+    {todos.map(todo => (
+      <TodoInfo todo={todo} />
+    ))}
+  </section>
 );
