64ec3fec45a49dbfc1694a12922d7780e970bd27
Roshan Jha <kartohajha@gmail.com>
2023-03-04 13:59:28 +0200

    done

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import { User } from './types/User';
 import { Todo } from './types/Todo';
 import { TodoList } from './components/TodoList';
 
-
 function getUser(userId: number): User | null {
   const foundUser = usersFromServer.find(user => user.id === userId);
 
@@ -24,6 +23,6 @@ export const todos: Todo[] = todosFromServer.map(todo => ({
 export const App: React.FC = () => (
   <div className="App">
     <h1 className="App__title">Static list of todos</h1>
-    <TodoList todos={ todos } />
+    <TodoList todos={todos} />
   </div>
 );
