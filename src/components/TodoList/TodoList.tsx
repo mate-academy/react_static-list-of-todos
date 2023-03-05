6c433b324d673dacb980212335521e65006abcd7
Roman <romanmalysh@romans-macbook-air.home>
2023-03-05 15:05:00 +0100

    someChanges

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,16 +1,17 @@
-import React from 'react';
-import { Todo } from '../../types/Todo';
-import { TodoInfo } from '../TodoInfo';
+import React from "react";
+import { Todo } from "../../types/Todo";
+import { TodoInfo } from "../TodoInfo";
 
 type Props = {
   todos: Todo[];
-}
+};
+
 export const TodoList: React.FC<Props> = ({ todos }) => (
   <section className="TodoList">
-    {todos.map(todo => {
-      return (
-        <TodoInfo key={todo.id} todo={todo} />
-      );
-    })}
+    <ul>
+      {todos.map((todo) => {
+        return <li><TodoInfo key={todo.id} todo={todo} /></li>;
+      })}
+    </ul>
   </section>
 );
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 import { User } from '../../types/User';
 
 interface Props {
-  user: User | null;
+  user: User;
 }
-export const UserInfo : React.FC<Props>= ({user}) => (
-  user && (
-    <a className="UserInfo" href={`mailto:${user.email}`}>
+export const UserInfo : React.FC<Props> = ({ user }) => (
+  <a className="UserInfo" href={`mailto:${user.email}`}>
       {user.name}
     </a>
-  )
 );
