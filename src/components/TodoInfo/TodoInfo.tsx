49ede7cad77a19e70bbef1f8357225031a6b3d08
Ihor Zakharov <ihor.o.zakharov@gmail.com>
2023-03-13 21:39:20 +0200

    Refinements

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,23 +1,7 @@
 // Add the required types and props
-import todosFromServer from '../../api/todos';
-import usersFromServer from '../../api/users';
-
-import { User } from '../../types/User';
 import { Todo } from '../../types/Todo';
 
 import { UserInfo } from '../UserInfo';
-
-function getUser(userId: number): User | null {
-  const foundUser = usersFromServer.find(user => user.id === userId);
-
-  // if there is no user with a given userId
-  return foundUser || null;
-}
-
-export const todos: Todo[] = todosFromServer.map(todo => ({
-  ...todo,
-  user: getUser(todo.userId),
-}));
 
 type Props = {
   todo: Todo;
