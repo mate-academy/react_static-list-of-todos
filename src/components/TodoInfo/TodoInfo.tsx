268b4eaa5ba65dbf635380012f2a352131ea0913
mstrilec <MaxStrilec2005@gmail.com>
2023-03-13 19:01:57 +0200

    fix code

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -16,14 +16,14 @@ export const TodoInfo: React.FC<Props> = ({ todo }) => {
 
   return (
     <article
-      className={
-        classNames(
-          'TodoInfo',
-          { 'TodoInfo--completed': completed },
-        )
-      }
+      className={classNames(
+        'TodoInfo',
+        { 'TodoInfo--completed': completed },
+      )}
     >
-      <h2 className="TodoInfo__title">{title}</h2>
+      <h2 className="TodoInfo__title">
+        {title}
+      </h2>
 
       {user && (
         <UserInfo user={user} />
