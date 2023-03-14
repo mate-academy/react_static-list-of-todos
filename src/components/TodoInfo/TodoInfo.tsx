18d8889e14845b7a9cef09cc37cd6002ec66bd27
Sergey Lipikhin <football3@ukr.net>
2023-03-14 21:27:00 +0200

    Fix className for TodoInfo

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 import { Todo } from '../../types/Todo';
 import { UserInfo } from '../UserInfo/UserInfo';
 
@@ -14,7 +15,11 @@ export const TodoInfo: React.FC<Props> = ({ todo }) => {
   } = todo;
 
   return (
-    <article className={`TodoInfo ${completed ? 'TodoInfo--completed' : ''}`}>
+    <article className={classNames(
+      'TodoInfo',
+      { 'TodoInfo--completed': completed },
+    )}
+    >
       <h2 className="TodoInfo__title">
         {title}
       </h2>
