0b7d4c6214ec58d98cd6442c271266c5a53a5933
Alex Nastychenko <nastychenko1993@gmail.com>
2023-03-15 17:47:25 +0200

    add task solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,22 @@
-// Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
+import { User } from '../../types/User';
+import { UserInfo } from '../UserInfo';
+
+type Props = {
+  title: string;
+  user: User | null;
+};
+
+export const TodoInfo: React.FC<Props> = ({ title, user }) => (
+  <>
+    <h2 className="TodoInfo__title">{title}</h2>
+
+    {user ? (
+      <UserInfo
+        name={user.name}
+        email={user.email}
+      />
+    ) : (
+      null
+    )}
+  </>
 );
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,28 @@
-// Add the required types and props
-export const TodoList = () => (
-  <>TodoList markup</>
+import classNames from 'classnames';
+import { TodoInfo } from '../TodoInfo';
+import { Todo } from '../../types/Todo';
+
+type Props = {
+  todos: Todo[];
+};
+
+export const TodoList: React.FC<Props> = ({ todos }) => (
+  <section className="TodoList">
+    {todos.map(todo => (
+      <article
+        className={classNames(
+          'TodoInfo',
+          {
+            'TodoInfo--completed': todo.completed,
+          },
+        )}
+        key={todo.id}
+      >
+        <TodoInfo
+          title={todo.title}
+          user={todo.user}
+        />
+      </article>
+    ))}
+  </section>
 );
