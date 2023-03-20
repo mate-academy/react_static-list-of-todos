13ba231d3182701fb022589f3836105b7d975bc0
Kateryna Babina <katerynababina01@gmail.com>
2023-03-20 13:16:59 +0200

    solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -20,7 +20,10 @@ export const TodoInfo: React.FC<TodoInfoProps> = ({ todo }) => {
       { 'TodoInfo--completed': completed },
     )}
     >
-      <h2 className="TodoInfo__title">{title}</h2>
+
+      <h2 className="TodoInfo__title">
+        {title}
+      </h2>
 
       {user && (
         <UserInfo user={user} />
