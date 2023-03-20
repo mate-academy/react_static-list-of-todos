c46d89124a978a38834e65b9475add5f4f4d39b0
Sergey Lipikhin <football3@ukr.net>
2023-03-20 16:01:11 +0200

    Extract component styles to separate files

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import classNames from 'classnames';
+import './TodoInfo.scss';
 import { Todo } from '../../types/Todo';
 import { UserInfo } from '../UserInfo/UserInfo';
 
