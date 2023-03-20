c9a3125d5cc9bc882d33087ece6ef5160cb83f57
NikitaStambul <stambul.n@gmail.com>
2023-03-20 17:21:53 +0200

    codestyle fixes

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,25 +1,31 @@
 import classNames from 'classnames';
 import { UserInfo } from '../UserInfo';
 import { Todo } from '../../types/Todo';
+import './TodoInfo.scss';
 
 type Props = {
   todo: Todo,
 };
 
 export const TodoInfo: React.FC<Props> = ({ todo }) => {
-  const { title, completed, user } = todo;
+  const {
+    title,
+    completed,
+    user,
+  } = todo;
 
   return (
     <article className={classNames('TodoInfo', {
       'TodoInfo--completed': completed,
     })}
     >
-      <h2 className="TodoInfo__title">{title}</h2>
+      <h2 className="TodoInfo__title">
+        {title}
+      </h2>
 
       {user && (
         <UserInfo user={user} />
       )}
-
     </article>
   );
 };
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,5 +1,6 @@
 import { Todo } from '../../types/Todo';
 import { TodoInfo } from '../TodoInfo';
+import './TodoList.scss';
 
 type Props = {
   todos: Todo[],
