4066bd049e5fda3c5473cfd990ddd79828b4ade3
Olha Kissa <olhakissa121r@gmail.com>
2023-03-22 17:53:04 +0200

    Solution

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -3,7 +3,7 @@ import { Todo } from '../../types/Todo';
 type Props = Pick<Todo, 'user'>;
 
 export const UserInfo: React.FC<Props> = ({ user }) => (
-  <a className="UserInfo" href={'mailto:' + user?.email}>
+  <a className="UserInfo" href={`mailto:${user?.email}`}>
     {user?.name}
   </a>
 );
