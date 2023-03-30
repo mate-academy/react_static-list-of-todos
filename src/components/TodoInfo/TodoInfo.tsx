c44ec9b412394ac3349bb55b1293e954c78dbeb4
Ukrainian_panda <94929618+Ukrainiane-panda@users.noreply.github.com>
2023-03-30 19:39:06 +0300

    Solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,19 @@
+import React from 'react';
+import { UserInfo } from '../UserInfo';
+import { Todo } from '../../types/Todo';
+
 // Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
+interface Props {
+  todo: Todo;
+}
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => (
+  <article className={(todo.completed
+    ? 'TodoInfo TodoInfo--completed'
+    : 'TodoInfo')}
+  >
+    <h2 className="TodoInfo__title">{todo.title}</h2>
+
+    {todo.user && <UserInfo user={todo.user} />}
+  </article>
 );
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,4 +1,15 @@
+import React from 'react';
+import { User } from '../../types/User';
+
+interface Props {
+  user: User | null;
+}
 // Add the required types and props
-export const UserInfo = () => (
-  <>UserInfo markup</>
+export const UserInfo: React.FC<Props> = ({ user }) => (
+  <a
+    className="UserInfo"
+    href={`mailto:${user?.email}`}
+  >
+    {user?.name}
+  </a>
 );
