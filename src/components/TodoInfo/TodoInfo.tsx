627f5f7736975e08a3360d2f2548ec9c0f9afe8d
Ukrainian_panda <94929618+Ukrainiane-panda@users.noreply.github.com>
2023-03-31 22:16:36 +0300

    fixed

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 import { UserInfo } from '../UserInfo';
 import { Todo } from '../../types/Todo';
 
@@ -8,9 +9,10 @@ interface Props {
 }
 
 export const TodoInfo: React.FC<Props> = ({ todo }) => (
-  <article className={(todo.completed
-    ? 'TodoInfo TodoInfo--completed'
-    : 'TodoInfo')}
+  <article
+    className={classNames(
+      'TodoInfo', { 'TodoInfo--completed': todo.completed },
+    )}
   >
     <h2 className="TodoInfo__title">{todo.title}</h2>
 
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { User } from '../../types/User';
 
 interface Props {
-  user: User | null;
+  user: User;
 }
 // Add the required types and props
-export const UserInfo: React.FC<Props> = ({ user }) => (
+export const UserInfo: React.FC<Props> = ({ user: { name, email } }) => (
   <a
     className="UserInfo"
-    href={`mailto:${user?.email}`}
+    href={`mailto:${email}`}
   >
-    {user?.name}
+    {name}
   </a>
 );
