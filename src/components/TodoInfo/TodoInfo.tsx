049dfb4cf9c3da61584188031f19ef58f40ae2b5
Kocherha Roman <f.your.d.2017@gmail.com>
2023-04-04 20:27:19 +0300

    done

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,17 @@
+import { Todo } from '../../types/Todo';
+
+type Props = {
+  todo: Todo;
+};
 // Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
+export const TodoInfo: React.FC<Props> = ({ todo }) => (
+  <>
+    <article className="TodoInfo TodoInfo--completed">
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+
+      <a className="UserInfo" href={`mailto:${todo.user?.email}`}>
+        {todo.user?.name}
+      </a>
+    </article>
+  </>
 );
