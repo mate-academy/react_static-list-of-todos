255bdae3ab21986da9985d185cc997836d22337d
Kocherha Roman <f.your.d.2017@gmail.com>
2023-04-05 16:37:41 +0300

    fixed

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,5 @@
 import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo';
 
 type Props = {
   todo: Todo;
@@ -6,12 +7,10 @@ type Props = {
 // Add the required types and props
 export const TodoInfo: React.FC<Props> = ({ todo }) => (
   <>
-    <article className="TodoInfo TodoInfo--completed">
+    <article className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}>
       <h2 className="TodoInfo__title">{todo.title}</h2>
 
-      <a className="UserInfo" href={`mailto:${todo.user?.email}`}>
-        {todo.user?.name}
-      </a>
+      <UserInfo user={todo.user} />
     </article>
   </>
 );
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,4 +1,16 @@
+import { User } from '../../types/User';
+
+type Props = {
+  user: User | null;
+};
 // Add the required types and props
-export const UserInfo = () => (
-  <>UserInfo markup</>
-);
+export const UserInfo: React.FC<Props> = ({ user }) => {
+  return (
+    <>
+      {
+        (user?.name && user?.email)
+          && <a className="UserInfo" href={`mailto:${user.email}`}>{user.name}</a>
+      }
+    </>
+  );
+};
