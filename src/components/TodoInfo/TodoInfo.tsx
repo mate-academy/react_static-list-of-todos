a0c9f0ccfac408d457d517de55521d6266ca0345
yana-kotsulym <yanak7170@gmail.com>
2023-04-06 17:06:57 +0200

    Fixes after code review

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -8,19 +8,30 @@ type Props = {
   todo: Todo;
 };
 
-export const TodoInfo: React.FC<Props> = ({ todo }) => (
-  <article className={classNames(
-    'TodoInfo',
-    {
-      'TodoInfo--completed': todo.completed,
-    },
-  )}
-  >
-    <h2 className="TodoInfo__title">{todo.title}</h2>
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const {
+    id,
+    title,
+    completed,
+    user,
+  } = todo;
 
-    {todo.user && (
-      <UserInfo user={todo.user} />
-    )}
+  return (
+    <article
+      key={id}
+      className={classNames(
+        'TodoInfo',
+        {
+          'TodoInfo--completed': completed,
+        },
+      )}
+    >
+      <h2 className="TodoInfo__title">{title}</h2>
 
-  </article>
-);
+      {user && (
+        <UserInfo user={user} />
+      )}
+
+    </article>
+  );
+};
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -5,8 +5,10 @@ type Props = {
   user: User | null;
 };
 
-export const UserInfo: React.FC<Props> = ({ user }) => (
-  <a className="UserInfo" href={`mailto:${user?.email}`}>
-    {user?.name}
-  </a>
-);
+export const UserInfo: React.FC<Props> = ({ user }) => {
+  return (
+    <a className="UserInfo" href={`mailto:${user?.email}`}>
+      {user?.name}
+    </a>
+  );
+};
