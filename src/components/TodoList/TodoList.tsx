1ba0b0c93b8cc8ac6562082ab6048c82d0a51a08
Nazarii AlieKsieiev <nazarmetall@gmail.com>
2023-04-07 19:52:26 +0300

    Solution react_static-list-of-todos

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,21 @@
+import { TodoInfo } from '../TodoInfo';
+
+import { Todo } from '../../types/Todo';
+
+type Props = {
+  todos: Todo[]
+};
+
 // Add the required types and props
-export const TodoList = () => (
-  <>TodoList markup</>
+export const TodoList: React.FC<Props> = ({ todos = [] }) => (
+  <>
+    <section className="TodoList">
+      {todos.map(todo => (
+        <TodoInfo
+          todo={todo}
+          key={todo.id}
+        />
+      ))}
+    </section>
+  </>
 );
