376b51ef32df929783faa901c043b1889a73e682
Andrii <remote.makandriival@gmail.com>
2023-04-11 16:22:24 +0300

    solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,25 @@
-// Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+import { Todo } from '../../types/Todo';
+
+interface Props {
+  todoProp: Todo;
+}
+
+export const TodoInfo: React.FC<Props> = ({ todoProp }) => {
+  const {
+    title,
+    user,
+  } = todoProp;
+
+  const userInfo = user && (
+    <a className="UserInfo" href={`mailto:${user?.email}`}>
+      {user?.name}
+    </a>
+  );
+
+  return (
+    <>
+      <h2 className="TodoInfo__title">{title}</h2>
+      {userInfo}
+    </>
+  );
+};
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,24 @@
-// Add the required types and props
-export const TodoList = () => (
-  <>TodoList markup</>
+import classNames from 'classnames';
+import { TodoInfo } from '../TodoInfo/TodoInfo';
+
+import { Todo } from '../../types/Todo';
+
+interface Props {
+  todoArr: Todo[];
+}
+
+export const TodoList: React.FC<Props> = ({ todoArr = [] }) => (
+  <section className="TodoList">
+    {todoArr.map(todo => (
+      <article
+        className={classNames('TodoInfo',
+          {
+            'TodoInfo--completed': todo.completed,
+          })}
+      >
+        <TodoInfo todoProp={todo} key={todo.id} />
+      </article>
+    ))}
+
+  </section>
 );
