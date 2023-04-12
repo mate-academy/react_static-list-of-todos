55efa3017526b026f91ffecc54d59d49ecb6a976
Rostyslav Hirin <hirin.rostyslav@gmail.com>
2023-04-12 22:17:37 +0300

    Static list

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,4 +1,14 @@
-// Add the required types and props
-export const UserInfo = () => (
-  <>UserInfo markup</>
+import { FC } from 'react';
+import { User } from '../../types/User';
+
+type Props = {
+  user: User | null;
+};
+
+export const UserInfo: FC<Props> = ({
+  user,
+}) => (
+  <a className="UserInfo" href={`mailto:${user?.email}` || 'Unknown email'}>
+    {user?.name || 'Unknown user'}
+  </a>
 );
