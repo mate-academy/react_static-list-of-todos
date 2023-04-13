cc645a4083a935c2a8bea102f0da445583ddd3a7
haduigon <haduigon@gmail.com>
2023-04-13 10:27:21 +0300

    first commit

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,21 @@
 // Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
+
+import React from 'react';
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo/UserInfo';
+
+type Props = {
+  todo: Todo
+};
+
+export const TodoInfo:React.FC<Props> = ({ todo }) => (
+  <article className="TodoInfo TodoInfo--completed">
+    <h2 className="TodoInfo__title">{todo.title}</h2>
+
+    <UserInfo
+      name={todo.user !== null ? todo.user.name : ''}
+      email={todo.user !== null ? todo.user.email : ''}
+    />
+  </article>
+
 );
