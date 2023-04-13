2a521702a2b5f445a983e68805e0f1b09cd6246d
Alisa Vinnyk <alisavinnyk@gmail.com>
2023-04-13 10:27:28 +0100

    changed type name to Props

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -3,11 +3,11 @@ import React from 'react';
 import { Todo } from '../../types/Todo';
 import { TodoInfo } from '../TodoInfo';
 
-type TodoProps = {
+type Props = {
   todos: Todo[];
 };
 
-export const TodoList: React.FC<TodoProps> = ({ todos }) => (
+export const TodoList: React.FC<Props> = ({ todos }) => (
   <section className="TodoList">
     {todos.map((todo) => (
       <TodoInfo todo={todo} key={todo.id} />
