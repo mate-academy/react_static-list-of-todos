675f296f25c19a2893028104cfdb1677cfb2d651
christian m wolf <108151633+devWizHub@users.noreply.github.com>
2023-04-13 15:09:01 +0200

    add task solution

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,4 +1,22 @@
 // Add the required types and props
-export const UserInfo = () => (
-  <>UserInfo markup</>
-);
+import React from 'react';
+// import { URLSearchParams } from 'url';
+import { User } from '../../types/User';
+
+interface UserInfoProps {
+  user: User;
+}
+
+export const UserInfo: React.FC<UserInfoProps> = ({ user }) => {
+  return (
+    <a className="UserInfo" href={`mailto:${user.email}`}>
+      <p>
+        mailto:
+        {user.email}
+      </p>
+      <p>
+        {user.name}
+      </p>
+    </a>
+  );
+};
