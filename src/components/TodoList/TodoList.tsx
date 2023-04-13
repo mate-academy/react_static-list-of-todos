23aa7c09deae636cd806701bf76f6e641c3f1b1a
Tanya Sydoruk <tanyasydoruk113022@gmail.com>
2023-04-13 22:33:40 +0300

    solution

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,14 @@
-// Add the required types and props
-export const TodoList = () => (
-  <>TodoList markup</>
+import { TodoInfo } from '../TodoInfo';
+import { Todo } from '../../types/Todo';
+
+interface TodoListProps {
+  todos: Todo[]
+}
+
+export const TodoList: React.FC<TodoListProps> = ({ todos }) => (
+  <section className="TodoList">
+    {todos.map((todo) => (
+      <TodoInfo todo={todo} key={todo.id} />
+    ))}
+  </section>
 );
