1a58b621ed99ddd8b3c94336e8904893c43bddca
Anna Kuzmenko <mogilevs.anna@gmail.com>
2023-04-13 10:57:58 +0300

    fixed murcup

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -16,16 +16,14 @@ export const TodoInfo: FC<Props> = ({ todo }) => {
 
   return (
     <article
-      className={classNames(
-        'TodoInfo',
-        {
-          'TodoInfo--completed': completed,
-        },
-      )}
+      className={classNames('TodoInfo', {
+        'TodoInfo--completed': completed,
+      })}
     >
       <h2 className="TodoInfo__title">
         {title}
       </h2>
+
       {user && (
         <UserInfo user={user} />
       )}
