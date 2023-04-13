44f5487df1910082376d7f1ffc72b0393eff7f35
Alisa Vinnyk <alisavinnyk@gmail.com>
2023-04-13 10:26:48 +0100

    changed type name, destructured todo

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -4,18 +4,29 @@ import classNames from 'classnames';
 import { Todo } from '../../types/Todo';
 import { UserInfo } from '../UserInfo';
 
-type TodoProps = {
+type Props = {
   todo: Todo;
 };
 
-export const TodoInfo: React.FC<TodoProps> = ({ todo }) => (
+export const TodoInfo: React.FC<Props> = ({
+  todo: {
+    title,
+    completed,
+    user,
+  },
+}) => (
+
   <article
     className={classNames('TodoInfo', {
-      'TodoInfo--completed': todo.completed,
+      'TodoInfo--completed': completed,
     })}
   >
-    <h2 className="TodoInfo__title">{todo.title}</h2>
-    {todo.user
-      && <UserInfo user={todo.user} />}
+    <h2 className="TodoInfo__title">
+      {title}
+    </h2>
+
+    {user && (
+      <UserInfo user={user} />
+    )}
   </article>
 );
