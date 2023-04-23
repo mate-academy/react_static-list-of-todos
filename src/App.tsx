5476769b11ea6e5db4212f795e15728a7f3e4bd7
Vyacheslav Stolyarchuk <haunt.beta@gmail.com>
2023-04-24 00:59:52 +0300

    fix code style, add destructure

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,10 @@ export const todos: Todo[] = todosFromServer.map(todo => ({
 export const App: React.FC = () => {
   return (
     <div className="App">
-      <h1 className="App__title">Static list of todos</h1>
+      <h1 className="App__title">
+        Static list of todos
+      </h1>
+
       <TodoList todos={todos} />
     </div>
   );
