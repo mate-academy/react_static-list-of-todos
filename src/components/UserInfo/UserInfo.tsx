8c17c2562362f421f175d34dc62d8aee4e46abaf
Dmitrij Kosow <dymitrkosow@gmail.com>
2023-04-25 15:19:03 +0200

    solution

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -8,7 +8,7 @@ export const UserInfo: React.FC<TypeUser> = ({ user }) => {
   const { name, email } = user;
 
   return (
-    <a className="UserInfo" href={`mailto: ${email}`}>
+    <a className="UserInfo" href={`mailto:${email}`}>
       { name }
     </a>
   );
