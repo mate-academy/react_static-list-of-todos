9a1f555d9a5c4063b53f12de18a90605ef09197c
Ilia <koleshnik160799@gmail.com>
2023-04-29 14:32:31 +0300

    Fix UserInfo

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { UserInfoType } from '../../types/User';
 
-export const UserInfo:React.FC<UserInfoType> = ({ user }) => (
-  <a className="UserInfo" href={`mailto:${user.email}`}>
-    {user.name}
+export const UserInfo:React.FC<UserInfoType> = ({ user: { name, email } }) => (
+  <a className="UserInfo" href={`mailto:${email}`}>
+    {name}
   </a>
 );
