ad51fbcd6cfea305e9b43b12db7b9943a0981391
Barbara Jeziorek <barbarajeziorek@gmail.com>
2023-05-03 13:44:26 +0200

    working solution

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,4 +1,22 @@
+import { FC } from 'react';
+import { User } from '../../types/User';
+
 // Add the required types and props
-export const UserInfo = () => (
-  <>UserInfo markup</>
-);
+
+interface Props{
+  user:User;
+}
+
+export const UserInfo: FC<Props > = ({ user }) => {
+  const {
+    id,
+    name,
+    email,
+  } = user;
+
+  return (
+    <a key={`${id}`} className="UserInfo" href={email}>
+      {name}
+    </a>
+  );
+};
