fc0cd0ded2d8a3114c8524399facb8841d80e8d4
kq333 <afugowski@yandex.com>
2023-05-04 15:23:06 +0200

    solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,24 @@
-// Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo';
+
+interface ListProps {
+  todo: Todo;
+}
+
+export const TodoInfo: React.FC<ListProps> = ({ todo }) => {
+  const {
+    completed, title, user,
+  } = todo;
+
+  const className = `TodoInfo ${completed ? 'TodoInfo--completed' : ''}`;
+
+  return (
+
+    <article
+      className={className}
+    >
+      <h2 className="TodoInfo__title">{title}</h2>
+      {user && <UserInfo user={user} />}
+    </article>
+  );
+};
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,17 @@
-// Add the required types and props
-export const TodoList = () => (
-  <>TodoList markup</>
-);
+import React from 'react';
+import { Todo } from '../../types/Todo';
+import { TodoInfo } from '../TodoInfo';
+
+interface TodoListProps {
+  todoList: Todo[];
+}
+
+export const TodoList: React.FC<TodoListProps> = ({ todoList }) => {
+  return (
+    <section className="TodoList">
+      {todoList.map((todo) => (
+        <TodoInfo todo={todo} key={todo.id} />
+      ))}
+    </section>
+  );
+};
