42e0ec6a660c248478ffde1d3ec72bf53d23c15a
Jedrzej Kielpinski <jedrzej.kielpinski@gmail.com>
2023-05-06 00:18:29 +0200

    fix2

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -17,7 +17,6 @@ export const TodoInfo: React.FC<TodoInfoProps> = ({ todo }) => {
       className={`TodoInfo ${completed ? 'TodoInfo--completed' : ''}`}
     >
       <h2 className="TodoInfo__title">{title}</h2>
-
       {user && <UserInfo user={user} />}
     </article>
   );
