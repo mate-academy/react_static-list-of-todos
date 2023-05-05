ce753805bd2118fb52dfeff344fb05d2e6a3fecf
Diana Snihur <dianasnigur2@gmail.com>
2023-05-05 14:52:00 +0300

    Solution

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,4 +1,12 @@
-// Add the required types and props
-export const UserInfo = () => (
-  <>UserInfo markup</>
+import React from "react";
+import { User } from "../../types/User";
+
+type Props = {
+  users: User,
+};
+
+export const UserInfo: React.FC<Props> = ({ users }) => (
+  <a className="UserInfo" href={`mailto:${users.email}`}>
+    {users.name}
+  </a>
 );
