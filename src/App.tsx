e43dd2cfeec207faf6098a0935e79d5707356457
Petro Lozynskyi <petrolozynskyi01@gmail.com>
2023-05-06 07:27:25 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,6 @@ import { TodoList } from './components/TodoList/TodoList';
 function getUser(userId: number): User | null {
   const foundUser = usersFromServer.find(user => user.id === userId);
 
-  // if there is no user with a given userId
   return foundUser || null;
 }
 
