c98461472398b4cd8dc2a90125f683e97620244c
Nikitin Evgeny <ngenya81@gmail.com>
2023-05-07 13:20:40 +0300

    commit message

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -5,7 +5,7 @@ import { Todo } from '../../types/Todo';
 type Props = {
   todos: Todo [];
 };
-// Add the required types and props
+
 export const TodoList: React.FC<Props> = ({ todos }) => (
   <section className="TodoList">
     <ul>
