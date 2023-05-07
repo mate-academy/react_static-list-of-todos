7d98da2599ae7934759d9574589eac524a51345d
catgode <105614630+catgode@users.noreply.github.com>
2023-05-07 16:52:34 +0200

    start building TodoInfo.tsx component

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,27 @@
-// Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+import cn from 'classnames';
+import { Todo } from '../../types/Todo';
+
+interface TodoInfoProps {
+  todoInfo: Todo
+}
+
+export const TodoInfo: React.FC<TodoInfoProps> = ({
+  todoInfo: {
+    id, title, user, userId, completed,
+  },
+}) => {
+  const className = cn({
+    TodoInfo: true,
+    'TodoInfo--completed': completed,
+  });
+
+  return (
+    <article className={className}>
+      <h2 className="TodoInfo__title">{title}</h2>
+
+      <a className="UserInfo" href="mailto:Sincere@april.biz">
+        Leanne Graham
+      </a>
+    </article>
+  );
+};
