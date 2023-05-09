31cc8554fdbd6a472b31c0e0cac52c069479344d
marshalek37 <patrykmarp@gmai.com>
2023-05-09 18:42:41 +0200

    fix v2

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -2,9 +2,9 @@ import classNames from 'classnames';
 import { Todo } from '../../types/Todo';
 import { UserInfo } from '../UserInfo';
 
-type Props = {
+interface Props {
   todo: Todo;
-};
+}
 
 export const TodoInfo: React.FC<Props> = ({ todo }) => {
   const {
