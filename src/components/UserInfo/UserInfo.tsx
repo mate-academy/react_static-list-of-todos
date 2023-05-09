6846bf373da311cc943016b8f9bb009554edfea0
Pavlo Petrashevskyi <dragonthunderaddsdp@gmail.com>
2023-05-09 10:49:27 +0300

    Todolist was created

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,4 +1,14 @@
-// Add the required types and props
-export const UserInfo = () => (
-  <>UserInfo markup</>
+import React from 'react';
+import { User } from '../../types/User';
+
+type UserInfoProps = {
+  user: User | null
+};
+
+export const UserInfo: React.FC<UserInfoProps> = ({ user }) => (
+  <>
+    <a className="UserInfo" href={`mailto:${user?.email}`}>
+      {user?.name}
+    </a>
+  </>
 );
