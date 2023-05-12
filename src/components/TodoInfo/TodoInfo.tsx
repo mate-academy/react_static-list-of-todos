a729f55316cae2e396b6fb3e4f08e5e921908068
Nikoramo <kanada387@gmail.com>
2023-05-12 19:57:52 +0200

    solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,26 @@
-// Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+import React from 'react';
+import { User } from '../../types/User';
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo/UserInfo';
+import usersFromServer from '../../api/users';
+
+type Props = {
+  todo: Todo;
+};
+
+function getUser(userId: number): User | null {
+  const foundUser = usersFromServer.find(user => user.id === userId);
+
+  return foundUser || null;
+}
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const user = getUser(todo.userId);
+
+  return (
+    <article className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}>
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+      {user && <UserInfo user={user} />}
+    </article>
+  );
+};
