bccffdeddabdae0536a36ea898a6e357ce8c6b38
Olena Shevchenko <shevochkas@gmail.com>
2023-05-23 14:50:05 +0300

    components list of todos were added

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,24 @@
+import React from 'react';
+import classNames from 'classnames';
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo';
 // Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
+
+type Props = {
+  todoWithUser: Todo;
+};
+
+export const TodoInfo: React.FC<Props> = ({
+  todoWithUser: {
+    title,
+    completed,
+    user,
+  },
+}) => (
+  <article className={classNames('TodoInfo',
+    { 'TodoInfo--completed': completed })}
+  >
+    <h2 className="TodoInfo__title">{title}</h2>
+    {user && <UserInfo user={user} />}
+  </article>
 );
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,20 @@
+import React from 'react';
+import { TodoInfo } from '../TodoInfo';
+import { Todo } from '../../types/Todo';
+
+type Props = {
+  todosWithUser: Todo[];
+};
 // Add the required types and props
-export const TodoList = () => (
-  <>TodoList markup</>
+export const TodoList: React.FC<Props> = ({ todosWithUser }) => (
+  <section className="TodoList">
+    {
+      todosWithUser.map(todoWithUser => (
+        <TodoInfo
+          key={todoWithUser.id}
+          todoWithUser={todoWithUser}
+        />
+      ))
+    }
+  </section>
 );
