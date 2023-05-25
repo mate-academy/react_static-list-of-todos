f1b385425e7c1946f622ff6e434bab146d27d826
Victor Buldenko <gookidoo@gmail.com>
2023-05-25 18:00:02 +0300

    failed tests

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,17 @@
-// Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+import React from 'react';
+import { Todo } from '../../types/Todo';
+
+export const TodoInfo: React.FC<Todo> = ({ title, user }) => {
+  if (user) {
+    return (
+      <>
+        <h2 className="TodoInfo__title">{title}</h2>
+        <a className="UserInfo" href={user.email}>
+          {user.name}
+        </a>
+      </>
+    );
+  }
+
+  return <></>;
+};
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,29 @@
-// Add the required types and props
-export const TodoList = () => (
-  <>TodoList markup</>
-);
+import React from 'react';
+import { Todo } from '../../types/Todo';
+import { TodoInfo } from '../TodoInfo/TodoInfo';
+
+type Props = {
+  data: Todo[];
+};
+
+export const TodoList: React.FC<Props> = ({ data }) => {
+  return (
+    <section className="TodoList">
+      {data.map(todoItem => {
+        if (todoItem.completed) {
+          return (
+            <article className="TodoInfo TodoInfo--completed" key={todoItem.id}>
+              <TodoInfo {...todoItem} />
+            </article>
+          );
+        }
+
+        return (
+          <article className="TodoInfo" key={todoItem.id}>
+            <TodoInfo {...todoItem} />
+          </article>
+        );
+      })}
+    </section>
+  );
+};
