38bccf883a7711f725ff697caea350344543cbe2
Vladyslav Kucher <vladkucher75755@gmail.com>
2023-05-28 17:35:30 -0400

    done

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { User } from '../../types/User'
+import { User } from '../../types/User';
 
 type Props = {
   user: User;
-}
+};
 
 export const UserInfo: React.FC<Props> = ({ user }) => {
   const { name, email } = user;
