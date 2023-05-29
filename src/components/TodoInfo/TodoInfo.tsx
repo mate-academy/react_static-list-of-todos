f62ef43dd9ad53d882138760803a3f9e956b7389
Maksym Nemera <nickmitchell@ukr.net>
2023-05-29 14:48:08 +0300

    add task solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,24 @@
-// Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+import React from 'react';
+
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo';
+
+interface Prop {
+  info: Todo;
+}
+
+export const TodoInfo: React.FC<Prop> = ({ info }) => {
+  const { completed, title, user } = info;
+
+  return (
+    <article className={`TodoInfo ${completed ? 'TodoInfo--completed' : ''}`}>
+      <h2 className="TodoInfo__title">
+        {title}
+      </h2>
+
+      {user && (
+        <UserInfo user={user} />
+      )}
+    </article>
+  );
+};
