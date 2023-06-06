17a27a931be364e25c5e9631d09af95c7faf4a39
Oleksii Lysenko <nano77@ukr.net>
2023-06-06 15:29:54 +0300

    add solution to ToDoList

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,20 @@
-// Add the required types and props
-export const TodoList = () => (
-  <>TodoList markup</>
+import React from 'react';
+
+import './TodoList.scss';
+
+import { Todo } from '../../types/Todo';
+import { TodoInfo } from '../TodoInfo';
+
+type Props = {
+  todoList: Todo[]
+};
+
+export const TodoList: React.FC<Props> = ({ todoList = [] }) => (
+  <section className="TodoList">
+    {
+      todoList.map((item: Todo) => {
+        return <TodoInfo todoItem={item} key={item.id} />;
+      })
+    }
+  </section>
 );
