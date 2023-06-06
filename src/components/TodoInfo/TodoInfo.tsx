6fb5daf5ea163fb6eb26db4a86f8ac38f6afc971
Yulia Shvets <juls.shvets@gmail.com>
2023-06-06 18:56:16 +0300

    fixes after code review

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -7,8 +7,14 @@ type Props = {
 };
 
 export const TodoInfo: React.FC <Props> = ({ todo }) => (
-  <article className={`TodoInfo ${todo.completed && 'TodoInfo--completed'}`}>
-    <h2 className="TodoInfo__title">{todo.title}</h2>
+  <article className={`TodoInfo ${todo.completed
+    ? 'TodoInfo--completed'
+    : ''}`}
+  >
+    <h2 className="TodoInfo__title">
+      {todo.title}
+    </h2>
+
     {todo.user && <UserInfo user={todo.user} />}
   </article>
 );
