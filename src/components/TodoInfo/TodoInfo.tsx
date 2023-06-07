38ab1c39e02f661a47b1202b311a0ea8d69066c1
Stepan Haida <stepanhaida@gmail.com>
2023-06-07 13:27:27 +0300

    add conditional class adding using classnames package

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import { Todo } from '../../types/Todo';
 import { UserInfo } from '../UserInfo';
 
@@ -9,14 +10,20 @@ export const TodoInfo: React.FC<TodoInfoType> = ({ todoInfo }) => {
   const {
     title,
     user,
+    completed,
   } = todoInfo;
 
   if (!user) {
     throw new Error('Cant find user');
   }
 
+  const arcticleClassname = classNames(
+    'TodoInfo',
+    { 'TodoInfo--completed': completed },
+  );
+
   return (
-    <article className="TodoInfo TodoInfo--completed">
+    <article className={arcticleClassname}>
       <h2 className="TodoInfo__title">{title}</h2>
 
       <UserInfo user={user} />
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -9,22 +9,6 @@ export const TodoList: React.FC<TodosList> = ({ todosList }) => {
   return (
     <section className="TodoList">
       {todosList.map(todoItem => <TodoInfo todoInfo={todoItem} />)}
-
-      <article className="TodoInfo TodoInfo--completed">
-        <h2 className="TodoInfo__title">JS</h2>
-
-        <a className="UserInfo" href="mailto:Shanna@melissa.tv">
-          Ervin Howell
-        </a>
-      </article>
-
-      <article className="TodoInfo">
-        <h2 className="TodoInfo__title">React</h2>
-
-        <a className="UserInfo" href="mailto:Nathan@yesenia.net">
-          Clementine Bauch
-        </a>
-      </article>
     </section>
   );
 };
