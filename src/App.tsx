af7e50f43cee46ad896bd3cd2c82be1d4e9891fd
Stepan Haida <stepanhaida@gmail.com>
2023-06-07 13:33:44 +0300

    moved api processing to a separate file

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,8 @@
 import React from 'react';
 import './App.scss';
 
-import todosFromServer from './api/todos';
-import usersFromServer from './api/users';
-
-import { User } from './types/User';
-import { Todo } from './types/Todo';
 import { TodoList } from './components/TodoList';
-
-function getUser(userId: number): User | null {
-  const foundUser = usersFromServer.find(user => user.id === userId);
-
-  // if there is no user with a given userId
-  return foundUser || null;
-}
-
-export const todos: Todo[] = todosFromServer.map(todo => ({
-  ...todo,
-  user: getUser(todo.userId),
-}));
+import { todos } from './prepareApiData';
 
 export const App: React.FC = () => (
   <div className="App">
---
+++ src/prepareApiData.ts
@@ -0,0 +1,16 @@
+import todosFromServer from './api/todos';
+import usersFromServer from './api/users';
+
+import { User } from './types/User';
+import { Todo } from './types/Todo';
+
+function getUser(userId: number): User | null {
+  const foundUser = usersFromServer.find(user => user.id === userId);
+
+  return foundUser || null;
+}
+
+export const todos: Todo[] = todosFromServer.map(todo => ({
+  ...todo,
+  user: getUser(todo.userId),
+}));
