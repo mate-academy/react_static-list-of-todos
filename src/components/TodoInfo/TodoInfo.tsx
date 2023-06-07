2d27289a7a21b8189e0a0ee0382376293b155313
Mykola Maik <mykoluamaik@gmail.com>
2023-06-07 14:16:33 +0300

    Solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,24 @@
-// Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+import React from 'react';
+
+import { UserInfo } from '../UserInfo';
+import { Todo } from '../../types/Todo';
+
+type Props = {
+  todo: Todo;
+};
+
+export const TodoInfo:React.FC<Props> = ({ todo }) => {
+  const audit = todo.completed ? 'TodoInfo--completed' : '';
+
+  return (
+    <>
+      <article className={`TodoInfo ${audit}`}>
+        <h2 className="TodoInfo__title">
+          {todo.title}
+        </h2>
+
+        {todo.user && <UserInfo user={todo.user} />}
+      </article>
+    </>
+  );
+};
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,19 @@
-// Add the required types and props
-export const TodoList = () => (
-  <>TodoList markup</>
+import React from 'react';
+import { Todo } from '../../types/Todo';
+import { TodoInfo } from '../TodoInfo';
+
+type Prop = {
+  todos: Todo[],
+};
+
+export const TodoList: React.FC<Prop> = ({ todos }) => (
+  <>
+    <section className="TodoList">
+      {todos.map(todo => (
+        <React.Fragment key={todo.id}>
+          <TodoInfo todo={todo} />
+        </React.Fragment>
+      ))}
+    </section>
+  </>
 );
