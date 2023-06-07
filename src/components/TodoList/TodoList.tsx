988751b773edf067644cb96803adc392dae0d125
Ivan Khoroshylov <host513@gmail.com>
2023-06-07 17:44:06 +0300

    add solution

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,18 @@
+import { FC } from 'react';
+import { TodoInfo } from '../TodoInfo';
+import { Todo } from '../../types/Todo';
+
+interface Props{
+  todos: Todo[]
+}
+
 // Add the required types and props
-export const TodoList = () => (
-  <>TodoList markup</>
-);
+export const TodoList:FC<Props> = ({ todos }) => {
+  return ((
+    <section className="TodoList">
+      {todos.map(todo => (
+        <TodoInfo key={todo.id} todo={todo} />
+      ))}
+    </section>
+  ));
+};
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,4 +1,13 @@
+import { FC } from 'react';
+import { User } from '../../types/User';
+
 // Add the required types and props
-export const UserInfo = () => (
-  <>UserInfo markup</>
+interface Props{
+  user: User,
+}
+
+export const UserInfo: FC<Props> = ({ user }) => (
+  <a className="UserInfo" href={`mailto:${user.email}`}>
+    {user.name}
+  </a>
 );
