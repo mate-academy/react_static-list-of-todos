da24853ee0cad719a57194b01b7bcbba4a033b97
Sonya Liubchenko <conilub@gmail.com>
2023-06-07 17:54:28 +0300

    soluton

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,22 @@
-// Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+import React from 'react';
+import classNames from 'classnames';
+import { UserInfo } from '../UserInfo/UserInfo';
+import { Todo } from '../../types/Todo';
+
+interface Props {
+  todo: Todo;
+}
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const todoClass = classNames(
+    'TodoInfo',
+    { 'TodoInfo--completed': todo.completed },
+  );
+
+  return (
+    <article className={todoClass}>
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+      { todo.user && <UserInfo user={todo.user} />}
+    </article>
+  );
+};
