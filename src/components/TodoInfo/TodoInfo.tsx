c006e2fef86f6ed131fe7c01cbd8e21c17c56125
Yehor Myronov <2726048@gmail.com>
2023-06-08 11:52:30 +0300

    fixed

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -7,17 +7,24 @@ type Props = {
   todo: Todo;
 };
 
-export const TodoInfo: React.FC<Props> = ({ todo }) => (
-  <article
-    className={classNames(
-      'TodoInfo',
-      {
-        'TodoInfo--completed': todo.completed === true,
-      },
-    )}
-    key={todo.id}
-  >
-    <h2 className="TodoInfo__title">{todo.title}</h2>
-    <UserInfo user={todo.user} />
-  </article>
-);
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const {
+    completed,
+    title,
+    user,
+  } = todo;
+
+  return (
+    <article
+      className={classNames(
+        'TodoInfo',
+        {
+          'TodoInfo--completed': completed === true,
+        },
+      )}
+    >
+      <h2 className="TodoInfo__title">{title}</h2>
+      {user && <UserInfo user={user} />}
+    </article>
+  );
+};
