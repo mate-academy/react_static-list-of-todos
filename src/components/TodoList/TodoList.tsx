392d0bb36f8752a4d8e373cc4abcc34d1924c0e6
Sergii Mishchenko <serge.mishchenko@gmail.com>
2023-06-08 17:32:50 +0300

    Todo list was created

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,17 @@
-// Add the required types and props
-export const TodoList = () => (
-  <>TodoList markup</>
+import { Fragment } from 'react';
+import { TodoInfo } from '../TodoInfo';
+import { Todo } from '../../types/Todo';
+
+type Props = {
+  todos: Todo[];
+};
+
+export const TodoList: React.FC<Props> = ({ todos }) => (
+  <section className="TodoList">
+    {todos.map(todo => (
+      <Fragment key={todo.id}>
+        <TodoInfo todo={todo} />
+      </Fragment>
+    ))}
+  </section>
 );
