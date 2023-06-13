5574352f3557e494af13262c6b5572d488e38aa4
Vitalii Parkhomenko <poroh1991ukr@gmail.com>
2023-06-13 18:51:02 +0300

    react_static-list-of-todos

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,32 @@
-// Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+import classNames from 'classnames';
+import { UserInfo } from '../UserInfo';
+
+import { Todo } from '../../types/Todo';
+
+import usersFromServer from '../../api/users';
+
+type Props = {
+  todo: Omit<Todo, 'user'>;
+};
+
+export const TodoInfo: React.FC<Props> = ({
+  todo: { title, completed, userId },
+}) => {
+  const user = userId
+    ? usersFromServer.find(usr => usr.id === userId)
+    : null;
+
+  return (
+    <article
+      className={classNames(
+        'TodoInfo',
+        { 'TodoInfo--completed': completed },
+      )}
+    >
+      <h2 className="TodoInfo__title">{title}</h2>
+      {userId && user && (
+        <UserInfo user={user} />
+      )}
+    </article>
+  );
+};
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,14 @@
-// Add the required types and props
-export const TodoList = () => (
-  <>TodoList markup</>
+import { TodoInfo } from '../TodoInfo';
+import { Todo } from '../../types/Todo';
+
+type Props = {
+  todos: Omit<Todo, 'user'>[];
+};
+
+export const TodoList: React.FC<Props> = ({ todos }) => (
+  <section className="TodoList">
+    {todos.map(todoItem => (
+      <TodoInfo todo={todoItem} />
+    ))}
+  </section>
 );
