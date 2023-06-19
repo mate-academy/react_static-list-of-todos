a3c07a2e30bb639ca9989c3405026a50041d9088
Antonio S <soi4.toxa@ukr.net>
2023-06-19 18:39:06 +0300

    React__static-list-of-todos__have-some-problrms-with-tests

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,20 @@
-// Add the required types and props
-export const TodoList = () => (
-  <>TodoList markup</>
+import React from 'react';
+
+import { Todo } from '../../types/Todo';
+import { TodoInfo } from '../TodoInfo';
+
+type Props = {
+  todos: Todo[],
+};
+
+export const TodoList: React.FC<Props> = (props) => (
+  <section className="TodoList">
+    {props.todos.map((todo: Todo) => (
+      <TodoInfo
+        completed={todo.completed}
+        title={todo.title}
+        user={todo.user}
+      />
+    ))}
+  </section>
 );
