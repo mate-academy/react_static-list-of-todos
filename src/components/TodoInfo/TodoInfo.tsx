c64984a3354bc637ed19a6ae810da052c42c367a
Vo7kov <jegor.volkov.a@gmail.com>
2023-06-19 13:40:38 +0300

    solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,33 @@
-// Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
+// Libraries
+import React from 'react';
+import classNames from 'classnames';
+
+// Types
+import { User } from '../../types/User';
+
+// Components
+import { UserInfo } from '../UserInfo';
+
+type Props = {
+  completed: boolean;
+  title: string;
+  user: User | null;
+};
+
+export const TodoInfo: React.FC<Props> = ({ completed, title, user }) => (
+  <article className={classNames(
+    'TodoInfo',
+    {
+      'TodoInfo--completed': completed === true,
+    },
+  )}
+  >
+    <h2 className="TodoInfo__title">
+      {title}
+    </h2>
+
+    <UserInfo
+      user={user}
+    />
+  </article>
 );
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,25 @@
-// Add the required types and props
-export const TodoList = () => (
-  <>TodoList markup</>
+// Libraries
+import React from 'react';
+
+// Types
+import { Todo } from '../../types/Todo';
+
+// Components
+import { TodoInfo } from '../TodoInfo';
+
+type Props = {
+  todos: Todo[];
+};
+
+export const TodoList: React.FC<Props> = ({ todos }) => (
+  <section className="TodoList">
+    {todos.map((todo: Todo) => (
+      <TodoInfo
+        completed={todo.completed}
+        title={todo.title}
+        user={todo.user}
+        key={todo.id}
+      />
+    ))}
+  </section>
 );
