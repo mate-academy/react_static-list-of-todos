23782664e13c0debdc24c04a2ba7a1851adaf218
Rodionsav <rodionsav@mate.academy>
2023-06-20 21:28:02 +0200

    Solution

--- src/components/TodoInfo/TodoInfo.spec.jsx
+++ src/components/TodoInfo/TodoInfo.spec.jsx
@@ -1,6 +1,3 @@
-/* eslint-disable max-len */
-/* eslint-disable react/jsx-filename-extension */
-
 import React from 'react';
 import { mount } from '@cypress/react';
 import { TodoInfo } from './TodoInfo';
