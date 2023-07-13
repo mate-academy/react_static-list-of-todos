514bb4500aeb640e0f36de6d7b6b9392ce66cb7d
Artem Pinchuk <aviktorovich193@gmail.com>
2023-07-13 14:34:42 +0300

    Solution

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,4 +1,12 @@
 // Add the required types and props
-export const UserInfo = () => (
-  <>UserInfo markup</>
+import { User } from '../../types/User';
+
+export const UserInfo = ({ user }: { user: User }) => (
+  <a
+    key={user.id}
+    className="UserInfo"
+    href={`mailto:${user.email}`}
+  >
+    {user.name}
+  </a>
 );
