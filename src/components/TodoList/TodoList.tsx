1ee4ebc0a22081f224ac6df9c729befb883e1c08
Amadeusz Lisiecki <amadeusz.lisiecki.1993@gmail.com>
2023-07-14 20:16:59 +0200

    Add todos components

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,18 @@
-// Add the required types and props
-export const TodoList = () => (
-  <>TodoList markup</>
-);
+import { Todo } from '../../types/Todo';
+import { TodoInfo } from '../TodoInfo';
+
+type TodosList = {
+  todos: Todo[];
+};
+
+export const TodoList: React.FC<TodosList> = ({ todos }) => {
+  return (
+    <section className="TodoList">
+      {todos.map(
+        todo => (
+          <TodoInfo todo={todo} key={todo.id} />
+        ),
+      )}
+    </section>
+  );
+};
