11fb284032cf9f450a5fabf3d8b24e3f5261cc7c
Amadeusz Lisiecki <amadeusz.lisiecki.1993@gmail.com>
2023-07-18 15:32:58 +0200

    Code refactor

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,17 +1,15 @@
 import { Todo } from '../../types/Todo';
 import { TodoInfo } from '../TodoInfo';
 
-type TodosList = {
+type TodosListProps = {
   todos: Todo[];
 };
 
-export const TodoList: React.FC<TodosList> = ({ todos }) => {
+export const TodoList: React.FC<TodosListProps> = ({ todos }) => {
   return (
     <section className="TodoList">
       {todos.map(
-        todo => (
-          <TodoInfo todo={todo} key={todo.id} />
-        ),
+        todo => <TodoInfo todo={todo} key={todo.id} />,
       )}
     </section>
   );
