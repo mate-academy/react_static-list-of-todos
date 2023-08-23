5d1c4b52d7059719629fe1eb62dfa907a94ca4cd
Andrzej Pietkiewicz <pietan@me.com>
2023-08-23 21:21:40 +0200

    StaticListOfTodos solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,19 @@
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo';
+
 // Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+type Props = {
+  todo: Todo,
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const { title, user, completed } = todo;
+
+  return (
+    <article className={`TodoInfo ${completed ? 'TodoInfo--completed' : null}`}>
+      <h2 className="TodoInfo__title">{title}</h2>
+      {user !== null && <UserInfo user={user} />}
+    </article>
+
+  );
+};
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,50 @@
+import { TodoInfo } from '../TodoInfo';
+import { Todo } from '../../types/Todo';
+
 // Add the required types and props
-export const TodoList = () => (
-  <>TodoList markup</>
+type Props = {
+  todos: Todo[];
+};
+
+export const TodoList: React.FC<Props> = ({ todos }) => (
+
+  <ul className="TodoList">
+    {todos.map(todo => (
+      <li key={todo.id}>
+        <TodoInfo todo={todo} />
+      </li>
+    ))}
+  </ul>
 );
+
+// <article className="TodoInfo TodoInfo--completed">
+//     <h2 className="TodoInfo__title">HTML</h2>
+
+//     <a className="UserInfo" href="mailto:Sincere@april.biz">
+//       Leanne Graham
+//     </a>
+//   </article>
+
+//   <article className="TodoInfo TodoInfo--completed">
+//     <h2 className="TodoInfo__title">CSS</h2>
+
+//     <a className="UserInfo" href="mailto:Sincere@april.biz">
+//       Leanne Graham
+//     </a>
+//   </article>
+
+//   <article className="TodoInfo TodoInfo--completed">
+//     <h2 className="TodoInfo__title">JS</h2>
+
+//     <a className="UserInfo" href="mailto:Shanna@melissa.tv">
+//       Ervin Howell
+//     </a>
+//   </article>
+
+//   <article className="TodoInfo">
+//     <h2 className="TodoInfo__title">React</h2>
+
+//     <a className="UserInfo" href="mailto:Nathan@yesenia.net">
+//       Clementine Bauch
+//     </a>
+//   </article>
