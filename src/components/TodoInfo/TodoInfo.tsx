19430550ef2ccdafe01531917518ab0aef773040
Hubert Klamann <hubert.klamann1@gmail.com>
2023-08-24 10:37:25 +0200

    task solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -8,16 +8,9 @@ type Prop = {
 export const TodoInfo = ({ todo }: Prop) => (
   <article className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}>
     <h2 className="TodoInfo__title">
-      {todo.user && (
-        <>
-          {todo.title}
-        </>
-      )}
+      {todo.title}
     </h2>
-    {todo.user && (
-      <>
-        <UserInfo user={todo.user} />
-      </>
+    {todo.user && (<UserInfo user={todo.user} />
     )}
   </article>
 );
