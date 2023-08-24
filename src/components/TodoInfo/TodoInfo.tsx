c1c0d816d41312c62ec7c3f3ed83d26b8fabec0c
Kamil <majcherczak.kamil@protonmail.com>
2023-08-24 09:20:22 +0200

    added userInfo conditional display

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -15,7 +15,7 @@ export const TodoInfo: React.FC<Props> = ({ todo }) => {
     <article className={className}>
       <h2 className="TodoInfo__title">{todo.title}</h2>
 
-      <UserInfo user={todo.user} />
+      {todo.user && <UserInfo user={todo.user} />}
     </article>
   );
 };
