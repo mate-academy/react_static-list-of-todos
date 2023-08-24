ce56ddea357957675540f79a7c837bf8136b2972
Piotr <piotrkrasucki@gmail.com>
2023-08-24 07:52:06 +0200

    sol

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,18 @@
+import React from 'react';
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo';
+
 // Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
+type PPP = {
+  todo: Todo;
+};
+
+export const TodoInfo: React.FC<PPP> = ({ todo }) => (
+  <article className={`TodoInfo
+     ${todo.completed ? 'TodoInfo--completed' : ''}`}
+  >
+
+    <h2 className="TodoInfo__title">{todo.title}</h2>
+    {todo.user && <UserInfo user={todo.user} />}
+  </article>
 );
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,4 +1,13 @@
+import React from 'react';
+import { User } from '../../types/User';
+
 // Add the required types and props
-export const UserInfo = () => (
-  <>UserInfo markup</>
+type P = {
+  user: User;
+};
+
+export const UserInfo: React.FC<P> = ({ user }) => (
+  <a className="UserInfo" href={`mailto:${user.email}`}>
+    {user.name}
+  </a>
 );
