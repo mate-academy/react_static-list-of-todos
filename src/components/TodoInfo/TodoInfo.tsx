84cd963045b1926e9880bcb6b769e2b522d2c4bd
Andrzej Pec <pimatstudio@gmail.com>
2023-08-24 18:12:20 +0200

    I want to believe I know what Im doing

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,21 @@
-// Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+import React from 'react';
+import { UserInfo } from '../UserInfo/UserInfo';
+import { Todo } from '../../types/Todo';
+
+interface TodoInfoProps {
+  todo: Todo;
+}
+export const TodoInfo: React.FC<TodoInfoProps> = ({ todo }) => {
+  const isCompleted = todo.completed;
+
+  return (
+    <article
+      className={`TodoInfo ${isCompleted ? 'TodoInfo--completed' : ''}`}
+    >
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+      {todo.user && (
+        <UserInfo user={todo.user} />
+      )}
+    </article>
+  );
+};
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,4 +1,13 @@
-// Add the required types and props
-export const UserInfo = () => (
-  <>UserInfo markup</>
-);
+import React from 'react';
+import { User } from '../../types/User';
+
+interface UserInfoProps {
+  user: User;
+}
+export const UserInfo: React.FC<UserInfoProps> = ({ user }) => {
+  return (
+    <a className="UserInfo" href={`mailto:${user.email}`}>
+      {user.name}
+    </a>
+  );
+};
