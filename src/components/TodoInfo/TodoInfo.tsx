7ab4b3c25f3f0eed693a7748bac59ade9e4e6769
kate-prakofyeva <catherine.smiff@gmail.com>
2023-11-29 12:00:59 +0100

    add task solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,15 @@
-// Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo';
+
+type Props = {
+  todo: Todo;
+};
+
+export const TodoInfo = ({ todo } : Props) => (
+  <article className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}>
+    <h2 className="TodoInfo__title">{todo.title}</h2>
+    {todo.user && (
+      <UserInfo key={todo.userId} user={todo.user} />
+    )}
+  </article>
 );
