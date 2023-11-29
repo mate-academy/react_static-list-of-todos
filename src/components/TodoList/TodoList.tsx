f2d41f78bdd4730d18e2e2b83fc00bb437e1dab0
Marcin Ligmann <marcinligmann2@gmail.com>
2023-11-30 00:24:32 +0100

    initial commit

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,26 @@
-// Add the required types and props
+// import React from 'react';
+// import { TodoInfo } from '../TodoInfo/TodoInfo';
+// import { Todo } from '../../types/Todo';
+import Todos from '../../api/todos';
+import Users from '../../api/users';
+
 export const TodoList = () => (
-  <>TodoList markup</>
+
+  <section className="TodoList">
+    {Todos.map(task => {
+      const name = Users.find(user => user.id === task.userId);
+
+      return (
+        <article className={task.completed
+          ? 'TodoInfo TodoInfo--completed'
+          : 'TodoInfo'}
+        >
+          <h2 className="TodoInfo__title">{task.title}</h2>
+          <a className="UserInfo" href={`mailto:${name?.email}`}>
+            {name?.name}
+          </a>
+        </article>
+      );
+    })}
+  </section>
 );
