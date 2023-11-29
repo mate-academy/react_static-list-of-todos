4092cb4dea474f8cc8fb31fb044f49f5e82fac3e
Kozubowicz <55114447+Kozubowicz@users.noreply.github.com>
2023-11-29 18:20:57 +0100

    solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,16 @@
 // Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo';
+
+type TodoInfoProps = {
+  todo: Todo
+};
+
+export const TodoInfo = ({ todo }: TodoInfoProps) => (
+  <>
+    <article className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}>
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+      {todo.user && <UserInfo user={todo.user} key={todo.userId} />}
+    </article>
+  </>
 );
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,16 @@
+import { TodoInfo } from '../TodoInfo';
+import { Todo } from '../../types/Todo';
 // Add the required types and props
-export const TodoList = () => (
-  <>TodoList markup</>
+type TodoListProps = {
+  todos: Todo[];
+};
+
+export const TodoList = ({ todos }: TodoListProps) => (
+  <>
+    <section className="TodoList">
+      {todos.map((el) => (
+        <TodoInfo todo={el} key={el.id} />
+      ))}
+    </section>
+  </>
 );
