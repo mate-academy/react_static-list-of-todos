bd068eeb8c675202446d2a2108e5264b0e952181
Justin <thedarkvibe1@gmail.com.com>
2024-03-18 14:05:21 +0100

    complete

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,2 +1,20 @@
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo';
+
 // Add the required types and props
-export const TodoInfo = () => <>TodoInfo markup</>;
+type TodoInfoProps = {
+  todo: Todo;
+};
+
+export const TodoInfo: React.FC<TodoInfoProps> = ({
+  todo: { title, completed, user },
+}) => (
+  <article className={`TodoInfo  + ${completed && 'TodoInfo--completed'}`}>
+    <h2 className="TodoInfo__title">{title}</h2>
+    {user && <UserInfo user={user} />}
+  </article>
+);
+
+// - `TodoInfo` takes a `todo` and renders its details, including a `todo.user` with `UserInfo` component if the todo has a user;
+// - make sure that the `TodoInfo` has a `TodoInfo--completed` modifier for all completed todos;
+// - `UserInfo` takes a `user` and renders the details.
