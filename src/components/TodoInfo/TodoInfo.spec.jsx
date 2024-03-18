995e982a0fc222e969d7840d42c3175edf697492
Misha Grinko <glukym@gmail.com>
2024-03-18 14:42:11 +0200

    fix node14 deps

--- src/components/TodoInfo/TodoInfo.spec.jsx
+++ src/components/TodoInfo/TodoInfo.spec.jsx
@@ -2,7 +2,7 @@
 /* eslint-disable react/jsx-filename-extension */
 
 import React from 'react';
-import { mount } from '@cypress/react';
+import { mount } from '@cypress/react18';
 import { TodoInfo } from './TodoInfo';
 
 describe('TodoInfo', () => {
--- src/components/TodoList/TodoList.spec.jsx
+++ src/components/TodoList/TodoList.spec.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-filename-extension */
 
 import React from 'react';
-import { mount } from '@cypress/react';
+import { mount } from '@cypress/react18';
 import { TodoList } from './TodoList';
 
 describe('TodoList', () => {
