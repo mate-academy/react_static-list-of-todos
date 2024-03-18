c1c60f07921a67c76d4742766ebaa3932e29d1b3
Justin <thedarkvibe1@gmail.com.com>
2024-03-18 14:17:06 +0100

    fix classname false

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -9,7 +9,7 @@ type TodoInfoProps = {
 export const TodoInfo: React.FC<TodoInfoProps> = ({
   todo: { title, completed, user },
 }) => (
-  <article className={`TodoInfo  + ${completed && 'TodoInfo--completed'}`}>
+  <article className={`TodoInfo ${completed ? 'TodoInfo--completed' : ''}`}>
     <h2 className="TodoInfo__title">{title}</h2>
     {user && <UserInfo user={user} />}
   </article>
