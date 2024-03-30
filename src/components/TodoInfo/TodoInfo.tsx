1c58edeef300e55a3afb44e2720e4f2151d0bf07
Olaf <olaf.chuszno@gmail.com>
2024-03-30 21:39:34 +0100

    Destructure App to components

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,2 +1,14 @@
+import { Todo } from '../../types/Todo';
+import { UserInfo } from '../UserInfo';
+
 // Add the required types and props
-export const TodoInfo = () => <>TodoInfo markup</>;
+export const TodoInfo: React.FC<{ todo: Todo }> = ({ todo }) => {
+  const className = `TodoInfo${todo.completed ? ' TodoInfo--completed' : ''}`;
+
+  return (
+    <article className={className}>
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+      <UserInfo user={todo.user} key={todo.userId} />
+    </article>
+  );
+};
