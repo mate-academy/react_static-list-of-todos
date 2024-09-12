4e4c70cc2dcb5beee69d5468dfaf3605ba3390fb
Wojciech Przyłuski <przyluskiwojciech@gmail.com>
2024-09-12 16:38:18 +0200

    refactor: extract UserInfo component from TodoInfo

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Todo } from '../../types/Todo';
 import cn from 'classnames';
+import { UserInfo } from '../UserInfo';
 
 // Add the required types and props
 interface TodoInfoInterface {
@@ -19,9 +20,7 @@ export const TodoInfo: React.FC<TodoInfoInterface> = ({ todoInfoItems }) => {
         return (
           <article key={todo.id} className={className}>
             <h2 className="TodoInfo__title">{todo.title}</h2>
-            <a className="UserInfo" href={`mailto:${todo.user?.email}`}>
-              {todo.user?.name}
-            </a>
+            <UserInfo todo={todo} />
           </article>
         );
       })}
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,2 +1,11 @@
+import React from 'react';
+import { Todo } from '../../types/Todo';
+
 // Add the required types and props
-export const UserInfo = () => <>UserInfo markup</>;
+export const UserInfo: React.FC<{ todo: Todo }> = ({ todo }) => {
+  return (
+    <a className="UserInfo" href={`mailto:${todo.user?.email}`}>
+      {todo.user?.name}
+    </a>
+  );
+};
