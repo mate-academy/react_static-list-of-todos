9d960e7448eaa689b206ddf40822effb8fab218e
Wojciech Przyłuski <przyluskiwojciech@gmail.com>
2024-09-12 16:33:10 +0200

    refactor: extract logic for TodoInfo component

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,2 +1,30 @@
+import React from 'react';
+import { Todo } from '../../types/Todo';
+import cn from 'classnames';
+
 // Add the required types and props
-export const TodoInfo = () => <>TodoInfo markup</>;
+interface TodoInfoInterface {
+  todoInfoItems: Todo[];
+}
+
+export const TodoInfo: React.FC<TodoInfoInterface> = ({ todoInfoItems }) => {
+  return (
+    <>
+      {todoInfoItems.map(todo => {
+        const className = cn({
+          TodoInfo: true,
+          'TodoInfo--completed': todo.completed,
+        });
+
+        return (
+          <article key={todo.id} className={className}>
+            <h2 className="TodoInfo__title">{todo.title}</h2>
+            <a className="UserInfo" href={`mailto:${todo.user?.email}`}>
+              {todo.user?.name}
+            </a>
+          </article>
+        );
+      })}
+    </>
+  );
+};
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,29 +1,15 @@
 import React from 'react';
 import { Todo } from '../../types/Todo';
-import cn from 'classnames';
+import { TodoInfo } from '../TodoInfo';
 
 interface TodoListInterface {
-  todoItems: Todo[];
+  todoListItems: Todo[];
 }
 
-export const TodoList: React.FC<TodoListInterface> = ({ todoItems }) => {
+export const TodoList: React.FC<TodoListInterface> = ({ todoListItems }) => {
   return (
     <section className="TodoList">
-      {todoItems.map(todo => {
-        const className = cn({
-          TodoInfo: true,
-          'TodoInfo--completed': todo.completed,
-        });
-
-        return (
-          <article key={todo.id} className={className}>
-            <h2 className="TodoInfo__title">{todo.title}</h2>
-            <a className="UserInfo" href={`mailto:${todo.user?.email}`}>
-              {todo.user?.name}
-            </a>
-          </article>
-        );
-      })}
+      <TodoInfo todoInfoItems={todoListItems} />
     </section>
   );
 };
