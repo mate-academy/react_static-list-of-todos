f3c88817f0ea650d61ded00d16fcf22d6fba061d
Kamil <kpazdziora2@gmail.com>
2024-09-20 15:46:54 +0200

    task solution

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,2 +1,14 @@
+import { User } from "../../types/User";
+
 // Add the required types and props
-export const UserInfo = () => <>UserInfo markup</>;
+export const UserInfo: React.FC<{ user: User | null}> = ({ user }) => {
+  if (!user) {
+    return null;
+  }
+
+  return (
+    <a className="UserInfo" href={`mailto:${user.email}`}>
+      {user.name}
+    </a>
+  );
+};
