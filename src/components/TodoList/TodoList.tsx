60c53a93a8201443b18ac15a96935495e0fc44c0
Krzysztof Piatkowski <krzysztofpiatkowski1987@gmail.com>
2025-05-10 18:09:13 +0200

    Task solution

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,2 +1,15 @@
-// Add the required types and props
-export const TodoList = () => <>TodoList markup</>;
+import { Todo } from '../../types/Todo';
+import { TodoInfo } from '../TodoInfo';
+
+interface Props {
+  todos: Todo[];
+}
+export const TodoList = ({ todos }: Props) => {
+  return (
+    <section className="TodoList">
+      {todos.map(todo => (
+        <TodoInfo todo={todo} key={todo.id} />
+      ))}
+    </section>
+  );
+};
