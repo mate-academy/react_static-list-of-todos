400976b1b656aa52927caff6ab816c741328690e
DDefty <dawid.kogut99@gmail.com>
2025-09-27 11:42:00 +0200

    Refactor Todo components to use props and improve structure

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,2 +1,23 @@
-// Add the required types and props
-export const TodoInfo = () => <>TodoInfo markup</>;
+import { UserInfo } from '../UserInfo';
+import type { Todo } from '../../types/Todo';
+import { getUser } from '../../App';
+import cn from 'classnames';
+
+interface TodoProps {
+  todo: Todo;
+}
+
+export const TodoInfo: React.FC<TodoProps> = ({ todo }) => {
+  const user = getUser(todo.userId);
+
+  return (
+    <>
+      <article
+        className={cn('TodoInfo', { 'TodoInfo--completed': todo.completed })}
+      >
+        <h2 className="TodoInfo__title">{todo.title}</h2>
+        {user && <UserInfo user={user} />}
+      </article>
+    </>
+  );
+};
