4f30d7f593c075a2b793f4dc4d351e63c5ed3a0b
Maciej Sikora <maciej.sikora1508@gmail.com>
2025-11-12 16:55:20 +0100

    task solution and cypress cache cleaning

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import React from 'react';
 import './App.scss';
 
@@ -5,16 +6,14 @@ import todosFromServer from './api/todos';
 import usersFromServer from './api/users';
 
 import { User } from './types/User';
-import { Todo } from './types/Todo';
+import { TodoWithUser } from './types/TodoWithUser';
+import { TodoList } from './components/TodoList';
 
 function getUser(userId: number): User | null {
-  const foundUser = usersFromServer.find(user => user.id === userId);
-
-  // if there is no user with a given userId
-  return foundUser || null;
+  return usersFromServer.find(u => u.id === userId) || null;
 }
 
-export const todos: Todo[] = todosFromServer.map(todo => ({
+export const todos: TodoWithUser[] = todosFromServer.map(todo => ({
   ...todo,
   user: getUser(todo.userId),
 }));
@@ -22,39 +21,6 @@ export const todos: Todo[] = todosFromServer.map(todo => ({
 export const App: React.FC = () => (
   <div className="App">
     <h1 className="App__title">Static list of todos</h1>
-
-    <section className="TodoList">
-      <article className="TodoInfo TodoInfo--completed">
-        <h2 className="TodoInfo__title">HTML</h2>
-
-        <a className="UserInfo" href="mailto:Sincere@april.biz">
-          Leanne Graham
-        </a>
-      </article>
-
-      <article className="TodoInfo TodoInfo--completed">
-        <h2 className="TodoInfo__title">CSS</h2>
-
-        <a className="UserInfo" href="mailto:Sincere@april.biz">
-          Leanne Graham
-        </a>
-      </article>
-
-      <article className="TodoInfo TodoInfo--completed">
-        <h2 className="TodoInfo__title">JS</h2>
-
-        <a className="UserInfo" href="mailto:Shanna@melissa.tv">
-          Ervin Howell
-        </a>
-      </article>
-
-      <article className="TodoInfo">
-        <h2 className="TodoInfo__title">React</h2>
-
-        <a className="UserInfo" href="mailto:Nathan@yesenia.net">
-          Clementine Bauch
-        </a>
-      </article>
-    </section>
+    <TodoList todos={todos} />
   </div>
 );
